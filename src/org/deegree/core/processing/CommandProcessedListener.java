8d1b782f1fd573237d60672e2c85d70883ee8e71
Andreas Poth <poth@lat-lon.de>
2008-03-31 14:48:52 +0000

    topic: api changed type: other module: core description: package name refactored

---
+++ src/org/deegree/core/processing/CommandProcessedListener.java
@@ -0,0 +1,44 @@
+package org.deegree.core.processing;
+
+/**
+ * 
+ * 
+ * 
+ * @author <a href="mailto:poth@lat-lon.de">Andreas Poth</a>
+ * @author last edited by: $Author: poth $
+ * 
+ * @version. $Revision: 6251 $, $Date: 2007-03-19 16:59:28 +0100 (Mo, 19 Mrz 2007) $
+ */
+public interface CommandProcessedListener {
+
+    /**
+     * 
+     * @param event
+     */
+    public void commandPerformed( CommandProcessedEvent event );
+
+    /**
+     * 
+     * @param event
+     */
+    public void commandStarted( CommandProcessedEvent event );
+
+    /**
+     * 
+     * @param event
+     */
+    public void commandStoped( CommandProcessedEvent event );
+
+    /**
+     * 
+     * @param event
+     */
+    public void commandResumed( CommandProcessedEvent event );
+
+    /**
+     * 
+     * @param event
+     */
+    public void commandCancelled( CommandProcessedEvent event );
+
+}
---
+++ src/org/deegree/core/processing/CommandProcessingException.java
@@ -0,0 +1,29 @@
+package org.deegree.core.processing;
+
+/**
+ * 
+ * 
+ * 
+ * @author <a href="mailto:poth@lat-lon.de">Andreas Poth</a>
+ * @author last edited by: $Author: poth $
+ * 
+ * @version. $Revision: 6251 $, $Date: 2007-03-19 16:59:28 +0100 (Mo, 19 Mrz 2007) $
+ */
+public class CommandProcessingException extends RuntimeException {
+
+    /**
+     * @param message
+     * @param e
+     */
+    CommandProcessingException( String message, Throwable e ) {
+        super( message, e );
+    }
+
+    /**
+     * @param message
+     */
+    CommandProcessingException( String message ) {
+        super( message );
+    }
+
+}
