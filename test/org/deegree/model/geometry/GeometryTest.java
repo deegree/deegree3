7beaca8b3760de387002d656c011cfada8bedc73
Oliver Tonnhofer <olt@bogosoft.com>
2008-04-16 15:44:33 +0000

    topic: bug fix type: normal module: model description: added test

---
+++ test/org/deegree/model/geometry/GeometryTest.java
@@ -0,0 +1,68 @@
+package org.deegree.model.geometry;
+
+import org.deegree.model.geometry.GeometryFactory;
+import org.deegree.model.geometry.GeometryFactoryCreator;
+import org.deegree.model.geometry.primitive.Envelope;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+/**
+ * Some very basic tests. Just to make sure we can create a GeometryFactory and do some simple operations.
+ *
+ * @author <a href="mailto:tonnhofer@lat-lon.de">Oliver Tonnhofer</a>
+ * @author last edited by: $Author: $
+ *
+ * @version $Revision: $, $Date: $
+ *}
+ */
+public class GeometryTest extends TestCase {
+
+	private static GeometryFactory geomFactory = GeometryFactoryCreator.getInstance().getGeometryFactory();
+	private static double DELTA = 0.001;
+	
+	Envelope env1, env2, env3, env4, env5;
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		env1 = createEnvelope(10, 10, 20, 20);
+		env2 = createEnvelope(30, 30, 40, 40); // disjoint
+		env3 = createEnvelope(15, 15, 20, 20); // covers env1
+		env4 = createEnvelope(12, 12, 18, 18); // inside env1
+		env5 = createEnvelope(20, 20, 30, 30); // touch env1
+		
+		
+	}
+
+	private Envelope createEnvelope(int x1, int y1, int x2, int y2) {
+		return geomFactory.createEnvelope(new double[] {x1, y1},
+				   						  new double[] {x2, y2},
+				   						  DELTA, null);
+	}
+    
+	/**
+	 * 
+	 */
+	@Test
+	public void testContains() {
+		assertTrue(env1.contains(env1));
+		assertTrue(env1.contains(env4));
+		assertTrue(env1.contains(env3));
+		assertFalse(env4.contains(env1));
+		assertFalse(env3.contains(env1));
+        assertFalse(env1.contains(env2));
+        assertFalse(env1.contains(env5));
+	}
+	
+	/**
+	 * 
+	 */
+    @Test
+	public void testIntersects() {
+		assertTrue(env1.intersects(env1));
+		assertFalse(env1.intersects(env2));
+		assertTrue(env1.intersects(env3));
+		assertTrue(env1.intersects(env4));
+		assertTrue(env1.intersects(env5));
+	}
+}
