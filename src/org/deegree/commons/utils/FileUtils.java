0d8e9f783a2ace8544e932d0ca5ed46dc9817b79
Oliver Tonnhofer <olt@bogosoft.com>
2008-04-16 09:48:46 +0000

    topic: class added module: commons description: added the FileUtils class with some methods for file name handling, more handy methods will come

---
+++ src/org/deegree/commons/utils/FileUtils.java
@@ -0,0 +1,89 @@
+//$HeadURL$
+/*----------------    FILE HEADER  ------------------------------------------
+
+ This file is part of deegree.
+ Copyright (C) 2001-2008 by:
+ EXSE, Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+
+ ---------------------------------------------------------------------------*/
+package org.deegree.commons.utils;
+
+import java.io.File;
+
+/**
+ * This class contains static utility methods for handling files and filenames.
+ * 
+ * @author <a href="mailto:tonnhofer@lat-lon.de">Oliver Tonnhofer</a>
+ * @author last edited by: $Author: $
+ * 
+ * @version $Revision: $, $Date: $
+ * 
+ */
+public class FileUtils {
+    /**
+     * Returns the filename, without any extension. (Eg. /tmp/foo.txt -> /tmp/foo)
+     * 
+     * @param file
+     * @return the basename
+     */
+    public static String getBasename( File file ) {
+        return splitFilename( file ).first;
+    }
+    
+    /**
+     * Returns the file extension (Eg. /tmp/foo.txt -> txt)
+     * 
+     * @param file
+     * @return the file extension
+     */
+    public static String getFileExtension( File file ) {
+        return splitFilename( file ).second;
+    }
+
+    /**
+     * Split a filename in basename and extension.
+     * @param file
+     * @return a StringPair with basename and extension
+     */
+    private static StringPair splitFilename( File file ) {
+        String filename = file.getAbsolutePath();
+        int pos = filename.lastIndexOf( "." );
+        String basename = filename.substring( 0, pos + 1 );
+        String suffix = filename.substring( pos + 1 );
+        return new StringPair( basename, suffix );
+    }
+}
