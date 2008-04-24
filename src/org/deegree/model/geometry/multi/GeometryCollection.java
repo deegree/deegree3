7e84262c53e6202d959be45d17fed7dcb2b10b9a
Andreas Poth <poth@lat-lon.de>
2008-04-24 12:22:54 +0000

    topic: comment added/corrected description: comments for a lot of geometries and their factory added

--- src/org/deegree/model/geometry/multi/GeometryCollection.java
+++ src/org/deegree/model/geometry/multi/GeometryCollection.java
@@ -1,3 +1,40 @@
+//$HeadURL: $
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2007 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
 package org.deegree.model.geometry.multi;
 
 /**
@@ -11,16 +48,40 @@ package org.deegree.model.geometry.multi;
  */
 public interface GeometryCollection<T> extends MultiGeometry<T> {
 
+    /**
+     * 
+     * @return true if a collection contains point geometries
+     */
     public boolean containsPoints();
     
+    /**
+     * 
+     * @return true if a collection contains curve geometries
+     */
     public boolean containsCurves();
     
+    /**
+     * 
+     * @return true if a collection contains curve geometries
+     */
     public boolean containsSurfaces();
     
+    /**
+     * 
+     * @return true if a collection contains solid geometries
+     */
     public boolean containsSolids();
     
+    /**
+     * 
+     * @return true if a collection contains geometric complexes
+     */
     public boolean containsComplexes();
     
+    /**
+     * 
+     * @return true if a collection contains geometry collections
+     */
     public boolean containsCollections();
 	
 }
--- src/org/deegree/model/geometry/multi/MultiSolid.java
+++ src/org/deegree/model/geometry/multi/MultiSolid.java
@@ -1,4 +1,44 @@
+//$HeadURL: $
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2007 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
 package org.deegree.model.geometry.multi;
+
+import org.deegree.model.crs.coordinatesystems.CoordinateSystem;
+import org.deegree.model.geometry.primitive.Solid;
 
 /**
  * 
@@ -11,6 +51,11 @@ package org.deegree.model.geometry.multi;
  */
 public interface MultiSolid<T> extends MultiGeometry<T> {
 
+    /**
+     * A volume will be calculated as the sum of all contained {@link Solid}s
+     * 
+     * @return volume of a MultiSolid measured in units of the used {@link CoordinateSystem}
+     */
     public double getVolume();
-       
+
 }
