ed110a9a99cd81a36fae19b965960581c469cb5f
Markus Schneider <schneider@occamlabs.de>
2008-11-28 09:57:19 +0000

    topic: code enhanced type: functionality module: d3_commons description: Added geometry validation capabilities.

--- src/org/deegree/model/gml/GML311GeometryValidator.java
+++ src/org/deegree/model/gml/GML311GeometryValidator.java
@@ -46,6 +46,9 @@ package org.deegree.model.gml;
 import static javax.xml.stream.XMLStreamConstants.END_DOCUMENT;
 import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.xml.namespace.QName;
 import javax.xml.stream.Location;
 import javax.xml.stream.XMLStreamException;
@@ -55,18 +58,31 @@ import org.deegree.commons.xml.stax.XMLStreamReaderWrapper;
 import org.deegree.model.geometry.Geometry;
 import org.deegree.model.geometry.GeometryFactoryCreator;
 import org.deegree.model.geometry.composite.CompositeGeometry;
+import org.deegree.model.geometry.linearization.NumPointsCriterion;
+import org.deegree.model.geometry.linearization.SurfaceLinearizer;
 import org.deegree.model.geometry.multi.MultiGeometry;
 import org.deegree.model.geometry.primitive.Curve;
 import org.deegree.model.geometry.primitive.GeometricPrimitive;
 import org.deegree.model.geometry.primitive.Point;
+import org.deegree.model.geometry.primitive.Ring;
+import org.deegree.model.geometry.primitive.Surface;
 import org.deegree.model.geometry.primitive.curvesegments.CurveSegment;
+import org.deegree.model.geometry.primitive.curvesegments.LineStringSegment;
+import org.deegree.model.geometry.primitive.surfacepatches.PolygonPatch;
+import org.deegree.model.geometry.primitive.surfacepatches.SurfacePatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.operation.valid.IsValidOp;
+import com.vividsolutions.jts.operation.valid.TopologyValidationError;
 
 /**
- * Takes an XML stream as input (which typically contains a GML feature or feature collection document) and validates
- * all contained <code>gml:_Geometry</code> elements.
+ * Takes an XML stream as input (which should contain a GML geometry, GML feature or feature collection document) and
+ * validates all contained <code>gml:_Geometry</code> elements (at all levels of the document).
  * <p>
  * Validation continues if errors are encountered.
  * 
@@ -83,9 +99,15 @@ public class GML311GeometryValidator extends XMLAdapter {
 
     private XMLStreamReaderWrapper xmlStream;
 
+    private SurfaceLinearizer linearizer;
+
+    private GeometryFactory jtsFactory;
+
     public GML311GeometryValidator( XMLStreamReaderWrapper xmlStream ) {
-        geomParser = new GML311GeometryParser( GeometryFactoryCreator.getInstance().getGeometryFactory(), xmlStream );
         this.xmlStream = xmlStream;
+        geomParser = new GML311GeometryParser( GeometryFactoryCreator.getInstance().getGeometryFactory(), xmlStream );
+        linearizer = new SurfaceLinearizer( GeometryFactoryCreator.getInstance().getGeometryFactory() );
+        jtsFactory = new GeometryFactory();
     }
 
     public void validateGeometries()
@@ -106,77 +128,141 @@ public class GML311GeometryValidator extends XMLAdapter {
         System.out.println( "Validating GML geometry element ('" + xmlStream.getLocalName() + "') at line: "
                             + location.getLineNumber() + ", column: " + location.getColumnNumber() + "." );
         try {
-            validateGeometry(  geomParser.parseGeometry( null ) );
+            validateGeometry( geomParser.parseGeometry( null ) );
         } catch ( Exception e ) {
+            e.printStackTrace();
             System.out.println( "Parsing of GML geometry element ('" + xmlStream.getLocalName() + "') at line: "
                                 + location.getLineNumber() + ", column: " + location.getColumnNumber() + " failed: "
                                 + e.getMessage() + "." );
         }
     }
 
-    private void validateGeometry( Geometry geom ) {
+    public boolean validateGeometry( Geometry geom ) {
+        boolean isValid = false;
         switch ( geom.getGeometryType() ) {
         case COMPOSITE_GEOMETRY: {
-            validate( (CompositeGeometry<?>) geom );
+            isValid = validate( (CompositeGeometry<?>) geom );
             break;
         }
         case COMPOSITE_PRIMITIVE: {
-            validate( (CompositeGeometry<?>) geom );
+            isValid = validate( (CompositeGeometry<?>) geom );
             break;
         }
         case ENVELOPE: {
             String msg = "Internal error: envelope 'geometries' should not occur here.";
-            throw new IllegalArgumentException(msg);
+            throw new IllegalArgumentException( msg );
         }
         case MULTI_GEOMETRY: {
-            validate( (MultiGeometry<?>) geom );            
+            isValid = validate( (MultiGeometry<?>) geom );
             break;
         }
         case PRIMITIVE_GEOMETRY: {
-            validate( (GeometricPrimitive) geom );
+            isValid = validate( (GeometricPrimitive) geom );
             break;
         }
         }
+        return isValid;
     }
 
-    private void validate( GeometricPrimitive geom ) {
-        switch (geom.getPrimitiveType()) {
+    private boolean validate( GeometricPrimitive geom ) {
+        boolean isValid = true;
+        switch ( geom.getPrimitiveType() ) {
         case Point: {
-            System.out.println ("Point geometry. No validation necessary.");
+            System.out.println( "Point geometry. No validation necessary." );
             break;
         }
         case Curve: {
-            System.out.println ("Curve geometry. Validating segment continuity.");
+            System.out.println( "Curve geometry. Validating segment continuity." );
             Curve curve = (Curve) geom;
             Point lastSegmentEndPoint = null;
-            for (CurveSegment segment : curve.getCurveSegments()) {
-                if (lastSegmentEndPoint != null) {
+            for ( CurveSegment segment : curve.getCurveSegments() ) {
+                if ( lastSegmentEndPoint != null ) {
                     Point startPoint = segment.getStartPoint();
-                    System.out.println (startPoint);
+                    isValid = false;
                 }
             }
             break;
         }
         case Surface: {
-            System.out.println ("Surface geometry. Validating using JTS.");
+            System.out.println( "Surface geometry. Validating patches using JTS." );
+            Surface surface = (Surface) geom;
+            System.out.println( surface.getId() );
+            for ( SurfacePatch patch : surface.getPatches() ) {
+                if ( !( patch instanceof PolygonPatch ) ) {
+                    System.out.println( "Skipping validation of patch -- not a PolygonPatch." );
+                } else {
+                    if ( !validate( (PolygonPatch) patch ) ) {
+                        isValid = false;
+                    }
+                }
+            }
             break;
         }
         case Solid: {
             String msg = "Validation of solids is not available";
-            throw new IllegalArgumentException(msg);
+            throw new IllegalArgumentException( msg );
         }
         }
+        return isValid;
     }
 
-    private void validate( CompositeGeometry<?> geom ) {
+    private boolean validate( PolygonPatch inputPatch ) {
+
+        boolean isValid = true;
+        PolygonPatch linearizedPatch = linearizer.linearize( inputPatch, new NumPointsCriterion( 3 ) );
+        if ( linearizedPatch.getExteriorRing() == null ) {
+            String msg = "Cannot validate patches without exterior boundary.";
+            throw new IllegalArgumentException( msg );
+        }
+
+        LinearRing shell = getJTSRing( linearizedPatch.getExteriorRing() );
+
+        List<Ring> interiorRings = linearizedPatch.getInteriorRings();
+        LinearRing[] holes = new LinearRing[interiorRings.size()];
+        for ( int i = 0; i < holes.length; i++ ) {
+            holes[i] = getJTSRing( interiorRings.get( i ) );
+        }
+
+        Polygon polygon = jtsFactory.createPolygon( shell, holes );
+        IsValidOp op = new IsValidOp( polygon );
+        if ( !op.isValid() ) {
+            TopologyValidationError error = op.getValidationError();
+            System.err.println( "Patch has a topology error (" + error.getMessage() + ") at position "
+                                + error.getCoordinate() );
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    private LinearRing getJTSRing( Ring ring ) {
+        List<Coordinate> coordinates = new LinkedList<Coordinate>();
+        for ( Curve member : ring.getMembers() ) {
+            for ( CurveSegment segment : member.getCurveSegments() ) {
+                for ( Point point : ( (LineStringSegment) segment ).getControlPoints() ) {
+                    coordinates.add( new Coordinate( point.getX(), point.getY() ) );
+                }
+            }
+        }
+        return jtsFactory.createLinearRing( coordinates.toArray( new Coordinate[coordinates.size()] ) );
+    }
+
+    private boolean validate( CompositeGeometry<?> geom ) {
+        boolean isValid = true;
         for ( GeometricPrimitive geometricPrimitive : geom ) {
-            validate( geometricPrimitive );
+            if ( !validate( geometricPrimitive ) ) {
+                isValid = false;
+            }
         }
+        return isValid;
     }
 
-    private void validate( MultiGeometry<?> geom ) {
+    private boolean validate( MultiGeometry<?> geom ) {
+        boolean isValid = true;
         for ( Geometry member : geom ) {
-            validateGeometry( member );
+            if ( !validateGeometry( member ) ) {
+                isValid = false;
+            }
         }
+        return isValid;
     }
 }
