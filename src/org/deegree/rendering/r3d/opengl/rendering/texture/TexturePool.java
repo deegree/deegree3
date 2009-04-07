b8672747197235402e58c9b62f9fa11e9ed5c3c8
Rutger Bezema <bezema@lat-lon.de>
2009-04-07 15:38:52 +0000

    topic: code enhanced type: functionality module:[d3] commons description: improved texture loading

--- src/org/deegree/rendering/r3d/opengl/rendering/texture/TexturePool.java
+++ src/org/deegree/rendering/r3d/opengl/rendering/texture/TexturePool.java
@@ -40,7 +40,6 @@ package org.deegree.rendering.r3d.opengl.rendering.texture;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -50,7 +49,6 @@ import javax.media.opengl.GLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.opengl.util.BufferUtil;
 import com.sun.opengl.util.texture.Texture;
 import com.sun.opengl.util.texture.TextureIO;
 
@@ -82,9 +80,34 @@ public class TexturePool {
                            + "/workspace/bonn_3doptimierung/resources/data/example_data/Magnolia_grandiflora_M.png" );
 
         idToFile.put( "1", System.getProperty( "user.home" ) + "/jogl.png" );
-        
-        idToFile.put( "18",  "/home/schneider/Desktop/tree.png" );
 
+        idToFile.put( "18", "/home/rutger/tree.png" );
+    }
+
+    public static synchronized void addTexturesFromDirectory( File textureDir ) {
+        if ( textureDir.exists() ) {
+            if ( textureDir.isDirectory() ) {
+                File[] files = textureDir.listFiles();
+                if ( files != null ) {
+                    for ( File f : files ) {
+                        if ( f != null ) {
+                            String key = f.getName();
+                            idToFile.put( key, f.getAbsolutePath() );
+                            // loadTextureFromFile( key );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param context
+     * @param texture
+     * @return the texture mapped to the given string or <code>null</code> if no texture with that id was found.
+     */
+    public static synchronized Texture getTexture( GL context, String texture ) {
+        return getTexture( texture );
     }
 
     /**
@@ -93,138 +116,20 @@ public class TexturePool {
      */
     public static synchronized void loadTexture( GL context, String texture ) {
 
-        Texture tex = getTexture("18" );
+        Texture tex = getTexture( context, texture );
         if ( tex == null ) {
             LOG.warn( "No texture for id: " + texture );
             return;
         }
-        tex.bind();        
-        // bindTextureToUnit( context, texture );
-        // System.out.println( tex.getTarget() );
-        // IntBuffer t = BufferUtil.newIntBuffer( 1 );
-        // context.glGetIntegerv( GL.GL_MAX_TEXTURE_IMAGE_UNITS, t );
-        // System.out.println( "Units: " + t.get( 0 ) );
-
+        tex.bind();
     }
 
-    /**
-     * @param context
-     * @param texture
-     */
-    // private static void bindTextureToUnit( GL context, String texture ) {
-    // Integer texUnit = get( texture );
-    // if ( texUnit == null ) {
-    // // System.out.println( "Binding" );
-    // texUnit = calcFreeTextureUnit( context, texture );
-    // context.glActiveTexture( texUnit );
-    // tex.bind();
-    // } else {
-    // System.out.println( texUnit );
-    // context.glDisable( GL.GL_TEXTURE_2D );
-    // context.glEnable( GL.GL_TEXTURE_2D );
-    // context.glActiveTexture( toGL_Texture( texUnit ) );
-    // // System.out.println( "not binding" );
-    // }
-    // }
     private synchronized static Texture getTexture( String id ) {
         Texture tex = idToTexture.get( id );
         if ( tex == null ) {
             tex = loadTextureFromFile( id );
         }
         return tex;
-    }
-
-    private static Integer calcFreeTextureUnit( GL context, String id ) {
-        IntBuffer t = BufferUtil.newIntBuffer( 1 );
-        context.glGetIntegerv( GL.GL_MAX_TEXTURE_IMAGE_UNITS, t );
-        int k = t.get( 0 );
-        // System.out.println( "k: " + k );
-        int result = -1;
-        int i = 0;
-        // System.out.println( idToUnit.values() );
-        for ( ; i < k && result == -1; i++ ) {
-            if ( !idToUnit.containsValue( new Integer( i ) ) ) {
-                result = toGL_Texture( i );
-            }
-        }
-        // System.out.println( "result: " + result );
-        // System.out.println( "i: " + i );
-        if ( result == -1 ) {
-            idToUnit.remove( idToUnit.get( 0 ) );
-            result = toGL_Texture( 0 );
-            i = 0;
-        }
-        idToUnit.put( id, i );
-        return result;
-    }
-
-    private static int toGL_Texture( int i ) {
-        switch ( i ) {
-        case 1:
-            return GL.GL_TEXTURE1;
-        case 2:
-            return GL.GL_TEXTURE2;
-        case 3:
-            return GL.GL_TEXTURE3;
-        case 4:
-            return GL.GL_TEXTURE4;
-        case 5:
-            return GL.GL_TEXTURE5;
-        case 6:
-            return GL.GL_TEXTURE6;
-        case 7:
-            return GL.GL_TEXTURE7;
-        case 8:
-            return GL.GL_TEXTURE8;
-        case 9:
-            return GL.GL_TEXTURE9;
-        case 10:
-            return GL.GL_TEXTURE10;
-        case 11:
-            return GL.GL_TEXTURE11;
-        case 12:
-            return GL.GL_TEXTURE12;
-        case 13:
-            return GL.GL_TEXTURE13;
-        case 14:
-            return GL.GL_TEXTURE14;
-        case 15:
-            return GL.GL_TEXTURE15;
-        case 16:
-            return GL.GL_TEXTURE16;
-        case 17:
-            return GL.GL_TEXTURE17;
-        case 18:
-            return GL.GL_TEXTURE18;
-        case 19:
-            return GL.GL_TEXTURE19;
-        case 20:
-            return GL.GL_TEXTURE20;
-        case 21:
-            return GL.GL_TEXTURE21;
-        case 22:
-            return GL.GL_TEXTURE22;
-        case 23:
-            return GL.GL_TEXTURE23;
-        case 24:
-            return GL.GL_TEXTURE24;
-        case 25:
-            return GL.GL_TEXTURE25;
-        case 26:
-            return GL.GL_TEXTURE26;
-        case 27:
-            return GL.GL_TEXTURE27;
-        case 28:
-            return GL.GL_TEXTURE28;
-        case 29:
-            return GL.GL_TEXTURE29;
-        case 30:
-            return GL.GL_TEXTURE30;
-        case 31:
-            return GL.GL_TEXTURE31;
-        default:
-            return GL.GL_TEXTURE0;
-        }
     }
 
     /**
