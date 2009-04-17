1c027a9d80a919ce9acbedc4937b5780f4a2e7fc
Rutger Bezema <bezema@lat-lon.de>
2009-04-17 15:07:16 +0000

    topic: code enhanced type: performance module: [d3] commons description: added convenience methods

--- src/org/deegree/feature/generic/GenericCustomPropertyValue.java
+++ src/org/deegree/feature/generic/GenericCustomPropertyValue.java
@@ -73,27 +73,64 @@ public class GenericCustomPropertyValue {
         childNodes.add( child );
     }
 
+    /**
+     * @return all textnodes in this {@link GenericCustomPropertyValue}.
+     */
+    public List<String> getTextNodes() {
+        List<String> result = new ArrayList<String>();
+        for ( Object o : childNodes ) {
+            if ( o != null ) {
+                if ( o instanceof String ) {
+                    result.add( (String) o );
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 
+     * @param qName
+     *            to match
+     * @return the direct childnodes of this {@link GenericCustomPropertyValue}, which are
+     *         {@link GenericCustomPropertyValue} as well.
+     */
+    public List<GenericCustomPropertyValue> getChildNodes( QName qName ) {
+        List<GenericCustomPropertyValue> result = new ArrayList<GenericCustomPropertyValue>();
+        for ( Object o : childNodes ) {
+            if ( o != null ) {
+                if ( o instanceof GenericCustomPropertyValue ) {
+                    if ( qName.equals( ( (GenericCustomPropertyValue) o ).name ) ) {
+                        result.add( (GenericCustomPropertyValue) o );
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     @Override
-    public String toString () {
+    public String toString() {
         String s = name.toString();
         s += "=[";
         int i = 0;
-        for ( Map.Entry<QName, String> attribute : attributes.entrySet()) {
+        for ( Map.Entry<QName, String> attribute : attributes.entrySet() ) {
             s += "@" + attribute.getKey() + "'" + attribute.getValue();
-            if (i++ != attributes.size() - 1) {
-                s += ",";                
+            if ( i++ != attributes.size() - 1 ) {
+                s += ",";
             }
         }
-        if (i != 0 && childNodes.size() > 0) {
+        if ( i != 0 && childNodes.size() > 0 ) {
             s += ",";
         }
         i = 0;
         for ( Object o : childNodes ) {
             s += o.toString();
-            if (i++ != childNodes.size() - 1) {
-                s += ",";                
+            if ( i++ != childNodes.size() - 1 ) {
+                s += ",";
             }
         }
         s += "]";
         return s;
-    }}
+    }
+}
