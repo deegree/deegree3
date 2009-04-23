8ed7a462ae414b543a202e8a86ca1b78f720615c
Andrei Ionita <ionita@lat-lon.de>
2009-04-23 15:58:29 +0000

    topic: class added module: d3_commons description: Added test classes for FeatureExporter and GeometryExporter (GML 3.1.1)

---
+++ test/org/deegree/feature/gml/GMLFeatureExporterTest.java
@@ -0,0 +1,124 @@
+//$HeadURL: svn+ssh://aionita@svn.wald.intevation.org/deegree/base/trunk/resources/eclipse/files_template.xml $
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2008 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
+
+package org.deegree.feature.gml;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.deegree.commons.xml.XMLParsingException;
+import org.deegree.commons.xml.stax.XMLStreamReaderWrapper;
+import org.deegree.commons.xml.stax.XMLStreamWriterWrapper;
+import org.deegree.crs.exceptions.UnknownCRSException;
+import org.deegree.feature.Feature;
+import org.deegree.feature.gml.schema.GMLApplicationSchemaXSDAdapter;
+import org.deegree.feature.gml.schema.GMLVersion;
+import org.deegree.feature.types.ApplicationSchema;
+import org.deegree.junit.XMLAssert;
+import org.junit.Test;
+import org.xml.sax.InputSource;
+
+/**
+ * Exports the features in the Philosophers example and validates them against the corresponding schema.
+ * 
+ * @author <a href="mailto:ionita@lat-lon.de">Andrei Ionita</a>
+ * 
+ * @author last edited by: $Author: ionita $
+ * 
+ * @version $Revision: $, $Date: $
+ * 
+ */
+public class GMLFeatureExporterTest {
+
+    final String DIR = "testdata/features/";
+    
+    final String SOURCE_FILE = "Philosopher_FeatureCollection.xml";
+    
+    final String SCHEMA_LOCATION_ATTRIBUTE = "schema/Philosopher_typesafe.xsd";
+    
+    final String SCHEMA_LOCATION = "http://www.deegree.org/app schema/Philosopher_typesafe.xsd"; 
+
+    @Test
+    public void testValidateExportedFeatures()
+                            throws ClassCastException, ClassNotFoundException, InstantiationException,
+                            IllegalAccessException, XMLStreamException, FactoryConfigurationError, IOException,
+                            XMLParsingException, UnknownCRSException {
+        String schemaURL = this.getClass().getResource( SCHEMA_LOCATION_ATTRIBUTE ).toString();
+        GMLApplicationSchemaXSDAdapter xsdAdapter = new GMLApplicationSchemaXSDAdapter( schemaURL,
+                                                                                        GMLVersion.VERSION_31 );
+        ApplicationSchema schema = xsdAdapter.extractFeatureTypeSchema();
+
+        URL docURL = GMLFeatureExporterTest.class.getResource( DIR + SOURCE_FILE );
+        XMLStreamReader xmlReader = XMLInputFactory.newInstance().createXMLStreamReader( docURL.toString(),
+                                                                                         docURL.openStream() );
+        xmlReader.next();
+        GMLIdContext idContext = new GMLIdContext();
+        GMLFeatureParser gmlAdapter = new GMLFeatureParser( schema, idContext );
+        Feature feature = gmlAdapter.parseFeature(new XMLStreamReaderWrapper( xmlReader, docURL.toString() ), null );
+        idContext.resolveXLinks( schema );
+        
+        XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+        OutputStream out = new FileOutputStream( "/tmp/exported_" + SOURCE_FILE );
+        XMLStreamWriterWrapper writer = 
+            new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ), SCHEMA_LOCATION_ATTRIBUTE );
+        writer.setDefaultNamespace( "http://www.opengis.net/gml" );
+        writer.setPrefix( "app", "http://www.deegree.org/app" );
+        writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+        writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+        writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+        writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+        writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );        
+        GMLFeatureExporter exporter = new GMLFeatureExporter( writer );
+        exporter.export( feature );    
+        writer.flush();
+        writer.close();
+        out.close();
+        
+        XMLAssert.assertValidDocument( schemaURL, new InputSource( new FileReader( "/tmp/exported_" + SOURCE_FILE ) ) );
+    }
+}
---
+++ test/org/deegree/feature/gml/GMLGeometryExporterTest.java
@@ -0,0 +1,334 @@
+//$HeadURL: svn+ssh://aionita@svn.wald.intevation.org/deegree/base/trunk/resources/eclipse/files_template.xml $
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2008 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
+
+package org.deegree.feature.gml;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+
+import org.deegree.commons.xml.XMLParsingException;
+import org.deegree.commons.xml.stax.XMLStreamReaderWrapper;
+import org.deegree.commons.xml.stax.XMLStreamWriterWrapper;
+import org.deegree.crs.exceptions.UnknownCRSException;
+import org.deegree.geometry.Geometry;
+import org.deegree.geometry.GeometryFactory;
+import org.deegree.geometry.GeometryFactoryCreator;
+import org.deegree.geometry.primitive.curvesegments.CurveSegment;
+import org.deegree.geometry.primitive.surfacepatches.SurfacePatch;
+import org.deegree.junit.XMLAssert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.InputSource;
+
+/**
+ * Exporting all types of geometries and validating them.
+ * 
+ * @author <a href="mailto:ionita@lat-lon.de">Andrei Ionita</a>
+ * 
+ * @author last edited by: $Author: ionita $
+ * 
+ * @version $Revision: $, $Date: $
+ * 
+ */
+public class GMLGeometryExporterTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger( GMLGeometryExporterTest.class );
+    
+    final static String DIR = "testdata/geometries/";
+    
+    final static String PATCH_DIR = "testdata/patches/";
+    
+    final static String SEGMENT_DIR = "testdata/segments/";
+        
+    private static List<String> sources = new ArrayList<String>();
+    
+    private static List<String> patchSources = new ArrayList<String>();
+    
+    private static List<String> segmentSources = new ArrayList<String>();
+    
+    private static List<String> envelopeSources = new ArrayList<String>();
+    
+    final String SCHEMA_LOCATION_ATTRIBUTE = 
+        "http://www.opengis.net/gml http://schemas.opengis.net/gml/3.1.1/base/gml.xsd";
+    
+    final String SCHEMA_LOCATION = "http://schemas.opengis.net/gml/3.1.1/base/gml.xsd";
+    
+    static {
+        sources.add( "CompositeCurve.gml" );
+        sources.add( "CompositeSolid.gml" );
+        sources.add( "CompositeSurface.gml" );
+        sources.add( "Curve.gml" );
+        sources.add( "GeometricComplex.gml" );
+        sources.add( "LinearRing.gml" );
+        sources.add( "LineString_coord.gml" );
+        sources.add( "LineString_coordinates.gml" );
+        sources.add( "LineString_pointProperty.gml" );
+        sources.add( "LineString_pointRep.gml" );
+        sources.add( "LineString_pos.gml" );
+        sources.add( "LineString_posList.gml" );
+        sources.add( "MultiCurve.gml" );
+        sources.add( "MultiGeometry.gml" );
+        sources.add( "MultiLineString.gml" );
+        sources.add( "MultiPoint_members.gml" );
+        sources.add( "MultiPolygon.gml" );
+        sources.add( "MultiSolid.gml" );
+        sources.add( "MultiSurface.gml" );
+        sources.add( "OrientableCurve.gml" );
+        sources.add( "OrientableSurface.gml" );
+        sources.add( "Point_coord.gml" );
+        sources.add( "Point_coordinates.gml" );
+        sources.add( "Point_pos.gml" );
+        sources.add( "Polygon.gml" );
+        sources.add( "PolyhedralSurface.gml" );
+        sources.add( "Ring.gml" );
+        sources.add( "Solid.gml" );
+        sources.add( "Surface.gml" );
+        sources.add( "Tin.gml" );
+        sources.add( "TriangulatedSurface.gml" );
+        
+        patchSources.add( "Cone.gml");
+        patchSources.add( "Cylinder.gml");
+        patchSources.add( "PolygonPatch.gml");
+        patchSources.add( "Rectangle.gml");
+        patchSources.add( "Sphere.gml");
+        patchSources.add( "Triangle.gml");
+        
+        segmentSources.add( "Arc.gml" );
+        segmentSources.add( "ArcByBulge.gml" );
+        segmentSources.add( "ArcByCenterPoint.gml" );
+        segmentSources.add( "ArcString.gml" );
+        segmentSources.add( "ArcStringByBulge.gml" );
+        segmentSources.add( "Bezier.gml" );
+        segmentSources.add( "BSpline.gml" );
+        segmentSources.add( "Circle.gml" );
+        segmentSources.add( "CircleByCenterPoint.gml" );
+        segmentSources.add( "Clothoid.gml" );
+        segmentSources.add( "CubicSpline.gml" );
+        segmentSources.add( "Geodesic.gml" );
+        segmentSources.add( "GeodesicString.gml" );
+        segmentSources.add( "LineStringSegment.gml" );
+        
+        envelopeSources.add( "Envelope_coord.gml" );
+        envelopeSources.add( "Envelope_coordinates.gml" );
+        envelopeSources.add( "Envelope_pos.gml" );
+        envelopeSources.add( "Envelope.gml" );
+    }
+    
+    @Test
+    public void testValidatingExportedAbstractGeometryTypes() throws XMLStreamException, XMLParsingException, UnknownCRSException, 
+    FactoryConfigurationError, IOException {        
+        for ( String source : sources ) {
+            LOG.info( "Exporting " +  DIR + source ); 
+            GMLIdContext idContext = new GMLIdContext();
+            GML311GeometryParser parser = 
+                new GML311GeometryParser( GeometryFactoryCreator.getInstance().getGeometryFactory(), idContext );
+            URL docURL = GMLGeometryExporterTest.class.getResource( DIR + source );
+            XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( docURL );
+            xmlReader.nextTag();
+            Geometry geom = parser.parseAbstractGeometry( xmlReader, null );
+            
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+            OutputStream out = new FileOutputStream( "/tmp/exported_" + source ); 
+            XMLStreamWriterWrapper writer = new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ),
+                                                                        SCHEMA_LOCATION_ATTRIBUTE );
+            writer.setPrefix( "app", "http://www.deegree.org/app" );
+            writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+            writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+            writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+            writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+            writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+            GML311GeometryExporter exporter = new GML311GeometryExporter( writer );
+            exporter.export( geom );
+            writer.flush();
+            writer.close();
+            out.close();
+            
+            XMLAssert.assertValidDocument( SCHEMA_LOCATION, new InputSource( new FileReader( "/tmp/exported_" + source ) ) );
+        }
+    }
+    
+    @Test
+    public void testValidatingExportedSurfacePatches() throws XMLStreamException, XMLParsingException, UnknownCRSException, 
+    FactoryConfigurationError, IOException {        
+        for ( String patchSource : patchSources ) {
+            LOG.info( "Exporting " +  PATCH_DIR + patchSource ); 
+            GMLIdContext idContext = new GMLIdContext();
+            GeometryFactory geomFactory = GeometryFactoryCreator.getInstance().getGeometryFactory();
+            GML311GeometryParser geometryParser = new GML311GeometryParser( geomFactory, idContext );
+            GML311SurfacePatchParser parser = new GML311SurfacePatchParser( geometryParser, geomFactory );
+            URL docURL = GMLGeometryExporterTest.class.getResource( PATCH_DIR + patchSource );
+            if ( docURL == null )
+                System.out.println( GMLGeometryExporterTest.class.getResource( PATCH_DIR + patchSource ) );
+            XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( docURL );
+            xmlReader.nextTag();
+            SurfacePatch surfPatch = parser.parseSurfacePatch( xmlReader, null );
+            
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+            OutputStream out = new FileOutputStream( "/tmp/exported_" + patchSource ); 
+            XMLStreamWriterWrapper writer = new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ),
+                                                                        SCHEMA_LOCATION_ATTRIBUTE );
+            writer.setPrefix( "app", "http://www.deegree.org/app" );
+            writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+            writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+            writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+            writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+            writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+            GML311GeometryExporter exporter = new GML311GeometryExporter( writer );
+            exporter.export( surfPatch );
+            writer.flush();
+            writer.close();
+            out.close();
+            
+            XMLAssert.assertValidDocument( SCHEMA_LOCATION, new InputSource( new FileReader( "/tmp/exported_" + patchSource ) ) );
+        }
+    }
+    
+    @Test
+    public void testValidatingExportedCurveSegments() throws XMLStreamException, XMLParsingException, UnknownCRSException, 
+    FactoryConfigurationError, IOException {        
+        for ( String segmentSource : segmentSources ) {
+            LOG.info( "Exporting " +  SEGMENT_DIR + segmentSource ); 
+            GMLIdContext idContext = new GMLIdContext();
+            GeometryFactory geomFactory = GeometryFactoryCreator.getInstance().getGeometryFactory();
+            GML311GeometryParser geometryParser = new GML311GeometryParser( geomFactory, idContext );
+            GML311CurveSegmentParser parser = new GML311CurveSegmentParser( geometryParser, geomFactory );
+            URL docURL = GMLGeometryExporterTest.class.getResource( SEGMENT_DIR + segmentSource );
+            XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( docURL );
+            xmlReader.nextTag();
+            CurveSegment curveSegment = parser.parseCurveSegment( xmlReader, null );
+            
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+            OutputStream out = new FileOutputStream( "/tmp/exported_" + segmentSource ); 
+            XMLStreamWriterWrapper writer = new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ),
+                                                                        SCHEMA_LOCATION_ATTRIBUTE );
+            writer.setPrefix( "app", "http://www.deegree.org/app" );
+            writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+            writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+            writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+            writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+            writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+            GML311GeometryExporter exporter = new GML311GeometryExporter( writer );
+            exporter.export( curveSegment );
+            writer.flush();
+            writer.close();
+            out.close();
+            
+            XMLAssert.assertValidDocument( SCHEMA_LOCATION, new InputSource( new FileReader( "/tmp/exported_" + segmentSource ) ) );
+        }
+    }
+    
+    @Test
+    public void testValidatingExportedEnvelope() throws XMLStreamException, FactoryConfigurationError, IOException, XMLParsingException, UnknownCRSException {
+        for ( String envelopeSource : envelopeSources ) {
+            LOG.info( "Exporting " +  DIR + envelopeSource ); 
+            GMLIdContext idContext = new GMLIdContext();
+            GML311GeometryParser parser = 
+                new GML311GeometryParser( GeometryFactoryCreator.getInstance().getGeometryFactory(), idContext );
+            URL docURL = GMLGeometryExporterTest.class.getResource( DIR + envelopeSource );
+            XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( docURL );
+            xmlReader.nextTag();
+            Geometry geom = parser.parseEnvelope( xmlReader, null );
+            
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+            OutputStream out = new FileOutputStream( "/tmp/exported_" + envelopeSource ); 
+            XMLStreamWriterWrapper writer = new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ),
+                                                                        SCHEMA_LOCATION_ATTRIBUTE );
+            writer.setPrefix( "app", "http://www.deegree.org/app" );
+            writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+            writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+            writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+            writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+            writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+            GML311GeometryExporter exporter = new GML311GeometryExporter( writer );
+            exporter.export( geom );
+            writer.flush();
+            writer.close();
+            out.close();
+            
+            XMLAssert.assertValidDocument( SCHEMA_LOCATION, new InputSource( new FileReader( "/tmp/exported_" + envelopeSource ) ) );
+        }
+    }
+    
+    @Test
+    public void testValidatingExportedXLinkMultiGeometry1() throws XMLParsingException, XMLStreamException, UnknownCRSException, FactoryConfigurationError, IOException {
+        String source =  "XLinkMultiGeometry1.gml";
+        LOG.info( "Exporting " +  DIR + source ); 
+        GMLIdContext idContext = new GMLIdContext();
+        GML311GeometryParser parser = 
+            new GML311GeometryParser( GeometryFactoryCreator.getInstance().getGeometryFactory(), idContext );
+        URL docURL = GMLGeometryExporterTest.class.getResource( DIR + source );
+        XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( docURL );
+        xmlReader.nextTag();
+        Geometry geom = parser.parseMultiGeometry( xmlReader, null );
+        
+        idContext.resolveXLinks( null );
+        
+        XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+        outputFactory.setProperty( "javax.xml.stream.isRepairingNamespaces", new Boolean( true ) );
+        OutputStream out = new FileOutputStream( "/tmp/exported_" + source ); 
+        XMLStreamWriterWrapper writer = new XMLStreamWriterWrapper( outputFactory.createXMLStreamWriter( out ),
+                                                                    SCHEMA_LOCATION_ATTRIBUTE );
+        writer.setPrefix( "app", "http://www.deegree.org/app" );
+        writer.setPrefix( "gml", "http://www.opengis.net/gml" );
+        writer.setPrefix( "ogc", "http://www.opengis.net/ogc" );
+        writer.setPrefix( "wfs", "http://www.opengis.net/wfs" );
+        writer.setPrefix( "xlink", "http://www.w3.org/1999/xlink" );
+        writer.setPrefix( "xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+        GML311GeometryExporter exporter = new GML311GeometryExporter( writer );
+        exporter.export( geom );
+        writer.flush();
+        writer.close();
+        out.close();
+        
+        XMLAssert.assertValidDocument( SCHEMA_LOCATION, new InputSource( new FileReader( "/tmp/exported_" + source ) ) );
+    }
+}
