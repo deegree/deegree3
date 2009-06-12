d40fe2c6974b528c1a79fcd51200c46cdc730b80
Andrei Ionita <ionita@lat-lon.de>
2009-06-12 12:31:36 +0000

    topic: class added module: d3_commons description: added classes for GetGmlObject request

--- src/org/deegree/protocol/wfs/getgmlobject/GetGmlObject.java
+++ src/org/deegree/protocol/wfs/getgmlobject/GetGmlObject.java
@@ -43,13 +43,73 @@
 package org.deegree.protocol.wfs.getgmlobject;
 
 /**
- * Represents a <code>GetGmlObject</code> request to a WFS.
+ * The <code>GetGmlObject</code> operation allows retrieval of features and elements by ID from a WFS. 
+ * A GetGmlObject request is processed by a WFS and an XML document fragment containing the result set is 
+ * returned to the client. The GetGmlObject operation is optional and if supported it must be advertised 
+ * on the getCapabilities operation.
+ * <b>Supported by WFS specification version 1.1.0</b>
  *
  * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
+ * @author <a href="mailto:ionita@lat-lon.de">Andrei Ionita</a>
  * @author last edited by: $Author: schneider $
  * 
  * @version $Revision: $, $Date: $
  */
 public class GetGmlObject {
+  
+    private GmlObjectId id;
+    
+    private String outputFormat;
+ 
+    private String traverseXlinkDepth;
+        
+    // Integer type used since it might be null (traverseXlinkExpiry attribute is optional)
+    private Integer traverseXlinkExpiry;
+    
+    /**
+     * @param id a GMLObjectId
+     * @param outputFormat  a String format of the result set
+     * @param traverseXlinkDepth    maximum depth of Xlink traversing, an integer as String or '*'  
+     * @param traverseXlinkExpiry   time in minutes for which the WFS waits for a response  
+     */
+    public GetGmlObject( GmlObjectId id, String outputFormat, String traverseXlinkDepth, 
+                         int traverseXlinkExpiry ) {
+        this.id = id;
+        this.outputFormat = outputFormat;
+        this.traverseXlinkDepth = traverseXlinkDepth;
+        this.traverseXlinkExpiry = traverseXlinkExpiry;
+    }
+
+    public GmlObjectId getId() {
+        return id;
+    }
+
+    /**
+     * Returns format of the result set 
+     * @return
+     *          the format of the result set; default value is 'text/xml; subtype=gml/3.1.1'; might be null 
+     */
+    public String getOutputFormat() {
+        return outputFormat;
+    }
+
+    /**
+     * Returns the maximum depth at which the Xlinks references will be traversed
+     * @return
+     *          the maximum depth at which the Xlinks references will be traversed; '*' for unbounded
+     */
+    public String getTraverseXlinkDepth() {
+        return traverseXlinkDepth;
+    }
+
+    // in minutes; indicates 
+    /**
+     * Returns how long a WFS should wait for a response
+     * @return
+     *          the time in minutes a WFS should wait for a response;  might be null
+     */
+    public int getTraverseXlinkExpiry() {
+        return traverseXlinkExpiry;
+    }
 
 }
