d66d7d95e95ec20dc6a24131a381cedbff98fe39
Andreas Schmitz <schmitz@occamlabs.de>
2009-07-07 15:20:00 +0000

    topic: bug fix type: normal module: wms description: Fixed termination problem when words were empty.

--- src/org/deegree/rendering/r2d/strokes/TextStroke.java
+++ src/org/deegree/rendering/r2d/strokes/TextStroke.java
@@ -33,19 +33,19 @@
  e-mail: info@deegree.org
 ----------------------------------------------------------------------------*/
 /*
-Copyright 2006 Jerry Huxtable
+ Copyright 2006 Jerry Huxtable
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
 
 package org.deegree.rendering.r2d.strokes;
@@ -71,6 +71,7 @@ import java.awt.geom.GeneralPath;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 import org.deegree.commons.utils.Pair;
 import org.deegree.rendering.r2d.styling.components.LinePlacement;
@@ -78,11 +79,11 @@ import org.slf4j.Logger;
 
 /**
  * <code>TextStroke</code>
- *
+ * 
  * @author Jerry Huxtable
  * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
  * @author last edited by: $Author$
- *
+ * 
  * @version $Revision$, $Date$
  */
 public class TextStroke implements Stroke {
@@ -126,6 +127,12 @@ public class TextStroke implements Stroke {
         Pair<Boolean, GeneralPath> pair = new Pair<Boolean, GeneralPath>();
 
         LinkedList<String> words = new LinkedList<String>( asList( text.split( "\\s" ) ) );
+        ListIterator<String> list = words.listIterator();
+        while ( list.hasNext() ) {
+            if ( list.next().trim().equals( "" ) ) {
+                list.remove();
+            }
+        }
         LinkedList<String> wordsCopy = new LinkedList<String>();
         wordsCopy.addAll( words );
         LinkedList<StringOrGap> wordsToRender = new LinkedList<StringOrGap>();
