2bb06edf9d41ee19c9affb9bed0f8002266dfdc1
Markus Schneider <schneider@occamlabs.de>
2009-07-29 08:48:13 +0000

    topic: code enhanced type: other module: d3_core description: Test cleanup.

--- test/org/deegree/feature/gml/schema/GMLApplicationSchemaXSDDecoderTest.java
+++ test/org/deegree/feature/gml/schema/GMLApplicationSchemaXSDDecoderTest.java
@@ -42,7 +42,7 @@ import org.deegree.feature.types.FeatureType;
 import org.junit.Test;
 
 /**
- * Tests that check that the extraction of {@link FeatureType}s from GML application schemas works as expected.
+ * Tests that check the extraction of {@link FeatureType}s from GML application schemas.
  * 
  * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
  * @author last edited by: $Author: schneider $
