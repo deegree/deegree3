1c0c6710c67af6c81e8ab79b45d0d34b06a6dc0f
Markus Schneider <schneider@occamlabs.de>
2009-08-04 12:49:50 +0000

    topic: bug fix type: normal module: d3_core description: Fixed NPE.

--- src/org/deegree/geometry/standard/primitive/DefaultPolygon.java
+++ src/org/deegree/geometry/standard/primitive/DefaultPolygon.java
@@ -111,11 +111,14 @@ public class DefaultPolygon extends DefaultSurface implements Polygon {
     @Override
     protected com.vividsolutions.jts.geom.Geometry buildJTSGeometry() {
 
-        LinearRing shell = (LinearRing) getAsAbstractDefaultGeometry( exteriorRing).getJTSGeometry();
-        LinearRing[] holes = new LinearRing[interiorRings.size()];
-        int i = 0;
-        for ( Ring ring : interiorRings ) {
-            holes[i++] = (LinearRing) getAsAbstractDefaultGeometry(ring).getJTSGeometry();
+        LinearRing shell = (LinearRing) getAsAbstractDefaultGeometry( exteriorRing ).getJTSGeometry();
+        LinearRing[] holes = null;
+        if ( interiorRings != null ) {
+            holes = new LinearRing[interiorRings.size()];
+            int i = 0;
+            for ( Ring ring : interiorRings ) {
+                holes[i++] = (LinearRing) getAsAbstractDefaultGeometry( ring ).getJTSGeometry();
+            }
         }
         return jtsFactory.createPolygon( shell, holes );
     }
