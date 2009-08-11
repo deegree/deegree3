aa341a22ae28c4c4e663586a10d578bb51cbaeaf
Rutger Bezema <bezema@lat-lon.de>
2009-08-11 15:03:20 +0000

    topic: code enhanced type: functionality, other module: [d3] core description: added more wcs 1.0.0 specific things

--- src/org/deegree/protocol/wcs/WCSConstants.java
+++ src/org/deegree/protocol/wcs/WCSConstants.java
@@ -37,6 +37,7 @@
 package org.deegree.protocol.wcs;
 
 import org.deegree.commons.types.ows.Version;
+import org.deegree.coverage.raster.interpolation.InterpolationType;
 
 /**
  * Important constants from the WCS specifications.
@@ -69,6 +70,9 @@ public class WCSConstants {
     /** WCS protocol version 1.1.0 */
     public static final Version VERSION_110 = Version.parseVersion( "1.1.0" );
 
+    /** WCS 1.0.0 exception xml mime type */
+    public static final String EXCEPTION_FORMAT_100 = "application/vnd.ogc.se_xml";
+
     /**
      * Enum type for discriminating between the different types of WebCoverageService (WCS) requests.
      * 
@@ -85,5 +89,101 @@ public class WCSConstants {
         GetCapabilities,
         /** Retrieve a coverage for a certain region. */
         GetCoverage
+    }
+
+    /**
+     * 
+     * The <code>InterpolationMethod</code> class maps Interpolations to the wcs 1.0.0 protocol name.
+     * 
+     * @author <a href="mailto:bezema@lat-lon.de">Rutger Bezema</a>
+     * @author last edited by: $Author: rbezema $
+     * @version $Revision: $, $Date: $
+     * 
+     */
+    public enum InterpolationMethod {
+        /** Interpolating nearest neighbor. */
+        Nearest_Neighbor( "nearest neighbor" ),
+        /** Interpolating bilinear. */
+        Bilinear( "bilinear" ),
+        /** Interpolating bicubic. */
+        Bicubic( "bicubic" ),
+        /** Interpolating lost area. */
+        LostArea( "lost area" ),
+        /** Interpolating barycentric. */
+        Barycentric( "barycentric" ),
+        /** Not Interpolating. */
+        None( "none" );
+
+        private final String name_1_0_0;
+
+        private InterpolationMethod( String name_1_0_0 ) {
+            this.name_1_0_0 = name_1_0_0;
+        }
+
+        /**
+         * 
+         * @param version
+         *            of the suspected protocol.
+         * @return the name as it is define in the spec for given protocol version.
+         */
+        public String getProtocolName( Version version ) {
+            if ( VERSION_100.equals( version ) ) {
+                return name_1_0_0;
+            }
+            return name();
+        }
+
+        /**
+         * @param ip
+         * @return an heuristically determined Interpolation type
+         */
+        public static InterpolationMethod map( String ip ) {
+            InterpolationMethod result = None;
+            if ( ip != null ) {
+                String t = ip.toLowerCase();
+                if ( t.contains( "neigh" ) ) {
+                    result = Nearest_Neighbor;
+                } else if ( t.contains( "bili" ) ) {
+                    result = Bilinear;
+                } else if ( t.contains( "bicu" ) ) {
+                    result = Bicubic;
+                } else if ( t.contains( "lost" ) ) {
+                    result = LostArea;
+                } else if ( t.contains( "bary" ) ) {
+                    result = Barycentric;
+                }
+            }
+            return result;
+        }
+
+        /**
+         * @param type
+         * @return maps the Raster-API {@link InterpolationType} to the WCS {@link InterpolationMethod}.
+         */
+        public static InterpolationMethod map( InterpolationType type ) {
+            InterpolationMethod result = None;
+            if ( type != null ) {
+                switch ( type ) {
+                case NEAREST_NEIGHBOR:
+                    result = Nearest_Neighbor;
+                    break;
+                case BILINEAR:
+                    result = Bilinear;
+                    break;
+                case NONE:
+                    result = None;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        /**
+         * @return this WCS interpolation type as a raster api type, or <code>null</code> if it the interpolation
+         *         method is not supported by the raster api.
+         */
+        public InterpolationType asRasterAPIType() {
+            return InterpolationType.fromString( name() );
+        }
     }
 }
