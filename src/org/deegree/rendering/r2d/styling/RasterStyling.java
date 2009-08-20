6775b76e4d12ea8105eaf2d38e1827a91978eeef
Andreas Schmitz <schmitz@occamlabs.de>
2009-08-20 12:03:47 +0000

    topic: code enhanced type: functionality module: commons description: First shot at raster styling.

--- src/org/deegree/rendering/r2d/styling/RasterStyling.java
+++ src/org/deegree/rendering/r2d/styling/RasterStyling.java
@@ -36,6 +36,12 @@
 
 package org.deegree.rendering.r2d.styling;
 
+import static org.deegree.rendering.r2d.styling.RasterStyling.Overlap.RANDOM;
+
+import java.util.HashMap;
+
+import org.deegree.rendering.r2d.se.unevaluated.Symbolizer;
+
 /**
  * <code>RasterStyling</code>
  * 
@@ -46,10 +52,131 @@ package org.deegree.rendering.r2d.styling;
  */
 public class RasterStyling implements Copyable<RasterStyling>, Styling {
 
-    public RasterStyling copy() {
-        return new RasterStyling();
+    /** Default is 1. */
+    public double opacity = 1;
+
+    /***/
+    public String redChannel;
+
+    /***/
+    public String greenChannel;
+
+    /***/
+    public String blueChannel;
+
+    /***/
+    public String grayChannel;
+
+    /***/
+    public HashMap<String, ContrastEnhancement> channelContrastEnhancements;
+
+    /** Default is RANDOM. */
+    public Overlap overlap = RANDOM;
+
+    // TODO colormap, function stuff
+
+    /** Default is no contrast enhancement. */
+    public ContrastEnhancement contrastEnhancement;
+
+    /** Default is no shaded relief. */
+    public ShadedRelief shaded;
+
+    /** Default is no image outline (should be line or polygon parameterized). */
+    public Symbolizer<?> imageOutline;
+
+    /**
+     * <code>ShadedRelief</code>
+     * 
+     * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
+     * @author last edited by: $Author$
+     * 
+     * @version $Revision$, $Date$
+     */
+    public static class ShadedRelief implements Copyable<ShadedRelief> {
+        /** Default is false. */
+        public boolean brightnessOnly;
+
+        /** Default is 55. */
+        public double reliefFactor = 55;
+
+        public ShadedRelief copy() {
+            ShadedRelief copy = new ShadedRelief();
+
+            copy.brightnessOnly = brightnessOnly;
+            copy.reliefFactor = reliefFactor;
+
+            return copy;
+        }
     }
 
-    // TODO
+    /**
+     * <code>ContrastEnhancement</code>
+     * 
+     * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
+     * @author last edited by: $Author$
+     * 
+     * @version $Revision$, $Date$
+     */
+    public static class ContrastEnhancement implements Copyable<ContrastEnhancement> {
+        /***/
+        public boolean normalize;
+
+        /***/
+        public boolean histogram;
+
+        /** Default is 1 == no gamma correction. */
+        public double gamma = 1;
+
+        public ContrastEnhancement copy() {
+            ContrastEnhancement copy = new ContrastEnhancement();
+
+            copy.normalize = normalize;
+            copy.histogram = histogram;
+            copy.gamma = gamma;
+
+            return copy;
+        }
+    }
+
+    /**
+     * <code>Overlap</code>
+     * 
+     * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
+     * @author last edited by: $Author$
+     * 
+     * @version $Revision$, $Date$
+     */
+    public static enum Overlap {
+        /** */
+        LATEST_ON_TOP,
+        /** */
+        EARLIEST_ON_TOP,
+        /** */
+        AVERAGE,
+        /** */
+        RANDOM
+    }
+
+    public RasterStyling copy() {
+        RasterStyling copy = new RasterStyling();
+
+        copy.opacity = opacity;
+        copy.redChannel = redChannel;
+        copy.greenChannel = greenChannel;
+        copy.blueChannel = blueChannel;
+        if ( channelContrastEnhancements != null ) {
+            copy.channelContrastEnhancements = new HashMap<String, ContrastEnhancement>();
+            for ( String chan : channelContrastEnhancements.keySet() ) {
+                copy.channelContrastEnhancements.put( chan, channelContrastEnhancements.get( chan ).copy() );
+            }
+        }
+        copy.overlap = overlap;
+        copy.contrastEnhancement = contrastEnhancement == null ? null : contrastEnhancement.copy();
+        copy.shaded = shaded == null ? null : shaded.copy();
+        // should be able to share the symbolizers:
+        copy.imageOutline = imageOutline;
+
+        return copy;
+    }
 
 }
