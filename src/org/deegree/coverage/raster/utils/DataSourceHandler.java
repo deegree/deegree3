f9d7796fea5b3c0c10831a9a1526d66cd5fc5adc
Andreas Schmitz <schmitz@occamlabs.de>
2009-08-20 09:07:22 +0000

    topic: class added module: commons description: Added class that builds rasters from datasources.

---
+++ src/org/deegree/coverage/raster/utils/DataSourceHandler.java
@@ -0,0 +1,114 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.coverage.raster.utils;
+
+import static org.deegree.coverage.raster.utils.RasterBuilder.buildTiledRaster;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import javax.xml.bind.JAXBElement;
+
+import org.deegree.commons.datasource.configuration.AbstractGeospatialDataSourceType;
+import org.deegree.commons.datasource.configuration.MultiResolutionDataSource;
+import org.deegree.commons.datasource.configuration.RasterDataSource;
+import org.deegree.commons.datasource.configuration.RasterFileSetType;
+import org.deegree.commons.datasource.configuration.MultiResolutionDataSource.Resolution;
+import org.deegree.commons.xml.XMLAdapter;
+import org.deegree.coverage.raster.AbstractRaster;
+import org.deegree.coverage.raster.MultiResolutionRaster;
+import org.deegree.crs.CRS;
+import org.slf4j.Logger;
+
+/**
+ * <code>DataSourceHandler</code>
+ * 
+ * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ */
+public class DataSourceHandler {
+
+    private static final Logger LOG = getLogger( DataSourceHandler.class );
+
+    /**
+     * @param datasource
+     * @param adapter
+     * @return a corresponding raster
+     */
+    public static MultiResolutionRaster fromDatasource( MultiResolutionDataSource datasource, XMLAdapter adapter ) {
+        CRS crs = new CRS( datasource.getCrs() );
+        MultiResolutionRaster mrr = new MultiResolutionRaster();
+        mrr.setCoordinateSystem( crs );
+        for ( Resolution resolution : datasource.getResolution() ) {
+            JAXBElement<? extends AbstractGeospatialDataSourceType> dsElement = resolution.getAbstractGeospatialDataSource();
+            RasterDataSource ds = (RasterDataSource) dsElement.getValue();
+            RasterFileSetType directory = ds.getRasterDirectory();
+            File resolutionDir;
+            try {
+                resolutionDir = new File( adapter.resolve( directory.getValue() ).getFile() );
+                AbstractRaster rasterLevel = buildTiledRaster( resolutionDir, directory.getFilePattern(),
+                                                               directory.isRecursive(), crs );
+                // double res = RasterBuilder.getPixelResolution( resolution.getRes(), resolutionDir );
+                mrr.addRaster( rasterLevel );
+            } catch ( MalformedURLException e ) {
+                LOG.warn( "Could not resolve the file {}, corresponding data will not be available.",
+                          directory.getValue() );
+            }
+        }
+        return mrr;
+    }
+
+    /**
+     * @param datasource
+     * @param adapter
+     * @return a corresponding raster, null if files could not be fund
+     */
+    public static AbstractRaster fromDatasource( RasterDataSource datasource, XMLAdapter adapter ) {
+        CRS crs = new CRS( datasource.getCrs() );
+        RasterFileSetType directory = datasource.getRasterDirectory();
+        try {
+            File rasterFiles = new File( adapter.resolve( directory.getValue() ).getFile() );
+            return buildTiledRaster( rasterFiles, directory.getFilePattern(), directory.isRecursive(), crs );
+        } catch ( MalformedURLException e ) {
+            LOG.warn( "Could not resolve the file {}, corresponding data will not be available.", directory.getValue() );
+        }
+        return null;
+    }
+
+}
