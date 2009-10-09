1cf756138c4f7151f14b0676d914ffc8414ef8b7
Andrei Ionita <ionita@lat-lon.de>
2009-10-09 09:35:09 +0000

    topic: code enhanced type: functionality module: d3_core description: fixed the resolving of features that have no namespace (in WFS GetFeature)

--- src/org/deegree/protocol/wfs/getfeature/TypeName.java
+++ src/org/deegree/protocol/wfs/getfeature/TypeName.java
@@ -2,9 +2,9 @@
 /*----------------------------------------------------------------------------
  This file is part of deegree, http://deegree.org/
  Copyright (C) 2001-2009 by:
-   Department of Geography, University of Bonn
+ Department of Geography, University of Bonn
  and
-   lat/lon GmbH
+ lat/lon GmbH
 
  This library is free software; you can redistribute it and/or modify it under
  the terms of the GNU Lesser General Public License as published by the Free
@@ -32,7 +32,7 @@
  http://www.geographie.uni-bonn.de/deegree/
 
  e-mail: info@deegree.org
-----------------------------------------------------------------------------*/
+ ----------------------------------------------------------------------------*/
 
 package org.deegree.protocol.wfs.getfeature;
 
@@ -45,10 +45,10 @@ import org.apache.axiom.om.OMElement;
 
 /**
  * A feature type name with an optional alias, as it may be used in WFS 1.1.0 or 2.0.0 queries.
- *
+ * 
  * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
  * @author last edited by: $Author: schneider $
- *
+ * 
  * @version $Revision: $, $Date: $
  */
 public class TypeName {
@@ -59,7 +59,7 @@ public class TypeName {
 
     /**
      * Creates a new {@link TypeName} with optional alias.
-     *
+     * 
      * @param ftName
      *            name of the feature, must not be null
      * @param alias
@@ -72,42 +72,49 @@ public class TypeName {
         this.ftName = ftName;
         this.alias = alias;
     }
-    
+
     /**
-     * Extracts an array of {@link TypeNames} from the TypeNameList string,
-     * whose pattern is ((\w:)?\w(=\w)?){1,}.
-     * Example: typeName="ns1:Inwatera_1m=A, ns2:CoastL_1M=B"
-     * where A is an alias for ns1:Inwatera_1m and B is an alias for ns2:CoastL_1M.
-     * (taken from http://schemas.opengis.net/wfs/1.1.0/wfs.xsd )
+     * Extracts an array of {@link TypeNames} from the TypeNameList string, whose pattern is ((\w:)?\w(=\w)?){1,}.
+     * Example: typeName="ns1:Inwatera_1m=A, ns2:CoastL_1M=B" where A is an alias for ns1:Inwatera_1m and B is an alias
+     * for ns2:CoastL_1M. (taken from http://schemas.opengis.net/wfs/1.1.0/wfs.xsd )
      * 
-     * @param context   the query element in which the TypeNameList attribute is defined,
-     *                  used to resolve the namespaceURIs for the prefixes 
-     * @param typeNameStr   the string that will be parsed
-     * @return  an array of {@link TypeName}
+     * @param context
+     *            the query element in which the TypeNameList attribute is defined, used to resolve the namespaceURIs
+     *            for the prefixes
+     * @param typeNameStr
+     *            the string that will be parsed
+     * @return an array of {@link TypeName}
      */
-    public static TypeName[] valuesOf( OMElement context, String typeNameStr ) {        
+    public static TypeName[] valuesOf( OMElement context, String typeNameStr ) {
         List<TypeName> resultList = new ArrayList<TypeName>();
-        
+
         String[] typeNameComma = typeNameStr.split( "," );
         for ( int i = 0; i < typeNameComma.length; i++ ) {
             String[] typeNameEqual = typeNameComma[i].split( "=" );
             if ( typeNameEqual.length == 2 ) {
-                resultList.add( new TypeName( context.resolveQName( typeNameEqual[0] ), typeNameEqual[1] ) );                                
-            } else if ( typeNameEqual.length == 1 ) { //no alias
-                resultList.add( new TypeName( context.resolveQName( typeNameEqual[0] ), null ) );
+                resultList.add( new TypeName( resolveQName( context, typeNameEqual[0] ), typeNameEqual[1] ) );
+            } else if ( typeNameEqual.length == 1 ) { // no alias
+                resultList.add( new TypeName( resolveQName( context, typeNameEqual[0] ), null ) );
             } else {
-                //TODO find a suitable exception
+                // TODO find a suitable exception
                 System.err.println( "More than one equal sign(=) in the declaration of TypeNameList" );
             }
         }
-        
-        TypeName[] resultArray = new TypeName[ resultList.size() ];
+
+        TypeName[] resultArray = new TypeName[resultList.size()];
         return resultList.toArray( resultArray );
+    }
+
+    private static QName resolveQName( OMElement context, String name ) {
+        if ( name.indexOf( ":" ) != -1 ) {
+            return context.resolveQName( name );
+        }
+        return new QName( name );
     }
 
     /**
      * Returns the feature type name.
-     *
+     * 
      * @return the feature type name, never null
      */
     public QName getFeatureTypeName() {
@@ -116,7 +123,7 @@ public class TypeName {
 
     /**
      * Returns the alias for the feature type.
-     *
+     * 
      * @return the alias for the feature type, or null if it has none
      */
     public String getAlias() {
