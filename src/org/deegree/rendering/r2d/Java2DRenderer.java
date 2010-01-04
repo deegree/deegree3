7f37f186402f755d0723026fdb5d49b334ea4941
Andreas Schmitz <schmitz@occamlabs.de>
2010-01-04 11:05:45 +0000

    topic: bug fix type: minor module: commons description: Fixed anchor points when rendering marks.

--- src/org/deegree/rendering/r2d/Java2DRenderer.java
+++ src/org/deegree/rendering/r2d/Java2DRenderer.java
@@ -376,12 +376,13 @@ public class Java2DRenderer implements Renderer {
 
         if ( g.image == null ) {
             renderMark( g.mark, g.size < 0 ? 6 : round( considerUOM( g.size, styling.uom ) ), styling.uom, this,
-                        rect.x, rect.y, g.rotation );
+                        rect.getCenterX(), rect.getCenterY(), g.rotation );
             return;
         }
 
         if ( g.image != null ) {
-            graphics.drawImage( g.image, round( rect.x ), round( rect.y ), round( rect.width ), round( rect.height ), null );
+            graphics.drawImage( g.image, round( rect.x ), round( rect.y ), round( rect.width ), round( rect.height ),
+                                null );
         }
     }
 
