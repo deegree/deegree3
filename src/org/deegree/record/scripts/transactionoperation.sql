91356c76ee6d8780a9fafa85309d4e17ba043a7f
Steffen Thomas <thomas@lat-lon.de>
2010-01-04 17:19:59 +0000

    topic: other resources type: $select -> sql script module: framework description: some useful queries added

---
+++ src/org/deegree/record/scripts/transactionoperation.sql
@@ -0,0 +1,25 @@
+select * 
+from datasets, recordbrief
+where datasets.id = recordbrief.fk_datasets
+
+select * 
+from recordbrief
+
+INSERT INTO userdefinedqueryableproperties VALUES (7);
+
+INSERT INTO datasets VALUES (7,null,null,'','',null,FALSE,'','', '', null);
+
+insert into recordbrief (fk_datasets, format, data)
+values (7, 1, 'this is a test...');
+
+
+
+--to delete the inserted dataset
+delete from recordbrief 
+where fk_datasets = 7;
+
+delete from datasets 
+where id = 7;
+
+delete from userdefinedqueryableproperties 
+where fk_datasets = 7;
