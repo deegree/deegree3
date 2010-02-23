c7f452f6dafb626c0dd4ed7768cd702c16b71690
Andreas Schmitz <schmitz@occamlabs.de>
2010-02-23 08:10:32 +0000

    topic: bug fix type: minor module: commons description: Fixed symbol drawing to conform with visual tests, again...

--- src/org/deegree/rendering/r2d/Java2DRenderer.java
+++ src/org/deegree/rendering/r2d/Java2DRenderer.java
@@ -376,7 +376,7 @@ public class Java2DRenderer implements Renderer {
 
         if ( g.image == null ) {
             renderMark( g.mark, g.size < 0 ? 6 : round( considerUOM( g.size, styling.uom ) ), styling.uom, this,
-                        rect.getMaxX(), rect.getCenterY(), g.rotation );
+                        rect.getMinX(), rect.getMinY(), g.rotation );
             return;
         }
 
