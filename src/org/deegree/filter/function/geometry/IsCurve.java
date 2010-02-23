b329937e335a30bb70a6e6d74587a4494448d698
Andreas Schmitz <schmitz@occamlabs.de>
2010-02-23 10:12:44 +0000

    topic: code enhanced type: stability, flexibility module: commons description: Also return true when Multi* variants are encountered. Need to think more about this.

--- src/org/deegree/filter/function/geometry/IsCurve.java
+++ src/org/deegree/filter/function/geometry/IsCurve.java
@@ -7,6 +7,8 @@ import org.deegree.filter.Expression;
 import org.deegree.filter.FilterEvaluationException;
 import org.deegree.filter.MatchableObject;
 import org.deegree.filter.expression.Function;
+import org.deegree.geometry.multi.MultiCurve;
+import org.deegree.geometry.multi.MultiLineString;
 import org.deegree.geometry.primitive.Curve;
 
 /**
@@ -30,7 +32,11 @@ public class IsCurve extends Function {
     public Object[] evaluate( MatchableObject f )
                             throws FilterEvaluationException {
         Object[] vals = getParams()[0].evaluate( f );
-        return new Object[] { new Boolean( vals != null && vals.length > 0 && vals[0] instanceof Curve ).toString() };
+        // TODO is handling of multi geometries like this ok?
+        boolean result = vals != null
+                         && vals.length > 0
+                         && ( vals[0] instanceof Curve || vals[0] instanceof MultiCurve || vals[0] instanceof MultiLineString );
+        return new Object[] { new Boolean( result ).toString() };
     }
 
 }
