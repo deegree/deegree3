7742e8b589e36c302abeeeb3fab510ef9c60fae6
Andreas Schmitz <schmitz@occamlabs.de>
2010-03-17 13:07:05 +0000

    topic: code enhanced type: stability module: commons description: Do not fail parsing when unknown well known name is specified for marks.

--- src/org/deegree/rendering/r2d/se/parser/SymbologyParser.java
+++ src/org/deegree/rendering/r2d/se/parser/SymbologyParser.java
@@ -442,7 +442,13 @@ public class SymbologyParser {
             }
 
             if ( in.getLocalName().equals( "WellKnownName" ) ) {
-                base.wellKnown = SimpleMark.valueOf( in.getElementText().toUpperCase() );
+                String wkn = in.getElementText();
+                try {
+                    base.wellKnown = SimpleMark.valueOf( wkn.toUpperCase() );
+                } catch ( IllegalArgumentException e ) {
+                    LOG.warn( "Specified unsupported WellKnownName of '{}', using square instead.", wkn );
+                    base.wellKnown = SimpleMark.SQUARE;
+                }
             } else
                 sym: if ( in.getLocalName().equals( "OnlineResource" ) || in.getLocalName().equals( "InlineContent" ) ) {
                     LOG.debug( "Loading mark from external file." );
