11bcee53fb86c28bc3b50db8abe92cdf648ea0b8
Andreas Schmitz <schmitz@occamlabs.de>
2010-04-29 09:38:10 +0000

    removed obsolete files

--- deegree-core/resources/js/wmsclient.js
+++
@@ -1,8 +0,0 @@
-load('common.js')
-
-var Client = top.protocol.wms.client.WMSClient111
-var xml = top.commons.xml.XMLAdapter
-var capas = xml(java.net.URL("http://demo.deegree.org/deegree-wms/services?request=capabilities&service=WMS"))
-// var capaslocal = xml(java.io.File("/tmp/test.xml"))
-var client = Client(capas)
-var r = Client.Requests
