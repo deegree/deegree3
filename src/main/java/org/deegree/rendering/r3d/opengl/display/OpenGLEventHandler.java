2d9727c4da9ef87476809b97a9392de16f49461c
Steffen Thomas <thomas@lat-lon.de>
2010-06-16 15:46:54 +0000

    topic: comment added/corrected description: OpenGLEventHandler

--- src/main/java/org/deegree/rendering/r3d/opengl/display/OpenGLEventHandler.java
+++ src/main/java/org/deegree/rendering/r3d/opengl/display/OpenGLEventHandler.java
@@ -201,10 +201,11 @@ public class OpenGLEventHandler implements GLEventListener {
     }
 
     /**
-     * Add the given branch group to the scene and set the appropriate distance etc. After adding the branch group to
+     * Adds the given branch group to the scene and set the appropriate distance etc. After adding the branch group to
      * the rotation group which is controlled by the mouse rotator.
      * 
      * @param b
+     *            a list of WorldRenderableObjects, can be <Code>null</Code>.
      */
     public void addDataObjectToScene( List<WorldRenderableObject> w ) {
         if ( w != null ) {
