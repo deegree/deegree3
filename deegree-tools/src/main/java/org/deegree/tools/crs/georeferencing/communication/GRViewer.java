9bbf4f242acc782160ec2d4d4130355512d61643
Markus Schneider <schneider@occamlabs.de>
2010-06-22 16:06:11 +0000

    Tagging ${RELEASE_VERSION}

---
+++ deegree-tools/src/main/java/org/deegree/tools/crs/georeferencing/communication/GRViewer.java
@@ -0,0 +1,72 @@
+//$HeadURL$$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+
+package org.deegree.tools.crs.georeferencing.communication;
+
+import java.io.IOException;
+
+import org.deegree.tools.crs.georeferencing.application.Controller;
+import org.deegree.tools.crs.georeferencing.model.Scene2D;
+import org.deegree.tools.crs.georeferencing.model.Scene2DImplWMS;
+
+/**
+ * 
+ * Main class that calls all the relevant objects to build the georeferencing viewer.
+ * 
+ * @author <a href="mailto:thomas@lat-lon.de">Steffen Thomas</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ */
+public class GRViewer {
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main( String[] args )
+                            throws IOException {
+        GRViewerGUI gui = new GRViewerGUI();
+
+        Scene2D scene2d = new Scene2DImplWMS();
+
+        new Controller( gui, scene2d );
+
+        gui.setVisible( true );
+
+    }
+
+}
