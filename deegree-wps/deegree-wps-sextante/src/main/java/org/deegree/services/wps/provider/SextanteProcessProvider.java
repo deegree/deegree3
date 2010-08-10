529a5b47c568efc3353b4aa0030b3ad57ac75335
Markus Schneider <schneider@occamlabs.de>
2010-08-10 10:02:19 +0000

    Initializing module.

---
+++ deegree-wps/deegree-wps-sextante/src/main/java/org/deegree/services/wps/provider/SextanteProcessProvider.java
@@ -0,0 +1,111 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.services.wps.provider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.deegree.commons.tom.ows.CodeType;
+import org.deegree.services.exception.ServiceInitException;
+import org.deegree.services.wps.WPSProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link ProcessProvider} that provides <a
+ * href="http://www.osor.eu/studies/sextante-a-geographic-information-system-for-the-spanish-region-of-extremadura"
+ * >Sextante</a> processes.
+ * 
+ * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ */
+public class SextanteProcessProvider implements ProcessProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger( SextanteProcessProvider.class );
+
+    private final Map<CodeType, WPSProcess> idToProcess = new HashMap<CodeType, WPSProcess>();
+
+    /**
+     * 
+     */
+    SextanteProcessProvider() {
+    }
+
+    @Override
+    public void init()
+                            throws ServiceInitException {
+//        for ( ProcessDefinition processDefinition : processDefs ) {
+//            CodeType processId = new CodeType( processDefinition.getIdentifier().getValue(),
+//                                               processDefinition.getIdentifier().getCodeSpace() );
+//            String className = processDefinition.getJavaClass();
+//            try {
+//                LOG.info( "Initializing process with id '" + processId + "'" );
+//                LOG.info( "- process class: " + className );
+//                Processlet processlet = (Processlet) Class.forName( className ).newInstance();
+//                processlet.init();
+//
+//                ExceptionCustomizer customizer = null;
+//                if ( processlet instanceof ExceptionAwareProcesslet ) {
+//                    customizer = ( (ExceptionAwareProcesslet) processlet ).getExceptionCustomizer();
+//                }
+//                WPSProcess process = new WPSProcess( processDefinition, processlet, customizer );
+//                idToProcess.put( processId, process );
+//            } catch ( Exception e ) {
+//                String msg = "Could not create process instance. Class name ('" + className
+//                             + "') was not found on the classpath. "
+//                             + "Hint: spelling in configuration file might be incorrect.";
+//                throw new ServiceInitException( msg, e );
+//            }
+//        }
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public WPSProcess getProcess( CodeType id ) {
+        return idToProcess.get( id );
+    }
+
+    @Override
+    public Map<CodeType, WPSProcess> getProcesses() {
+        return idToProcess;
+    }
+}
