51ca3e726aa2e0d85725f6f8f2bc6e026e62ae01
Andreas Schmitz <schmitz@occamlabs.de>
2010-08-11 14:45:31 +0000

    unused scripts

--- deegree-test/scripts/batch/d3tests.bat
+++
@@ -1,3 +0,0 @@
-REM SET LD_LIBRARY_PATH=
-REM SET JAVA_OPTS=-Xmx1000M -XX:MaxDirectMemorySize=500M
-java %JAVA_OPTS% -cp deegree-tests-3.0-pre.jar:. org.deegree.test.TestingToolBox %*
--- deegree-test/scripts/shell/d3tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-OLDIR=`pwd`
-cd `dirname $0`
-
-java -cp deegree-test-3.0-pre.jar:. org.deegree.test.TestingToolBox "$@"
-cd $OLDIR
