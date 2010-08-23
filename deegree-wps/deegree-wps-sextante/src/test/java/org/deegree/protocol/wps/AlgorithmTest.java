100e9dfe1999e617c13790dd750f8205f0d12f9e
Jens Pabel <pabel@lat-lon.de>
2010-08-23 14:43:21 +0000

    Added switch for deactivating tests.

--- deegree-wps/deegree-wps-sextante/src/test/java/org/deegree/protocol/wps/AlgorithmTest.java
+++ deegree-wps/deegree-wps-sextante/src/test/java/org/deegree/protocol/wps/AlgorithmTest.java
@@ -24,100 +24,105 @@ import es.unex.sextante.parameters.Parameter;
 
 public class AlgorithmTest {
 
+    private static final boolean ENABLED = false;
+
     private static Logger LOG = LoggerFactory.getLogger( AlgorithmTest.class );
 
     @Test
     public void testVectorLayerAlgorithms()
                             throws OWSException, IOException, XMLStreamException {
-        try {
-            URL wpsURL = new URL(
-                                  "http://localhost:8080/deegree-wps-demo/services?service=WPS&version=1.0.0&request=GetCapabilities" );
+        if ( ENABLED ) {
+            try {
+                URL wpsURL = new URL(
+                                      "http://localhost:8080/deegree-wps-demo/services?service=WPS&version=1.0.0&request=GetCapabilities" );
 
-            WPSClient client = new WPSClient( wpsURL );
+                WPSClient client = new WPSClient( wpsURL );
 
-            Assert.assertNotNull( client );
+                Assert.assertNotNull( client );
 
-            File gmlPoints = new File( WPSClientTest.class.getResource( "GML31_MultiPoint.xml" ).getPath() );
-            File gmlLines = new File( WPSClientTest.class.getResource( "GML31_MultiLineString.xml" ).getPath() );
-            File gmlPolygons = new File( WPSClientTest.class.getResource( "GML31_MultiPolygon.xml" ).getPath() );
+                File gmlPoints = new File( WPSClientTest.class.getResource( "GML31_MultiPoint.xml" ).getPath() );
+                File gmlLines = new File( WPSClientTest.class.getResource( "GML31_MultiLineString.xml" ).getPath() );
+                File gmlPolygons = new File( WPSClientTest.class.getResource( "GML31_MultiPolygon.xml" ).getPath() );
 
-            // Process process = client.getProcess( "centroids", null );
-            // ProcessExecution execution = process.prepareExecution();
-            // execution.addXMLInput( "LAYER", null, gmlGeometry.toURI().toURL(), false, "text/xml", "UTF-8",
-            // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
-            //
-            // execution.addOutput( "RESULT", null, null, false, "text/xml", "UTF-8",
-            // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
-            //
-            // ExecutionOutputs response = execution.execute();
-            // Assert.assertNotNull( response );
+                // Process process = client.getProcess( "centroids", null );
+                // ProcessExecution execution = process.prepareExecution();
+                // execution.addXMLInput( "LAYER", null, gmlGeometry.toURI().toURL(), false, "text/xml", "UTF-8",
+                // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+                //
+                // execution.addOutput( "RESULT", null, null, false, "text/xml", "UTF-8",
+                // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+                //
+                // ExecutionOutputs response = execution.execute();
+                // Assert.assertNotNull( response );
 
-            GeoAlgorithm[] algs = SextanteProcessProvider.getVectorLayerInAndOutAlgorithms();
+                GeoAlgorithm[] algs = SextanteProcessProvider.getVectorLayerInAndOutAlgorithms();
 
-            // process all vector algorithms
-            for ( int i = 0; i < algs.length; i++ ) {
-                GeoAlgorithm alg = algs[i];
+                // process all vector algorithms
+                for ( int i = 0; i < algs.length; i++ ) {
+                    GeoAlgorithm alg = algs[i];
 
-                Process process = client.getProcess( alg.getCommandLineName(), null );
-                ProcessExecution execution = process.prepareExecution();
+                    Process process = client.getProcess( alg.getCommandLineName(), null );
+                    ProcessExecution execution = process.prepareExecution();
 
-                // add all inputs
-                ParametersSet paramSet = alg.getParameters();
-                for ( int j = 0; j < paramSet.getNumberOfParameters(); j++ ) {
-                    Parameter param = paramSet.getParameter( j );
+                    // add all inputs
+                    ParametersSet paramSet = alg.getParameters();
+                    for ( int j = 0; j < paramSet.getNumberOfParameters(); j++ ) {
+                        Parameter param = paramSet.getParameter( j );
 
-                    String inputIndentifier = param.getParameterName();
+                        String inputIndentifier = param.getParameterName();
 
-                    File geom = null;
+                        File geom = null;
 
-                    if ( inputIndentifier.equals( "POINTS" ) )
-                        geom = gmlPoints;
-                    else if ( inputIndentifier.equals( "LINES" ) )
-                        geom = gmlLines;
-                    else
-                        geom = gmlLines;
+                        if ( inputIndentifier.equals( "POINTS" ) )
+                            geom = gmlPoints;
+                        else if ( inputIndentifier.equals( "LINES" ) )
+                            geom = gmlLines;
+                        else
+                            geom = gmlLines;
 
-                    execution.addXMLInput( inputIndentifier, null, geom.toURI().toURL(), false, "text/xml", "UTF-8",
-                                           "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+                        execution.addXMLInput( inputIndentifier, null, geom.toURI().toURL(), false, "text/xml",
+                                               "UTF-8",
+                                               "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+                    }
+
+                    // set all output parameters
+                    OutputObjectsSet outputSet = alg.getOutputObjects();
+                    for ( int j = 0; j < outputSet.getOutputObjectsCount(); j++ ) {
+                        Output outp = outputSet.getOutput( j );
+
+                        String outputIdentifier = outp.getName();
+
+                        execution.addOutput( outputIdentifier, null, null, false, "text/xml", "UTF-8",
+                                             "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+
+                    }
+
+                    ExecutionOutputs outputs = execution.execute();
+
                 }
 
-                // set all output parameters
-                OutputObjectsSet outputSet = alg.getOutputObjects();
-                for ( int j = 0; j < outputSet.getOutputObjectsCount(); j++ ) {
-                    Output outp = outputSet.getOutput( j );
+                // execution
+                // .addXMLInput("GMLInput", null, gmlPoints.toURI().toURL(),
+                // false, "text/xml", null,
+                // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd");
 
-                    String outputIdentifier = outp.getName();
+                // execution.addOutput("CentroidSextante", null, null, true, null, null,
+                // null);
 
-                    execution.addOutput( outputIdentifier, null, null, false, "text/xml", "UTF-8",
-                                         "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd" );
+                // ExecutionOutputs outputs = execution.execute();
 
-                }
+                // access individual output values
+                // ComplexOutput outputGeometry = (ComplexOutput) outputs.get(
+                // "CentroidSextante", null);
 
-                ExecutionOutputs outputs = execution.execute();
+                // LiteralOutput out = outputs.getLiteral("Data", null);
 
+                // XMLStreamReader xmlStream = outputGeometry.getAsXMLStream();
+
+                // System.out.println(out.getValue());
+            } catch ( Throwable t ) {
+                LOG.error( t.getMessage(), t );
             }
-
-            // execution
-            // .addXMLInput("GMLInput", null, gmlPoints.toURI().toURL(),
-            // false, "text/xml", null,
-            // "http://schemas.opengis.net/gml/3.1.1/base/geometryComplexes.xsd");
-
-            // execution.addOutput("CentroidSextante", null, null, true, null, null,
-            // null);
-
-            // ExecutionOutputs outputs = execution.execute();
-
-            // access individual output values
-            // ComplexOutput outputGeometry = (ComplexOutput) outputs.get(
-            // "CentroidSextante", null);
-
-            // LiteralOutput out = outputs.getLiteral("Data", null);
-
-            // XMLStreamReader xmlStream = outputGeometry.getAsXMLStream();
-
-            // System.out.println(out.getValue());
-        } catch ( Throwable t ) {
-            LOG.error( t.getMessage(), t );
         }
     }
 }
