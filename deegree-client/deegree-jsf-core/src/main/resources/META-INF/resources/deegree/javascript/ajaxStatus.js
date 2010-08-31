de5024e39e955882f6ca9d7625bb5abf2d7f05cf
Lyn Goltz <goltz@lat-lon.de>
2010-08-31 08:42:23 +0000

    JSFCore  ajaxStatus / collapsiblePanel, changed layout - added id

--- deegree-client/deegree-jsf-core/src/main/resources/META-INF/resources/deegree/javascript/ajaxStatus.js
+++ deegree-client/deegree-jsf-core/src/main/resources/META-INF/resources/deegree/javascript/ajaxStatus.js
@@ -1,10 +1,16 @@
-function processAjaxUpdate(msgId) {
+function processAjaxUpdate(msgId, modal) {
 	function processEvent(data) {
 		var msg = document.getElementById(msgId);
-		if(msg != null){
+		if (msg != null) {
 			if (data.status == "begin") {
+				if (modal) {
+					document.body.style.cursor = 'wait'
+				}
 				msg.style.display = '';
 			} else if (data.status == "success") {
+				if (modal) {
+					document.body.style.cursor = 'auto'
+				}
 				msg.style.display = 'none';
 			}
 		}
@@ -12,6 +18,6 @@ function processAjaxUpdate(msgId) {
 	return processEvent;
 };
 
-function registerAjaxStatus(msgId) {
-	jsf.ajax.addOnEvent(processAjaxUpdate(msgId));
+function registerAjaxStatus(msgId, modal) {
+	jsf.ajax.addOnEvent(processAjaxUpdate(msgId, modal));
 }
