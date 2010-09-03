e4bfe1cfad954a4faec4f9c84aff774892ff4da9
Andreas Schmitz <schmitz@occamlabs.de>
2010-09-03 14:17:00 +0000

    Tagging ${RELEASE_VERSION}

---
+++ deegree-tools/src/main/java/org/deegree/tools/crs/georeferencing/application/RasterOptions.java
@@ -0,0 +1,136 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.tools.crs.georeferencing.application;
+
+import org.deegree.coverage.raster.io.RasterIOOptions;
+
+/**
+ * Stores the parameter for a getMap() request to a WMS.
+ * 
+ * @author <a href="mailto:thomas@lat-lon.de">Steffen Thomas</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ */
+public class RasterOptions {
+
+    private RasterIOOptions options;
+
+    private static final String RASTERIO_LAYER = "RASTERIO_LAYER";
+
+    private static final String RASTER_FORMATLIST = "RASTER_FORMATLIST";
+
+    private static final String RASTER_URL = "RASTER_URL";
+
+    private static final String RIO_WMS_SYS_ID = "RASTERIO_WMS_SYS_ID";
+
+    private static final String RIO_WMS_MAX_SCALE = "RASTERIO_WMS_MAX_SCALE";
+
+    private static final String RIO_WMS_DEFAULT_FORMAT = "RASTERIO_WMS_DEFAULT_FORMAT";
+
+    private static final String RIO_WMS_MAX_WIDTH = "RASTERIO_WMS_MAX_WIDTH";
+
+    private static final String RIO_WMS_MAX_HEIGHT = "RASTERIO_WMS_MAX_HEIGHT";
+
+    private static final String RIO_WMS_LAYERS = "RASTERIO_WMS_REQUESTED_LAYERS";
+
+    private static final String RIO_WMS_ENABLE_TRANSPARENT = "RASTERIO_WMS_ENABLE_TRANSPARENCY";
+
+    private static final String RIO_WMS_TIMEOUT = "RIO_WMS_TIMEOUT";
+
+    private static final String LEFT_LOWER_X = "LEFT_LOWER_X";
+
+    private static final String LEFT_LOWER_Y = "LEFT_LOWER_Y";
+
+    private static final String RIGHT_UPPER_X = "RIGHT_UPPER_X";
+
+    private static final String RIGHT_UPPER_Y = "RIGHT_UPPER_Y";
+
+    /**
+     * Specifies the size of the full drawn side.
+     */
+    private static final String RESOLUTION = "RESOLUTION";
+
+    public RasterOptions( ParameterStore store ) {
+        // options = new RasterIOOptions();
+        //
+        // options.add( RasterIOOptions.CRS, store.getCRS() );
+        // options.add( RIO_WMS_DEFAULT_FORMAT, store.getFormat() );
+        // options.add( RIO_WMS_LAYERS, store.getLayers() );
+        // options.add( LEFT_LOWER_X, store.getLEFT_LOWER_X() );
+        // options.add( LEFT_LOWER_Y, store.getLEFT_LOWER_Y() );
+        // options.add( RIGHT_UPPER_X, store.getRIGHT_UPPER_X() );
+        // options.add( RIGHT_UPPER_Y, store.getRIGHT_UPPER_Y() );
+        //
+        // // options.add( RasterIOOptions.CRS, "EPSG:4326" );
+        // // options.add( RIO_WMS_DEFAULT_FORMAT, "image/png" );
+        // // options.add( RIO_WMS_LAYERS, "cite:Lakes" );
+        // // options.add( LEFT_LOWER_X, "6.0E-4" );
+        // // options.add( LEFT_LOWER_Y, "-0.0018" );
+        // // options.add( RIGHT_UPPER_X, "0.0031" );
+        // // options.add( RIGHT_UPPER_Y, "-1.0E-4" );
+        //
+        // // (minX, minY, maxX, maxY) -> 2568720.0,5629890.0,2568800.0,5629970.0
+        // // options.add( RasterIOOptions.CRS, "EPSG:31466" );
+        // // options.add( RIO_WMS_DEFAULT_FORMAT, "image/png" );
+        // // options.add( RIO_WMS_LAYERS, "DTK" );
+        // // options.add( LEFT_LOWER_X, "2568720.0" );
+        // // options.add( LEFT_LOWER_Y, "5629890.0" );
+        // // options.add( RIGHT_UPPER_X, "2568800.0" );
+        // // options.add( RIGHT_UPPER_Y, "5629970.0" );
+        //
+        // // options.add( RasterIOOptions.CRS, "EPSG:32618" );
+        // // options.add( RIO_WMS_DEFAULT_FORMAT, "image/jpeg" );
+        // // options.add( RIO_WMS_LAYERS, "populationgrid" );
+        //
+        // options.add( RESOLUTION, "1.0" );
+        // options.add( RASTER_URL, store.getMapURL() );
+        // options.add( RasterIOOptions.OPT_FORMAT, "WMS_111" );
+        // options.add( RIO_WMS_SYS_ID, store.getMapURL() );
+        // options.add( RIO_WMS_MAX_SCALE, "0.1" );
+        //
+        // // specify the quality
+        // options.add( RIO_WMS_MAX_WIDTH, store.getQORX() );
+        // options.add( RIO_WMS_MAX_HEIGHT, store.getQORY() );
+        // options.add( RIO_WMS_ENABLE_TRANSPARENT, "true" );
+        // // options.add( RIO_WMS_TIMEOUT, "1000" );
+    }
+
+    public RasterIOOptions getOptions() {
+        return options;
+    }
+
+}
---
+++ deegree-tools/src/main/java/org/deegree/tools/crs/georeferencing/communication/GRViewerGUI.java
@@ -0,0 +1,294 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.tools.crs.georeferencing.communication;
+
+import static org.deegree.tools.crs.georeferencing.communication.GUIConstants.MENUITEM_EDIT_OPTIONS;
+import static org.deegree.tools.crs.georeferencing.communication.GUIConstants.MENU_EDIT;
+import static org.deegree.tools.crs.georeferencing.communication.GUIConstants.MENU_FILE;
+import static org.deegree.tools.crs.georeferencing.communication.GUIConstants.MENU_TRANSFORMATION;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentListener;
+import java.awt.image.BufferedImage;
+
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+import org.deegree.rendering.r3d.opengl.display.OpenGLEventHandler;
+import org.deegree.tools.crs.georeferencing.communication.checkboxlist.CheckboxListTransformation;
+import org.deegree.tools.crs.georeferencing.communication.navigationbar.NavigationBarPanelFootprint;
+import org.deegree.tools.crs.georeferencing.communication.navigationbar.NavigationBarPanelGeoref;
+import org.deegree.tools.crs.georeferencing.communication.panel2D.BuildingFootprintPanel;
+import org.deegree.tools.crs.georeferencing.communication.panel2D.Scene2DPanel;
+
+/**
+ * The <Code>GRViewerGUI</Code> class provides the client to view georeferencing tools/windows.
+ * 
+ * @author <a href="mailto:thomas@lat-lon.de">Steffen Thomas</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ */
+public class GRViewerGUI extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private final static Dimension SUBCOMPONENT_DIMENSION = new Dimension( 1, 1 );
+
+    private Scene2DPanel scenePanel2D;
+
+    private JPanel panelWest;
+
+    private JPanel panelEast;
+
+    private OpenGLEventHandler openGLEventListener;
+
+    private BuildingFootprintPanel footprintPanel;
+
+    private JMenuItem editMenuItem;
+
+    private JMenuItem openShape;
+
+    private JMenuItem openWMS;
+
+    private JMenuItem openBuilding;
+
+    private NavigationBarPanelGeoref naviPanelGeoref;
+
+    private NavigationBarPanelFootprint naviPanelFoot;
+
+    private JMenu menuTransformation;
+
+    private CheckboxListTransformation list;
+
+    public GRViewerGUI() {
+        super( GUIConstants.WINDOW_TITLE );
+
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        GridBagLayout gbl = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbl.setConstraints( this, gbc );
+
+        setLayout( gbl );
+        setMinimumSize( GUIConstants.FRAME_DIMENSION );
+
+        setPreferredSize( GUIConstants.FRAME_DIMENSION );
+
+        setupMenubar();
+        setup2DScene( gbl );
+        setupPanelFootprint( gbl );
+        setupOpenGL( gbl, false );
+        this.pack();
+    }
+
+    private void setupMenubar() {
+
+        JMenuBar menuBar;
+        JMenu menuFile;
+        JMenu menuEdit;
+
+        menuBar = new JMenuBar();
+        menuFile = new JMenu( MENU_FILE );
+        menuEdit = new JMenu( MENU_EDIT );
+        menuTransformation = new JMenu( MENU_TRANSFORMATION );
+
+        menuBar.add( menuFile );
+        menuBar.add( menuEdit );
+        menuBar.add( menuTransformation );
+
+        openShape = new JMenuItem( GUIConstants.MENUITEM_OPEN_SHAPEFILE );
+        openShape.setName( GUIConstants.MENUITEM_OPEN_SHAPEFILE );
+        openWMS = new JMenuItem( GUIConstants.MENUITEM_OPEN_WMS_LAYER );
+        openWMS.setName( GUIConstants.MENUITEM_OPEN_WMS_LAYER );
+        openBuilding = new JMenuItem( GUIConstants.MENUITEM_OPEN_BUILDING );
+        openBuilding.setName( GUIConstants.MENUITEM_OPEN_BUILDING );
+
+        menuFile.add( openShape );
+        menuFile.add( openWMS );
+        menuFile.add( openBuilding );
+
+        editMenuItem = new JMenuItem( MENUITEM_EDIT_OPTIONS );
+        menuEdit.add( editMenuItem );
+
+        this.getRootPane().setJMenuBar( menuBar );
+    }
+
+    private void setup2DScene( GridBagLayout gbl ) {
+        panelWest = new JPanel( new BorderLayout() );
+        scenePanel2D = new Scene2DPanel();
+        naviPanelGeoref = new NavigationBarPanelGeoref();
+        scenePanel2D.setBorder( BorderFactory.createBevelBorder( BevelBorder.LOWERED ) );
+        scenePanel2D.setPreferredSize( SUBCOMPONENT_DIMENSION );
+
+        panelWest.add( naviPanelGeoref, BorderLayout.NORTH );
+        panelWest.add( scenePanel2D, BorderLayout.CENTER );
+
+        GridBagLayoutHelper.addComponent( this.getContentPane(), gbl, panelWest, 0, 1, 2, 2, 1.0, 1.0 );
+
+    }
+
+    private void setupPanelFootprint( GridBagLayout gbl ) {
+        panelEast = new JPanel( new BorderLayout() );
+        footprintPanel = new BuildingFootprintPanel();
+        naviPanelFoot = new NavigationBarPanelFootprint();
+        footprintPanel.setBorder( BorderFactory.createBevelBorder( BevelBorder.LOWERED ) );
+        footprintPanel.setBackground( Color.white );
+        footprintPanel.setPreferredSize( SUBCOMPONENT_DIMENSION );
+
+        panelEast.add( naviPanelFoot, BorderLayout.NORTH );
+        panelEast.add( footprintPanel, BorderLayout.CENTER );
+
+        GridBagLayoutHelper.addComponent( this.getContentPane(), gbl, panelEast, 2, 1, 1, 1,
+                                          footprintPanel.getInsets(), GridBagConstraints.LINE_END, .5, 1 );
+
+    }
+
+    private void setupOpenGL( GridBagLayout gbl, boolean testSphere ) {
+        GLCapabilities caps = new GLCapabilities();
+        caps.setDoubleBuffered( true );
+        caps.setHardwareAccelerated( true );
+        caps.setAlphaBits( 8 );
+        caps.setAccumAlphaBits( 8 );
+        openGLEventListener = new OpenGLEventHandler( testSphere );
+
+        GLCanvas canvas = new GLCanvas( caps );
+        canvas.addGLEventListener( openGLEventListener );
+        canvas.addMouseListener( openGLEventListener.getTrackBall() );
+        canvas.addMouseWheelListener( openGLEventListener.getTrackBall() );
+        canvas.addMouseMotionListener( openGLEventListener.getTrackBall() );
+        canvas.setPreferredSize( SUBCOMPONENT_DIMENSION );
+
+        GridBagLayoutHelper.addComponent( this.getContentPane(), gbl, canvas, 2, 2, 1, 1, new Insets( 0, 10, 0, 0 ),
+                                          GridBagConstraints.LINE_END, .5, 1 );
+    }
+
+    /**
+     * not used at the moment
+     */
+    public void resetScene2D() {
+        scenePanel2D.paint( new BufferedImage( 0, 0, BufferedImage.TYPE_3BYTE_BGR ).createGraphics() );
+    }
+
+    /**
+     * Adds the actionListener to the visible components to interact with the user.
+     * 
+     * @param e
+     */
+    public void addListeners( ActionListener e ) {
+        naviPanelGeoref.addCoordListener( e );
+        naviPanelGeoref.addAbstractCoordListener( e );
+        naviPanelFoot.addAbstractCoordListener( e );
+        editMenuItem.addActionListener( e );
+        openShape.addActionListener( e );
+        openWMS.addActionListener( e );
+        openBuilding.addActionListener( e );
+
+    }
+
+    /**
+     * The {@link Scene2DPanel} is a child of this Container
+     * 
+     * @return
+     */
+    public Scene2DPanel getScenePanel2D() {
+        return scenePanel2D;
+    }
+
+    public BuildingFootprintPanel getFootprintPanel() {
+        return footprintPanel;
+    }
+
+    public void addHoleWindowListener( ComponentListener c ) {
+        this.addComponentListener( c );
+
+    }
+
+    public NavigationBarPanelGeoref getNavigationPanelGeoref() {
+        return naviPanelGeoref;
+    }
+
+    public NavigationBarPanelFootprint getNaviPanelFoot() {
+        return naviPanelFoot;
+    }
+
+    public OpenGLEventHandler getOpenGLEventListener() {
+        return openGLEventListener;
+    }
+
+    /**
+     * Sets everything that is needed to handle userinteraction with the checkboxes in the transformationMenu.
+     * 
+     * @param selectedCheckbox
+     *            the checkbox that has been selected by the user.
+     */
+    public void activateTransformationCheckbox( JCheckBox selectedCheckbox ) {
+        this.list.getModel().selectThisCheckbox( selectedCheckbox );
+        this.menuTransformation.getPopupMenu().setVisible( false );
+        this.menuTransformation.setVisible( true );
+        this.menuTransformation.setSelected( false );
+    }
+
+    public JMenu getMenuTransformation() {
+        return menuTransformation;
+    }
+
+    public void addToMenuTransformation( CheckboxListTransformation list ) {
+        this.list = list;
+        for ( JCheckBox box : list.getModel().getList() ) {
+            this.menuTransformation.add( box );
+        }
+
+    }
+
+}
