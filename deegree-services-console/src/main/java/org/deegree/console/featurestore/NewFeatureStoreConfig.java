c88a9bc1956e4e19eb4ef7ce2efcd1d07f2033cf
Andreas Schmitz <schmitz@occamlabs.de>
2010-09-03 15:47:52 +0000

    Tagging ${RELEASE_VERSION}

---
+++ deegree-services-console/src/main/java/org/deegree/console/featurestore/NewFeatureStoreConfig.java
@@ -0,0 +1,81 @@
+//$HeadURL: svn+ssh://mschneider@svn.wald.intevation.org/deegree/base/trunk/resources/eclipse/files_template.xml $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2010 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.console.featurestore;
+
+import java.io.IOException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.deegree.console.ConfigManager;
+import org.deegree.feature.persistence.FeatureStoreProvider;
+
+/**
+ * TODO add class documentation here
+ * 
+ * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
+ * @author last edited by: $Author: markus $
+ * 
+ * @version $Revision: $, $Date: $
+ */
+@ManagedBean
+@RequestScoped
+public class NewFeatureStoreConfig {
+
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String type;
+
+    public String create()
+                            throws IOException {
+
+        FeatureStoreConfigManager configManager = ConfigManager.getApplicationInstance().getFsManager();
+        FeatureStoreProvider provider = configManager.getProvider( type );
+        FeatureStoreConfig config = new FeatureStoreConfig( id, false, false, configManager, provider );
+        configManager.add( config );
+        config.create();
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "editConfig", config );
+        return "console/generic/xmleditor.jsf";
+    }
+}
