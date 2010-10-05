2f6654aa70c33f2a3c63250b4eb3ab38f7ee77a8
Markus Schneider <schneider@occamlabs.de>
2010-10-05 17:40:09 +0000

    Added Java version check tool.

--- deegree-misc/deegree-tomcat/src/main/resources/start-deegree.sh
+++ deegree-misc/deegree-tomcat/src/main/resources/start-deegree.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# TODO check for JAVA
+java -classpath deegree-javacheck.jar org.deegree.JavaCheck
+RETVAL=$?
+[ $RETVAL -ne 0 ] && exit
 
 bin/catalina.sh run &
 
 # TODO proper browser startup after Tomcat started
 sleep 5
-firefox http://127.0.0.1:8080
+firefox http://localhost:8080
+
+# Wait forever
+cat
