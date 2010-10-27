5d758c907bfcdd667f8c83a766415691ffee5d85
Andreas Schmitz <schmitz@occamlabs.de>
2010-10-27 07:51:34 +0000

    fixed location of files

--- deegree-test/deegree-wms-similarity-tests/src/main/resources/styles/genreqs.sh
+++ deegree-test/deegree-wms-similarity-tests/src/main/resources/styles/genreqs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # generate the requests
 
-loc=../../deegree-wms-similarity-tests/src/test/resources/org/deegree/test/services/wms/similaritytests.txt
+loc=../../../test/resources/org/deegree/test/services/wms/similaritytests.txt
 
 for file in points/*sld
 do
