0485bf44164dfe994f89d18e22bab432de8822f4
Markus Schneider <schneider@occamlabs.de>
2010-10-29 15:13:08 +0000

    Tagging 3.0-rc1

---
+++ deegree-tools/src/main/java/org/deegree/tools/coverage/TransformRaster.java
@@ -0,0 +1,171 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ Department of Geography, University of Bonn
+ and
+ lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.tools.coverage;
+
+import static org.deegree.tools.coverage.RasterCommandUtils.getInterpolationType;
+
+import java.io.File;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.deegree.coverage.raster.AbstractRaster;
+import org.deegree.coverage.raster.RasterTransformer;
+import org.deegree.coverage.raster.TiledRaster;
+import org.deegree.coverage.raster.container.MemoryTileContainer;
+import org.deegree.coverage.raster.geom.RasterGeoReference.OriginLocation;
+import org.deegree.coverage.raster.interpolation.InterpolationType;
+import org.deegree.coverage.raster.io.RasterIOOptions;
+import org.deegree.coverage.raster.utils.RasterFactory;
+import org.deegree.cs.CRS;
+import org.deegree.tools.CommandUtils;
+import org.deegree.tools.annotations.Tool;
+
+/**
+ * This is a command line tool to transform raster files between coordinate systems. Multiple input files will be
+ * mosaiced into one transformed output file.
+ * 
+ * @author <a href="mailto:tonnhofer@lat-lon.de">Oliver Tonnhofer</a>
+ * @author last edited by: $Author$
+ * 
+ * @version $Revision$, $Date$
+ * 
+ */
+@Tool("Transforms a raster with the given crs into another crs")
+public class TransformRaster {
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args ) {
+        CommandLineParser parser = new PosixParser();
+
+        Options options = new Options();
+
+        Option t_srs = new Option( "t_srs", "the srs of the target raster" );
+        t_srs.setRequired( true );
+        t_srs.setArgs( 1 );
+        t_srs.setArgName( "epsg code" );
+        options.addOption( t_srs );
+
+        Option s_srs = new Option( "s_srs", "the srs of the source raster" );
+        s_srs.setRequired( true );
+        s_srs.setArgs( 1 );
+        s_srs.setArgName( "epsg code" );
+        options.addOption( s_srs );
+
+        Option interpolation = new Option( "interpolation",
+                                           "the raster interpolation (nn: nearest neighbour, bl: bilinear" );
+        interpolation.setArgs( 1 );
+        interpolation.setArgName( "nn|bl" );
+        options.addOption( interpolation );
+
+        Option originLocation = new Option( "origin", "originlocation", true,
+                                            "the location of the origin on the upper left pixel (default = center)" );
+        interpolation.setArgs( 1 );
+        interpolation.setArgName( "center|outer" );
+        options.addOption( originLocation );
+
+        CommandUtils.addDefaultOptions( options );
+
+        // for the moment, using the CLI API there is no way to respond to a help argument; see
+        // https://issues.apache.org/jira/browse/CLI-179
+        if ( args.length == 0 || ( args.length > 0 && ( args[0].contains( "help" ) || args[0].contains( "?" ) ) ) ) {
+            printHelp( options );
+        }
+
+        try {
+            CommandLine line = parser.parse( options, args );
+
+            InterpolationType interpolationType = getInterpolationType( line.getOptionValue( interpolation.getOpt() ) );
+            OriginLocation location = getLocation( line.getOptionValue( originLocation.getOpt() ) );
+
+            transformRaster( line.getArgs(), line.getOptionValue( "s_srs" ), line.getOptionValue( "t_srs" ),
+                             interpolationType, location );
+
+        } catch ( ParseException exp ) {
+            System.out.println( "ERROR: Invalid command line:" + exp.getMessage() );
+        }
+        System.exit( 0 );
+    }
+
+    /**
+     * @param optionValue
+     * @return
+     */
+    private static OriginLocation getLocation( String optionValue ) {
+        OriginLocation result = OriginLocation.CENTER;
+        if ( "outer".equalsIgnoreCase( optionValue ) ) {
+            result = OriginLocation.OUTER;
+        }
+
+        return result;
+    }
+
+    private static void transformRaster( String[] args, String srcCRS, String dstCRS, InterpolationType type,
+                                         OriginLocation location ) {
+        try {
+            MemoryTileContainer tileContainer = new MemoryTileContainer();
+            for ( int i = 0; i < args.length - 1; i++ ) {
+                if ( args[i] != null ) {
+                    File f = new File( args[i] );
+                    RasterIOOptions options = RasterIOOptions.forFile( f );
+                    options.add( RasterIOOptions.GEO_ORIGIN_LOCATION, location.name() );
+                    tileContainer.addTile( RasterFactory.loadRasterFromFile( f, options ) );
+                }
+            }
+            AbstractRaster srcRaster = new TiledRaster( tileContainer );
+            RasterTransformer transf = new RasterTransformer( dstCRS );
+            srcRaster.setCoordinateSystem( new CRS( srcCRS ) );
+            AbstractRaster result = transf.transform( srcRaster, type );
+
+            RasterFactory.saveRasterToFile( result, new File( args[args.length - 1] ) );
+        } catch ( Exception ex ) {
+            System.err.println( "Couldn't transform raster file: " );
+            ex.printStackTrace();
+            System.exit( 2 );
+        }
+    }
+
+    private static void printHelp( Options options ) {
+        String msg = "This is a command line tool to transform raster files between coordinate systems."
+                     + " Multiple input files will be mosaiced into one transformed output file.";
+        CommandUtils.printHelp( options, "RasterTransformer", msg, "inputfile [more_inputfiles*] outputfile" );
+    }
+}
---
+++ deegree-tools/src/main/java/org/deegree/tools/rendering/dem/builder/PatchManager.java
@@ -0,0 +1,139 @@
+//$HeadURL: svn+ssh://mschneider@svn.wald.intevation.org/deegree/base/trunk/resources/eclipse/files_template.xml $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+   Department of Geography, University of Bonn
+ and
+   lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+----------------------------------------------------------------------------*/
+
+package org.deegree.tools.rendering.dem.builder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import org.deegree.rendering.r3d.multiresolution.MultiresolutionMesh;
+import org.deegree.tools.rendering.dem.builder.dag.FragmentInfo;
+
+/**
+ * Manages the storing of {@link MacroTriangle}-based patches during the generation of an
+ * {@link MultiresolutionMesh}-dataset (that is performed by the {@link DEMDatasetGenerator}).
+ *
+ * @author <a href="mailto:schneider@lat-lon.de">Markus Schneider</a>
+ * @author last edited by: $Author: schneider $
+ *
+ * @version $Revision: $, $Date: $
+ */
+public class PatchManager {
+
+    private int levels;
+
+    private Blob targetBlob;
+
+    // current offset in blob
+    private long currentPos = 0;
+
+    // contains the patch positions for all location codes
+    private FragmentInfo[] patchesByLocationCode;
+
+    private PatchManager(int levels, FragmentInfo[] patchesByLocationCode) {
+        this.levels = levels;
+        this.patchesByLocationCode = patchesByLocationCode;
+    }
+
+    PatchManager(int levels, Blob targetBlob) {
+        this.levels = levels;
+        this.targetBlob = targetBlob;
+        this.patchesByLocationCode = new FragmentInfo[(2 << levels) - 2];
+        System.out.println("Creating new triangle manager for " + levels + " levels. Reserved "
+                + patchesByLocationCode.length + " offsets.");
+    }
+
+    public int getLevels() {
+        return levels;
+    }
+
+    public synchronized void storePatch(MacroTriangle patch, ByteBuffer rawTileBytes)
+            throws SQLException {
+
+        byte [] bytes = new byte [rawTileBytes.capacity()];
+        ByteBuffer buffer = ByteBuffer.wrap( bytes );
+        rawTileBytes.rewind();
+        buffer.put( rawTileBytes );
+        buffer.rewind();
+
+        String locationCode = patch.getLocationCode();
+        // System.out.print("Storing macro triangle '" + locationCode
+        // + "' in blob and registering patch info...");
+        FragmentInfo pos = new FragmentInfo(patch, currentPos, rawTileBytes.capacity());
+        patchesByLocationCode[getArrayPosForLocationCode(locationCode)] = pos;
+        targetBlob.setBytes(pos.blobPosition + 1, bytes);
+        currentPos += pos.blobLength;
+        // System.out.println("done.");
+    }
+
+    private int getArrayPosForLocationCode(String locationCode) {
+        return Integer.parseInt("1" + locationCode, 2) - 2;
+    }
+
+    public FragmentInfo getPatchByLocationCode(int locationCode) {
+        return patchesByLocationCode[locationCode - 2];
+    }
+
+    public void saveToFile(File file) throws FileNotFoundException, IOException {
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(file));
+        os.writeInt(levels);
+        os.writeObject(patchesByLocationCode);
+        os.close();
+    }
+
+    public static PatchManager restoreFromFile(File file) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
+        ObjectInputStream is = new ObjectInputStream(new FileInputStream(file));
+        int levels = is.readInt();
+        FragmentInfo[] patchInfo = (FragmentInfo[]) is.readObject();
+        is.close();
+        return new PatchManager(levels, patchInfo);
+    }
+
+    public String toString() {
+        String s = "- Total number of patches: " + patchesByLocationCode.length;
+        s += "\n- Number of patches on level 0: " + (patchesByLocationCode.length + 2) / 2;
+        return s;
+    }
+}
