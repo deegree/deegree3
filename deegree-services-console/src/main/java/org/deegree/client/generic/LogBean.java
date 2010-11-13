69cd93b4621d22c24b2d5c03ada9005c45f18d32
Andreas Schmitz <schmitz@occamlabs.de>
2010-11-13 14:16:57 +0000

    move away from loginfailed page when correctly logging in

--- deegree-services-console/src/main/java/org/deegree/client/generic/LogBean.java
+++ deegree-services-console/src/main/java/org/deegree/client/generic/LogBean.java
@@ -52,10 +52,10 @@ import org.slf4j.LoggerFactory;
 
 /**
  * TODO add class documentation here
- * 
+ *
  * @author <a href="mailto:buesching@lat-lon.de">Lyn Buesching</a>
  * @author last edited by: $Author: lyn $
- * 
+ *
  * @version $Revision: $, $Date: $
  */
 @ManagedBean
@@ -101,7 +101,11 @@ public class LogBean implements Serializable {
         String view = FacesContext.getCurrentInstance().getViewRoot().getViewId();
         if ( password != null && password.equals( correctPw ) ) {
             loggedIn = true;
-            return view;
+            if(view.indexOf("Failed") == -1){
+                return view;
+            }else{
+                return "/console/jsf/welcome";
+            }
         }
         return "failed";
     }
