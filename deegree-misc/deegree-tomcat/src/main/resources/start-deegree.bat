3f8721b7a2006af617277979481aa898ec1821c7
Markus Schneider <schneider@occamlabs.de>
2010-11-14 15:27:36 +0000

    Set Heap to 1G

--- deegree-misc/deegree-tomcat/src/main/resources/start-deegree.bat
+++ deegree-misc/deegree-tomcat/src/main/resources/start-deegree.bat
@@ -3,6 +3,7 @@
 java -classpath deegree-javacheck.jar org.deegree.JavaCheck
 IF ERRORLEVEL 5 GOTO End
 
+SET JAVA_OPTS=-Xmx1024M
 bin\catalina.bat start
 START http://localhost:8080
 TIMEOUT /T -1
--- deegree-misc/deegree-tomcat/src/main/resources/start-deegree.sh
+++ deegree-misc/deegree-tomcat/src/main/resources/start-deegree.sh
@@ -4,6 +4,7 @@ java -classpath deegree-javacheck.jar org.deegree.JavaCheck
 RETVAL=$?
 [ $RETVAL -ne 0 ] && exit
 
+export JAVA_OPTS=-Xmx1024M
 bin/catalina.sh run &
 
 # TODO proper browser startup after Tomcat started
