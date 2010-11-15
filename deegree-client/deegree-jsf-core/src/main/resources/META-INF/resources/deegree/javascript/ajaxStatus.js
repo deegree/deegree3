688c0bd753397375089bf4f39652c2c8111dbf6c
Markus Schneider <schneider@occamlabs.de>
2010-11-15 19:26:17 +0000

    Tagging 3.0.0

---
+++ deegree-client/deegree-jsf-core/src/main/resources/META-INF/resources/deegree/javascript/ajaxStatus.js
@@ -0,0 +1,23 @@
+function processAjaxUpdate(msgId, modal) {
+	function processEvent(data) {
+		var msg = document.getElementById(msgId);
+		if (msg != null) {
+			if (data.status == "begin") {
+				if (modal) {
+					document.body.style.cursor = 'wait'
+				}
+				msg.style.display = 'inline';
+			} else if (data.status == "success") {
+				if (modal) {
+					document.body.style.cursor = 'auto'
+				}
+				msg.style.display = 'none';
+			}
+		}
+	}
+	return processEvent;
+};
+
+function registerAjaxStatus(msgId, modal) {
+	jsf.ajax.addOnEvent(processAjaxUpdate(msgId, modal));
+}
