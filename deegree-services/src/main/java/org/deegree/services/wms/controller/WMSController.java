678d1df7fe0c298a1911ff44157f44f538e80496
Andreas Schmitz <schmitz@occamlabs.de>
2010-11-22 09:43:31 +0000

    fixed missing import

--- deegree-services/src/main/java/org/deegree/services/wms/controller/WMSController.java
+++ deegree-services/src/main/java/org/deegree/services/wms/controller/WMSController.java
@@ -55,6 +55,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
