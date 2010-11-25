ac7c422b38266f4b7023ab4273cbac1f0263603f
Sebastian Görke <goerke@lat-lon.de>
2010-11-25 16:43:34 +0000

    start building featuretype

--- deegree-osmstore/src/main/java/org/deegree/feature/persistence/osm/OSMFeatureStore.java
+++ deegree-osmstore/src/main/java/org/deegree/feature/persistence/osm/OSMFeatureStore.java
@@ -35,35 +35,88 @@
  ----------------------------------------------------------------------------*/
 package org.deegree.feature.persistence.osm;
 
+import static org.deegree.commons.tom.primitive.PrimitiveType.STRING;
+import static org.deegree.cs.CRS.EPSG_4326;
+import static org.deegree.feature.types.property.GeometryPropertyType.CoordinateDimension.DIM_2;
+import static org.deegree.feature.types.property.GeometryPropertyType.GeometryType.POINT;
+import static org.deegree.feature.types.property.ValueRepresentation.BOTH;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
 
-import org.deegree.commons.tom.primitive.PrimitiveType;
+import org.deegree.cs.CRS;
+import org.deegree.feature.Feature;
 import org.deegree.feature.persistence.FeatureStore;
 import org.deegree.feature.persistence.FeatureStoreException;
 import org.deegree.feature.persistence.FeatureStoreTransaction;
 import org.deegree.feature.persistence.lock.LockManager;
 import org.deegree.feature.persistence.query.FeatureResultSet;
 import org.deegree.feature.persistence.query.Query;
+import org.deegree.feature.property.GenericProperty;
+import org.deegree.feature.property.Property;
 import org.deegree.feature.types.ApplicationSchema;
 import org.deegree.feature.types.FeatureType;
 import org.deegree.feature.types.GenericFeatureType;
+import org.deegree.feature.types.property.GeometryPropertyType;
 import org.deegree.feature.types.property.PropertyType;
 import org.deegree.feature.types.property.SimplePropertyType;
 import org.deegree.filter.FilterEvaluationException;
 import org.deegree.geometry.Envelope;
+import org.deegree.geometry.Geometry;
+import org.deegree.geometry.GeometryFactory;
 import org.deegree.gml.GMLObject;
 import org.slf4j.Logger;
 
 public class OSMFeatureStore implements FeatureStore {
 
     private static final Logger LOG = getLogger( OSMFeatureStore.class );
+
+    private static final String OSM_NS = "http://www.deegree.org/osm";
+
+    private static FeatureType nodeFt;
+
+    private static FeatureType wayFt;
+
+    private static FeatureType relationFt;
+
+    private static GeometryPropertyType nodeGeomPt;
+
+    static {
+        QName ftName = new QName( OSM_NS, "node", "osm" );
+        List<PropertyType> pts = new ArrayList<PropertyType>();
+        nodeGeomPt = new GeometryPropertyType( new QName( OSM_NS, "geometry", "osm" ), 1, 1, false, false, null, POINT,
+                                               DIM_2, BOTH );
+        pts.add( nodeGeomPt );
+        pts.add( new SimplePropertyType( new QName( OSM_NS, "highway", "osm" ), 0, 1, STRING, false, false, null ) );
+        nodeFt = new GenericFeatureType( ftName, pts, false );
+
+        // TODO create feature types
+    }
+
+    OSMFeatureStore( ApplicationSchema schema, CRS storageCRS ) throws FeatureStoreException, FileNotFoundException,
+                            XMLStreamException, IOException {
+
+        // OSMReader.getBounds();
+
+        GeometryFactory geomFac = new GeometryFactory();
+
+        String fid = null;
+        List<Property> props = new ArrayList<Property>();
+        Geometry geom = geomFac.createPoint( null, x, y, EPSG_4326 );
+        props.add( new GenericProperty( nodeGeomPt, nodeGeomPt.getName(), geom ) );
+        Feature node = nodeFt.newFeature( fid, props, null );
+
+        // OSMReader.getNodes();
+        // OSMReader.getWays();
+        // OSMReader.getRelations();
+
+    }
 
     @Override
     public FeatureStoreTransaction acquireTransaction()
@@ -97,14 +150,7 @@ public class OSMFeatureStore implements FeatureStore {
 
     @Override
     public ApplicationSchema getSchema() {
-        QName ftName = new QName( "http://www.deegree.org/osm", "OSMFeature", "osm" );
-        List<PropertyType> pts = new ArrayList<PropertyType>();
-        QName propName = new QName( "http://www.deegree.org/osm", "myprop", "osm" );
-        pts.add( new SimplePropertyType( propName, 1, 1, PrimitiveType.STRING, false, false, null ) );
-        FeatureType ft = new GenericFeatureType( ftName, pts, false );
-        Map<String, String> prefixToNS = new HashMap<String, String>();
-        prefixToNS.put( "osm", "http://www.deegree.org/osm" );
-        return new ApplicationSchema( new FeatureType[] { ft }, null, prefixToNS, null );
+        return new ApplicationSchema( new FeatureType[] { nodeFt, relationFt, wayFt }, null, null, null );
     }
 
     @Override
