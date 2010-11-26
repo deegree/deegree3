375d980c0ad7556a1a09bd9f59b9b90833d7fe5a
Johannes Wilden <wilden@lat-lon.de>
2010-11-26 13:41:52 +0000

    Tagging 3.0.1

---
+++ deegree-services/src/main/java/org/deegree/services/wps/annotations/commons/BBox.java
@@ -0,0 +1,61 @@
+//$HeadURL$
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2009 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
+
+package org.deegree.services.wps.annotations.commons;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.deegree.services.wps.annotations.ProcessDescription;
+
+/**
+ * The <code>BBox</code> annotates a bbox parameter
+ * <p>
+ * See {@link ProcessDescription} for a brief introduction to assigning values to annotations.
+ * 
+ * @author <a href="mailto:bezema@lat-lon.de">Rutger Bezema</a>
+ * @author last edited by: $Author$
+ * @version $Revision$, $Date$
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BBox {
+
+    /** An array of allowed coordinate systems, the crs[0] will be the default */
+    public String[] crs();
+}
