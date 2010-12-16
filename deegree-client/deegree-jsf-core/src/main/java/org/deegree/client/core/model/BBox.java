cae6fbba0dd9536909bd737fe624cc12f626321e
Johannes Wilden <wilden@lat-lon.de>
2010-12-16 12:36:05 +0000

    Tagging 3.1-pre2

---
+++ deegree-client/deegree-jsf-core/src/main/java/org/deegree/client/core/model/BBox.java
@@ -0,0 +1,93 @@
+//$HeadURL: svn+ssh://lbuesching@svn.wald.intevation.de/deegree/base/trunk/resources/eclipse/files_template.xml $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2010 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.client.core.model;
+
+/**
+ * TODO add class documentation here
+ * 
+ * @author <a href="mailto:buesching@lat-lon.de">Lyn Buesching</a>
+ * @author last edited by: $Author: lyn $
+ * 
+ * @version $Revision: $, $Date: $
+ */
+public class BBox {
+
+    private String crs;
+
+    private double maxy;
+
+    private double miny;
+
+    private double maxx;
+
+    private double minx;
+
+    public BBox() {
+    }
+
+    public BBox( String crs, double minx, double miny, double maxx, double maxy ) {
+        this.crs = crs;
+        this.minx = minx;
+        this.miny = miny;
+        this.maxx = maxx;
+        this.maxy = maxy;
+    }
+
+    public String getCrs() {
+        return crs;
+    }
+
+    public double getMaxY() {
+        return maxy;
+    }
+
+    public double getMinY() {
+        return miny;
+    }
+
+    public double getMaxX() {
+        return maxx;
+    }
+
+    public double getMinx() {
+        return minx;
+    }
+
+    @Override
+    public String toString() {
+        return "min: " + minx + ", " + miny + " max: " + maxx + ", " + maxy + " (" + crs + ")";
+    }
+}
