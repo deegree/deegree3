e833b8a7ca2f569451fda1f1a2316cd79795c767
Johannes Wilden <wilden@lat-lon.de>
2011-03-04 13:34:08 +0000

    Tagging 3.0.3

---
+++ deegree-services/src/main/java/org/deegree/services/wps/annotations/commons/ComplexFormat.java
@@ -0,0 +1,72 @@
+//$HeadURL$
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2009 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ Andreas Poth
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ E-Mail: poth@lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
+
+package org.deegree.services.wps.annotations.commons;
+
+import static org.deegree.services.wps.annotations.ProcessDescription.NOT_SET;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.deegree.services.wps.annotations.ProcessDescription;
+
+/**
+ * The <code>ComplexFormat</code> annotates a complex parameter with the data it can describes.
+ * <p>
+ * See {@link ProcessDescription} for a brief introduction to assigning values to annotations.
+ * 
+ * @author <a href="mailto:bezema@lat-lon.de">Rutger Bezema</a>
+ * @author last edited by: $Author$
+ * @version $Revision$, $Date$
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target( ElementType.ANNOTATION_TYPE )
+public @interface ComplexFormat {
+
+    /** mimetype of the data, defaults to 'text/xml' */
+    public String mimeType() default "text/xml";
+
+    /** encoding of the data, defaults to 'UTF-8' */
+    public String encoding() default "UTF-8";
+
+    /** schema describing the data, not set */
+    public String schema() default NOT_SET;
+}
