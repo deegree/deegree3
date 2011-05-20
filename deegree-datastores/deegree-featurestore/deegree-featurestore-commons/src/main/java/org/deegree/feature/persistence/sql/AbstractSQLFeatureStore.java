d0112e44383f8d6b2f75081c08e64d18de56413f
Andreas Schmitz <schmitz@occamlabs.de>
2011-05-20 12:01:58 +0000

    copied the implementation from postgis fs

--- deegree-datastores/deegree-featurestore/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/sql/AbstractSQLFeatureStore.java
+++ deegree-datastores/deegree-featurestore/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/sql/AbstractSQLFeatureStore.java
@@ -83,6 +83,7 @@ import org.deegree.feature.persistence.query.FilteredFeatureResultSet;
 import org.deegree.feature.persistence.query.IteratorResultSet;
 import org.deegree.feature.persistence.query.MemoryFeatureResultSet;
 import org.deegree.feature.persistence.query.Query;
+import org.deegree.feature.persistence.sql.blob.BlobCodec;
 import org.deegree.feature.persistence.sql.blob.BlobMapping;
 import org.deegree.feature.persistence.sql.blob.FeatureBuilderBlob;
 import org.deegree.feature.persistence.sql.converter.ConverterFactory;
@@ -288,8 +289,41 @@ public abstract class AbstractSQLFeatureStore implements SQLFeatureStore {
         return geomOrFeature;
     }
 
-    protected abstract GMLObject getObjectByIdBlob( String id, BlobMapping blobMapping )
-                            throws FeatureStoreException;
+    protected GMLObject getObjectByIdBlob( String id, BlobMapping blobMapping )
+                            throws FeatureStoreException {
+        GMLObject geomOrFeature = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            StringBuilder sql = new StringBuilder( "SELECT " );
+            sql.append( blobMapping.getDataColumn() );
+            sql.append( " FROM " );
+            sql.append( blobMapping.getTable() );
+            sql.append( " WHERE " );
+            sql.append( blobMapping.getGMLIdColumn() );
+            sql.append( "=?" );
+
+            conn = ConnectionManager.getConnection( getConnId() );
+            stmt = conn.prepareStatement( sql.toString() );
+            stmt.setString( 1, id );
+            rs = stmt.executeQuery();
+            if ( rs.next() ) {
+                LOG.debug( "Recreating object '" + id + "' from bytea." );
+                BlobCodec codec = blobMapping.getCodec();
+                geomOrFeature = codec.decode( rs.getBinaryStream( 1 ), getNamespaceContext(), getSchema(),
+                                              blobMapping.getCRS(), new FeatureStoreGMLIdResolver( this ) );
+                getCache().add( geomOrFeature );
+            }
+        } catch ( Exception e ) {
+            String msg = "Error retrieving object by id (BLOB mode): " + e.getMessage();
+            LOG.debug( msg, e );
+            throw new FeatureStoreException( msg, e );
+        } finally {
+            close( rs, stmt, conn, LOG );
+        }
+        return geomOrFeature;
+    }
 
     protected abstract GMLObject getObjectByIdRelational( String id )
                             throws FeatureStoreException;
