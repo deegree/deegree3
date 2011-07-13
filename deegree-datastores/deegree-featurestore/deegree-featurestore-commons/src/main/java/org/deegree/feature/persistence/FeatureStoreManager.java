2e5b95e8d5a4e2512a05aa60591d150eac06810b
Markus Schneider <schneider@occamlabs.de>
2011-07-13 23:05:46 +0000

    Separated common and ISO metadata store functionality.

--- deegree-datastores/deegree-featurestore/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/FeatureStoreManager.java
+++ deegree-datastores/deegree-featurestore/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/FeatureStoreManager.java
@@ -53,6 +53,7 @@ import org.deegree.commons.utils.TempFileManager;
 import org.deegree.feature.persistence.cache.BBoxCache;
 import org.deegree.feature.persistence.cache.BBoxPropertiesCache;
 import org.deegree.filter.function.FunctionManager;
+import org.deegree.sqldialect.SQLDialectManager;
 import org.slf4j.Logger;
 
 /**
@@ -109,7 +110,7 @@ public class FeatureStoreManager extends AbstractResourceManager<FeatureStore> {
 
     @SuppressWarnings("unchecked")
     public Class<? extends ResourceManager>[] getDependencies() {
-        return new Class[] { ProxyUtils.class, ConnectionManager.class, FunctionManager.class };
+        return new Class[] { ProxyUtils.class, ConnectionManager.class, FunctionManager.class, SQLDialectManager.class };
     }
 
     static class FeatureStoreManagerMetadata extends DefaultResourceManagerMetadata<FeatureStore> {
