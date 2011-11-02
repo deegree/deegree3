dec01287e344927cc3e8c8285cb96ba733871c3b
Andreas Schmitz <schmitz@occamlabs.de>
2011-11-02 15:49:34 +0000

    working on generalizing feature layer config, adding defaulting

--- deegree-layers/deegree-layers-feature/src/main/java/org/deegree/layer/persistence/feature/FeatureLayerProvider.java
+++ deegree-layers/deegree-layers-feature/src/main/java/org/deegree/layer/persistence/feature/FeatureLayerProvider.java
@@ -50,6 +50,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -66,17 +67,21 @@ import org.deegree.commons.utils.Pair;
 import org.deegree.cs.coordinatesystems.ICRS;
 import org.deegree.feature.persistence.FeatureStore;
 import org.deegree.feature.persistence.FeatureStoreManager;
+import org.deegree.feature.types.FeatureType;
 import org.deegree.filter.OperatorFilter;
 import org.deegree.filter.xml.Filter110XMLDecoder;
 import org.deegree.geometry.metadata.SpatialMetadata;
 import org.deegree.layer.Layer;
 import org.deegree.layer.metadata.LayerMetadata;
-import org.deegree.layer.persistence.LayerStore;
 import org.deegree.layer.persistence.LayerStoreProvider;
-import org.deegree.layer.persistence.SingleLayerStore;
+import org.deegree.layer.persistence.MultipleLayerStore;
 import org.deegree.layer.persistence.base.jaxb.ScaleDenominatorsType;
-import org.deegree.layer.persistence.feature.jaxb.FeatureLayer;
+import org.deegree.layer.persistence.feature.jaxb.FeatureLayerType;
+import org.deegree.layer.persistence.feature.jaxb.FeatureLayers;
+import org.deegree.layer.persistence.feature.jaxb.FeatureLayers.AutoLayers;
 import org.deegree.protocol.ows.metadata.Description;
+import org.deegree.style.persistence.StyleStore;
+import org.deegree.style.persistence.StyleStoreManager;
 import org.deegree.style.se.unevaluated.Style;
 import org.slf4j.Logger;
 
@@ -88,7 +93,7 @@ public class FeatureLayerProvider implements LayerStoreProvider {
 
     private static final Logger LOG = getLogger( FeatureLayerProvider.class );
 
-    private static final URL SCHEMA_URL = FeatureLayerProvider.class.getResource( "/META-INF/schemas/layers/feature/3.1.0/feature.xsd" );
+    private static final URL SCHEMA_URL = FeatureLayerProvider.class.getResource( "/META-INF/schemas/layers/feature/3.2.0/feature.xsd" );
 
     private DeegreeWorkspace workspace;
 
@@ -97,60 +102,106 @@ public class FeatureLayerProvider implements LayerStoreProvider {
         this.workspace = workspace;
     }
 
+    private MultipleLayerStore createInAutoMode( AutoLayers auto )
+                            throws ResourceInitException {
+        Map<String, Layer> map = new HashMap<String, Layer>();
+        FeatureStoreManager mgr = workspace.getSubsystemManager( FeatureStoreManager.class );
+        String id = auto.getFeatureStoreId();
+        FeatureStore store = mgr.get( id );
+        if ( store == null ) {
+            throw new ResourceInitException( "Feature layer config was invalid, feature store with id " + id
+                                             + " is not available." );
+        }
+        StyleStoreManager smgr = workspace.getSubsystemManager( StyleStoreManager.class );
+        id = auto.getStyleStoreId();
+        StyleStore sstore = smgr.get( id );
+        if ( id != null && sstore == null ) {
+            throw new ResourceInitException( "Feature layer config was invalid, style store with id " + id
+                                             + " is not available." );
+        }
+
+        for ( FeatureType ft : store.getSchema().getFeatureTypes() ) {
+            String name = ft.getName().getLocalPart();
+            LayerMetadata md = new LayerMetadata( name, null, null );
+            Map<String, Style> styles = new HashMap<String, Style>();
+            for ( Style s : sstore.getAll( name ) ) {
+                styles.put( s.getName(), s );
+                if ( !styles.containsKey( "default" ) ) {
+                    styles.put( "default", s );
+                }
+            }
+            if ( !styles.containsKey( "default" ) ) {
+                styles.put( "default", new Style() );
+            }
+            Layer l = new FeatureLayer( md, store, ft.getName(), null, styles, styles );
+            map.put( name, l );
+        }
+
+        return new MultipleLayerStore( map );
+    }
+
     @Override
-    public LayerStore create( URL configUrl )
+    public MultipleLayerStore create( URL configUrl )
                             throws ResourceInitException {
         String pkg = "org.deegree.layer.persistence.feature.jaxb";
         try {
-            FeatureLayer lay = (FeatureLayer) unmarshall( pkg, SCHEMA_URL, configUrl, workspace );
+            FeatureLayers lays = (FeatureLayers) unmarshall( pkg, SCHEMA_URL, configUrl, workspace );
 
-            QName featureType = lay.getFeatureType();
-
-            OperatorFilter filter = null;
-            XMLInputFactory fac = XMLInputFactory.newInstance();
-            if ( lay.getFilter() != null ) {
-                XMLStreamReader reader = fac.createXMLStreamReader( new DOMSource( lay.getFilter() ) );
-                nextElement( reader );
-                nextElement( reader );
-                filter = (OperatorFilter) Filter110XMLDecoder.parse( reader );
-                reader.close();
+            if ( lays.getAutoLayers() != null ) {
+                return createInAutoMode( lays.getAutoLayers() );
             }
 
             FeatureStoreManager mgr = workspace.getSubsystemManager( FeatureStoreManager.class );
-            String fsRef = lay.getFeatureStoreId();
-            FeatureStore fs = mgr.get( fsRef );
-            if ( fs == null ) {
-                throw new ResourceInitException( "Feature layer config was invalid, feature store with id " + fsRef
+            String id = lays.getFeatureStoreId();
+            FeatureStore store = mgr.get( id );
+            if ( store == null ) {
+                throw new ResourceInitException( "Feature layer config was invalid, feature store with id " + id
                                                  + " is not available." );
             }
 
-            SpatialMetadata smd = fromJaxb( lay.getEnvelope(), lay.getCRS() );
-            Description desc = fromJaxb( lay.getTitle(), lay.getAbstract(), lay.getKeywords() );
-            LayerMetadata md = new LayerMetadata( lay.getName(), desc, smd );
+            Map<String, Layer> map = new HashMap<String, Layer>();
+            for ( FeatureLayerType lay : lays.getFeatureLayer() ) {
+                QName featureType = lay.getFeatureType();
 
-            if ( smd.getEnvelope() == null ) {
-                if ( featureType != null ) {
-                    smd.setEnvelope( fs.getEnvelope( featureType ) );
-                } else {
-                    smd.setEnvelope( getCombinedEnvelope( fs ) );
+                OperatorFilter filter = null;
+                XMLInputFactory fac = XMLInputFactory.newInstance();
+                if ( lay.getFilter() != null ) {
+                    XMLStreamReader reader = fac.createXMLStreamReader( new DOMSource( lay.getFilter() ) );
+                    nextElement( reader );
+                    nextElement( reader );
+                    filter = (OperatorFilter) Filter110XMLDecoder.parse( reader );
+                    reader.close();
                 }
-            }
-            if ( smd.getCoordinateSystems() == null || smd.getCoordinateSystems().isEmpty() ) {
-                List<ICRS> crs = new ArrayList<ICRS>();
-                crs.add( smd.getEnvelope().getCoordinateSystem() );
-                smd.setCoordinateSystems( crs );
-            }
 
-            ScaleDenominatorsType denoms = lay.getScaleDenominators();
-            if ( denoms != null ) {
-                md.setScaleDenominators( new DoublePair( denoms.getMin(), denoms.getMax() ) );
+                SpatialMetadata smd = fromJaxb( lay.getEnvelope(), lay.getCRS() );
+                Description desc = fromJaxb( lay.getTitle(), lay.getAbstract(), lay.getKeywords() );
+                LayerMetadata md = new LayerMetadata( lay.getName(), desc, smd );
+
+                if ( smd.getEnvelope() == null ) {
+                    if ( featureType != null ) {
+                        smd.setEnvelope( store.getEnvelope( featureType ) );
+                    } else {
+                        smd.setEnvelope( getCombinedEnvelope( store ) );
+                    }
+                }
+                if ( smd.getCoordinateSystems() == null || smd.getCoordinateSystems().isEmpty() ) {
+                    List<ICRS> crs = new ArrayList<ICRS>();
+                    crs.add( smd.getEnvelope().getCoordinateSystem() );
+                    smd.setCoordinateSystems( crs );
+                }
+
+                ScaleDenominatorsType denoms = lay.getScaleDenominators();
+                if ( denoms != null ) {
+                    md.setScaleDenominators( new DoublePair( denoms.getMin(), denoms.getMax() ) );
+                }
+
+                Pair<Map<String, Style>, Map<String, Style>> p = parseStyles( workspace, lay.getName(),
+                                                                              lay.getStyleRef() );
+
+                Layer l = new FeatureLayer( md, store, featureType, filter, p.first, p.second );
+                map.put( lay.getName(), l );
             }
-
-            Pair<Map<String, Style>, Map<String, Style>> p = parseStyles( workspace, lay.getName(), lay.getStyleRef() );
-
-            Layer l = new org.deegree.layer.persistence.feature.FeatureLayer( md, fs, featureType, filter, p.first,
-                                                                              p.second );
-            return new SingleLayerStore( l );
+            return new MultipleLayerStore( map );
         } catch ( Throwable e ) {
             throw new ResourceInitException( "Could not parse layer configuration file.", e );
         }
@@ -158,7 +209,7 @@ public class FeatureLayerProvider implements LayerStoreProvider {
 
     @Override
     public Class<? extends ResourceManager>[] getDependencies() {
-        return new Class[] { FeatureStoreManager.class };
+        return new Class[] { FeatureStoreManager.class, StyleStoreManager.class };
     }
 
     @Override
