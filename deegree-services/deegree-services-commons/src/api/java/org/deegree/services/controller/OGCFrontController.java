143b10f4f3ad826bc72e9271523d2ddc93695b66
Andreas Schmitz <schmitz@occamlabs.de>
2011-12-06 13:45:26 +0000

    implemented issue http://tracker.deegree.org/deegree-services/ticket/273

--- deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/OGCFrontController.java
+++ deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/OGCFrontController.java
@@ -291,6 +291,9 @@ public class OGCFrontController extends HttpServlet {
                 }
             }
 
+            // add cache control headers
+            response.addHeader( "Cache-Control", "no-cache, no-store" );
+
             response = handleCompression( response );
 
             String queryString = request.getQueryString();
