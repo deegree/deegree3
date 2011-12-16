71d5aaf3daed8cc6385d68379cc54ca84ef6d61a
Lyn Goltz <goltz@lat-lon.de>
2011-12-16 09:05:46 +0000

    implemented handling with missing/replacing fileidentifer

--- deegree-tools/deegree-tools-base/src/main/java/org/deegree/tools/metadata/Harvester.java
+++ deegree-tools/deegree-tools-base/src/main/java/org/deegree/tools/metadata/Harvester.java
@@ -1,12 +1,24 @@
 package org.deegree.tools.metadata;
 
+import static org.deegree.commons.xml.CommonNamespaces.ISOAP10GCONS;
+import static org.deegree.commons.xml.CommonNamespaces.ISOAP10GCO_PREFIX;
+import static org.deegree.commons.xml.CommonNamespaces.ISOAP10GMDNS;
+import static org.deegree.commons.xml.CommonNamespaces.ISOAP10GMD_PREFIX;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
 
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -16,6 +28,7 @@ import org.deegree.commons.annotations.Tool;
 import org.deegree.commons.config.DeegreeWorkspace;
 import org.deegree.commons.tools.CommandUtils;
 import org.deegree.commons.xml.CommonNamespaces;
+import org.deegree.commons.xml.NamespaceBindings;
 import org.deegree.commons.xml.XMLAdapter;
 import org.deegree.commons.xml.XPath;
 import org.deegree.metadata.MetadataRecordFactory;
@@ -40,9 +53,9 @@ import org.deegree.protocol.ows.exception.OWSExceptionReport;
 @Tool(value = "harvest metadata records and insert them in a CSW")
 public class Harvester {
 
-    private static final String xPathFileId = "//gmd:MD_Metadata/gmd:fileIdentifier/gco:CharacterString";
+    private static final NamespaceBindings NAMESPACE_CONTEXT = CommonNamespaces.getNamespaceContext();
 
-    private static final String xPathId = "gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:identifier/*/gmd:code/gco:CharacterString";
+    private static final String xPathFileId = "//gmd:MD_Metadata/gmd:fileIdentifier/gco:CharacterString";
 
     private static final String OPT_CSW_ADDRESS = "csw";
 
@@ -50,15 +63,33 @@ public class Harvester {
 
     private static final String OPT_WORKSPACE = "workspace";
 
+    private static final String OPT_SET_FI = "fileIdentifier";
+
+    private static final String OPT_FI_METHOD = "method";
+
+    public enum CREATEFI {
+        RETAIN, REPLACE, MISSING
+    }
+
+    public enum METHODFI {
+        UUID, FILENAME
+    }
+
     private final boolean verbose;
+
+    private final CREATEFI createFI;
+
+    private final METHODFI methodFI;
 
     /**
      * 
      * @param verbose
      * @throws IOException
      */
-    public Harvester( boolean verbose ) {
+    public Harvester( boolean verbose, METHODFI methodFI, CREATEFI createFI ) {
         this.verbose = verbose;
+        this.methodFI = methodFI;
+        this.createFI = createFI;
     }
 
     public void run( final String srcOpt, final String cswAddr )
@@ -130,28 +161,26 @@ public class Harvester {
 
             System.out.println( "Reading XML files from: " + inputDir );
             for ( int i = 0; i < listOfFiles.length; i++ ) {
-                System.out.println( listOfFiles[i].getName() );
+                final String fileName = listOfFiles[i].getName();
+                System.out.println( fileName );
                 XMLAdapter xml = null;
                 try {
                     xml = new XMLAdapter( listOfFiles[i] );
-                    // TODO
-                    // if ( redefineIDs ) {
-                    // OMElement elem = xml.getElement( xml.getRootElement(),
-                    // new XPath( xPathFileId, CommonNamespaces.getNamespaceContext() ) );
-                    // XMLTools.setNodeValue( elem, UUID.randomUUID().toString() );
-                    // elem = xml.getElement( xml.getRootElement(),
-                    // new XPath( xPathId, CommonNamespaces.getNamespaceContext() ) );
-                    // sFile = xml.getAsPrettyString();
-                    // }
+                    if ( createFI != null && createFI.equals( CREATEFI.REPLACE ) ) {
+                        xml = setNewFileIdentifier( fileName, xml );
+                    } else if ( createFI != null && createFI.equals( CREATEFI.MISSING ) ) {
+                        if ( xml.getElement( xml.getRootElement(), new XPath( xPathFileId, NAMESPACE_CONTEXT ) ) == null ) {
+                            xml = setNewFileIdentifier( fileName, xml );
+                        }
+                    }
                     boolean inserted = insertRecord( xml.getRootElement() );
                     if ( inserted )
                         count++;
                 } catch ( Exception e ) {
                     countFailed++;
                     if ( xml != null ) {
-                        final String fi = xml.getNodeAsString( xml.getRootElement(),
-                                                               new XPath( xPathFileId,
-                                                                          CommonNamespaces.getNamespaceContext() ),
+                        final String fi = xml.getNodeAsString( xml.getRootElement(), new XPath( xPathFileId,
+                                                                                                NAMESPACE_CONTEXT ),
                                                                null );
 
                         System.out.println( "ignore record with fileIdentifier " + fi + ", insert failed: "
@@ -165,6 +194,35 @@ public class Harvester {
             }
 
             System.out.println( "\n" + "Done. " + count + " records harvested." );
+        }
+
+        private XMLAdapter setNewFileIdentifier( String fileName, XMLAdapter xml )
+                                throws XMLStreamException {
+            String newFi;
+            if ( methodFI.equals( METHODFI.UUID ) ) {
+                newFi = UUID.randomUUID().toString();
+            } else {
+                newFi = fileName.contains( "." ) ? fileName.substring( 0, fileName.lastIndexOf( '.' ) ) : fileName;
+            }
+            System.out.println( "New fileIdentifier: " + newFi );
+            OMElement fiElem = xml.getElement( xml.getRootElement(), new XPath( xPathFileId, NAMESPACE_CONTEXT ) );
+            if ( fiElem == null ) {
+                OMElement mdMeta = xml.getElement( xml.getRootElement(), new XPath( "//gmd:MD_Metadata",
+                                                                                    NAMESPACE_CONTEXT ) );
+                OMFactory omFactory = mdMeta.getOMFactory();
+                OMElement csElem = omFactory.createOMElement( new QName( ISOAP10GCONS, "CharacterString",
+                                                                         ISOAP10GCO_PREFIX ) );
+                csElem.addChild( omFactory.createOMText( newFi ) );
+                OMElement newFiElem = omFactory.createOMElement( new QName( ISOAP10GMDNS, "fileIdentifier",
+                                                                            ISOAP10GMD_PREFIX ) );
+                newFiElem.addChild( csElem );
+                mdMeta.getFirstElement().insertSiblingBefore( newFiElem );
+            } else {
+                fiElem.setText( newFi );
+            }
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            xml.getRootElement().serialize( bos );
+            return new XMLAdapter( new ByteArrayInputStream( bos.toByteArray() ) );
         }
 
         abstract boolean insertRecord( OMElement record )
@@ -185,8 +243,25 @@ public class Harvester {
                 String cswOpt = cmdline.getOptionValue( OPT_CSW_ADDRESS );
                 String srcOpt = cmdline.getOptionValue( OPT_SOURCE );
                 String workspaceOpt = cmdline.getOptionValue( OPT_WORKSPACE );
+                String methodOpt = cmdline.getOptionValue( OPT_FI_METHOD );
+                String createOpt = cmdline.getOptionValue( OPT_SET_FI );
 
-                Harvester csw = new Harvester( verbose );
+                METHODFI m = null;
+                if ( methodOpt != null ) {
+                    m = METHODFI.valueOf( methodOpt.toUpperCase() );
+                }
+
+                CREATEFI c = null;
+                if ( createOpt != null ) {
+                    c = CREATEFI.valueOf( createOpt.toUpperCase() );
+                }
+
+                if ( c != null && !c.equals( CREATEFI.RETAIN ) && m == null ) {
+                    System.err.println( "Option " + OPT_SET_FI + " with value" + c.name()
+                                        + " requires additional option " + OPT_FI_METHOD );
+                    System.exit( 1 );
+                }
+                Harvester csw = new Harvester( verbose, m, c );
                 if ( cswOpt == null && workspaceOpt == null ) {
                     System.err.println( "The harvesting target is not passed. Either " + OPT_CSW_ADDRESS + " or "
                                         + OPT_WORKSPACE + " must be passed!" );
@@ -233,6 +308,29 @@ public class Harvester {
                           true,
                           "the workspace/store containing the target metadatastore, workspace and store must be seperated by a ':', e.g: workspace:store. Either this parameter or the "
                                                   + OPT_CSW_ADDRESS + " must be passed." );
+        opt.setRequired( false );
+        opts.addOption( opt );
+
+        opt = new Option( "m", OPT_FI_METHOD, true, "how to create/replace a new fileIdentifer, required if "
+                                                    + OPT_SET_FI + " is one of " + CREATEFI.MISSING + " or "
+                                                    + CREATEFI.REPLACE + ". Must be one of \n   " + METHODFI.UUID
+                                                    + ": create a new UUID\n   " + METHODFI.FILENAME
+                                                    + ": use name of the record file" );
+        opt.setRequired( false );
+        opts.addOption( opt );
+
+        opt = new Option(
+                          "f",
+                          OPT_SET_FI,
+                          true,
+                          "handling with fileIdentifier, possible values are: \n   "
+                                                  + CREATEFI.RETAIN
+                                                  + ": don't adjust the fileIdentifer (default, if this argument is missing)\n   "
+                                                  + CREATEFI.REPLACE
+                                                  + ": create a new fileIdentifier for all records (replace existing).\n   "
+                                                  + CREATEFI.MISSING
+                                                  + ": create a new fileIdentifer, when fileIdentifer is missing.\n   "
+                                                  + "Use this option with option '" + OPT_FI_METHOD + "'" );
         opt.setRequired( false );
         opts.addOption( opt );
 
