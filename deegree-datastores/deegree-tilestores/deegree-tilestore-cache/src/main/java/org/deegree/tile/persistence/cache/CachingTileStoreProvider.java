07db3b080c59ea32aa046d0c603fb626e2a0f213
Andreas Schmitz <schmitz@occamlabs.de>
2012-03-21 14:47:38 +0000

    added template for caching tile store

---
+++ deegree-datastores/deegree-tilestores/deegree-tilestore-cache/src/main/java/org/deegree/tile/persistence/cache/CachingTileStoreProvider.java
@@ -0,0 +1,106 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2010 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ Occam Labs UG (haftungsbeschränkt)
+ Godesberger Allee 139, 53175 Bonn
+ Germany
+ http://www.occamlabs.de/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.tile.persistence.cache;
+
+import static org.deegree.commons.xml.jaxb.JAXBUtils.unmarshall;
+
+import java.net.URL;
+
+import org.deegree.commons.config.DeegreeWorkspace;
+import org.deegree.commons.config.ResourceInitException;
+import org.deegree.commons.config.ResourceManager;
+import org.deegree.tile.persistence.TileStore;
+import org.deegree.tile.persistence.TileStoreProvider;
+import org.deegree.tile.persistence.cache.jaxb.CachingTileStore;
+
+/**
+ * The <code>GeoTIFFTileStoreProvider</code> provides a <code>TileMatrixSet</code> out of a GeoTIFF file (tiled
+ * BIGTIFF).
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: mschneider $
+ * 
+ * @version $Revision: 31882 $, $Date: 2011-09-15 02:05:04 +0200 (Thu, 15 Sep 2011) $
+ */
+
+public class CachingTileStoreProvider implements TileStoreProvider {
+
+    private static final URL SCHEMA = CachingTileStoreProvider.class.getResource( "/META-INF/schemas/datasource/tile/cache/3.2.0/cache.xsd" );
+
+    private DeegreeWorkspace workspace;
+
+    @Override
+    public void init( DeegreeWorkspace workspace ) {
+        this.workspace = workspace;
+    }
+
+    @Override
+    public TileStore create( URL configUrl )
+                            throws ResourceInitException {
+        try {
+            CachingTileStore p = (CachingTileStore) unmarshall( "org.deegree.tile.persistence.cache.jaxb", SCHEMA,
+                                                                configUrl, workspace );
+
+            System.out.println( p );
+
+            return null;
+        } catch ( Throwable e ) {
+            throw new ResourceInitException( "Unable to create tile store.", e );
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Class<? extends ResourceManager>[] getDependencies() {
+        return new Class[] {};
+    }
+
+    @Override
+    public String getConfigNamespace() {
+        return "http://www.deegree.org/datasource/tile/cache";
+    }
+
+    @Override
+    public URL getConfigSchema() {
+        return SCHEMA;
+    }
+
+}
