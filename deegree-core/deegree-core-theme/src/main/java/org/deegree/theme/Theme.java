8b3c759f866489b246d928ca0bd62506c7e59426
Andreas Schmitz <schmitz@occamlabs.de>
2012-07-20 12:34:48 +0000

    added javadocs

--- deegree-core/deegree-core-theme/src/main/java/org/deegree/theme/Theme.java
+++ deegree-core/deegree-core-theme/src/main/java/org/deegree/theme/Theme.java
@@ -46,12 +46,29 @@ import org.deegree.commons.config.Resource;
 import org.deegree.layer.Layer;
 import org.deegree.layer.metadata.LayerMetadata;
 
+/**
+ * A node in a theme tree.
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: mschneider $
+ * 
+ * @version $Revision: 31882 $, $Date: 2011-09-15 02:05:04 +0200 (Thu, 15 Sep 2011) $
+ */
 public interface Theme extends Resource {
 
+    /**
+     * @return the metadata (description, spatial) for this theme
+     */
     LayerMetadata getMetadata();
 
+    /**
+     * @return all direct child layers of this theme
+     */
     List<Layer> getLayers();
 
+    /**
+     * @return all direct child themes of this theme
+     */
     List<Theme> getThemes();
 
 }
