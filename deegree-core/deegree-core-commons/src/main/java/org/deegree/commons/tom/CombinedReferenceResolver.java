c968222a2540dcfff1e9af5442f72108a97395c7
Andreas Schmitz <schmitz@occamlabs.de>
2012-09-05 15:43:29 +0000

    added gml:identifier resolver to be used as custom resolver

---
+++ deegree-core/deegree-core-commons/src/main/java/org/deegree/commons/tom/CombinedReferenceResolver.java
@@ -0,0 +1,74 @@
+//$HeadURL: svn+ssh://svn.wald.intevation.org/deegree/deegree3/trunk/deegree-core/deegree-core-commons/src/main/java/org/deegree/commons/tom/ElementNode.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2011 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.commons.tom;
+
+import java.util.List;
+
+import org.deegree.commons.tom.gml.GMLObject;
+import org.deegree.commons.tom.gml.GMLReferenceResolver;
+
+/**
+ * Resolves objects by using a list of resolvers. Resolves an object to the first resolver that can resolve it.
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: mschneider $
+ * 
+ * @version $Revision: 30435 $, $Date: 2011-04-13 17:26:03 +0200 (Wed, 13 Apr 2011) $
+ */
+public class CombinedReferenceResolver implements GMLReferenceResolver {
+
+    private List<GMLReferenceResolver> resolvers;
+
+    public CombinedReferenceResolver( List<GMLReferenceResolver> resolvers ) {
+        this.resolvers = resolvers;
+    }
+
+    @Override
+    public GMLObject getObject( String uri, String baseURL ) {
+        for ( GMLReferenceResolver resolver : resolvers ) {
+            try {
+                GMLObject obj = resolver.getObject( uri, baseURL );
+                if ( obj != null ) {
+                    return obj;
+                }
+            } catch ( Throwable t ) {
+                // just skip to the next resolver
+            }
+        }
+        return null;
+    }
+
+}
---
+++ deegree-datastores/deegree-featurestores/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/FeatureStoreGmlIdentifierResolver.java
@@ -0,0 +1,135 @@
+//$HeadURL: svn+ssh://svn.wald.intevation.org/deegree/deegree3/trunk/deegree-datastores/deegree-featurestores/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/FeatureStoreGMLIdResolver.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2009 by:
+ Department of Geography, University of Bonn
+ and
+ lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.feature.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.namespace.QName;
+
+import org.deegree.commons.tom.gml.GMLObject;
+import org.deegree.commons.tom.gml.GMLReferenceResolver;
+import org.deegree.commons.tom.primitive.PrimitiveValue;
+import org.deegree.feature.Feature;
+import org.deegree.feature.persistence.query.Query;
+import org.deegree.feature.stream.FeatureInputStream;
+import org.deegree.feature.types.FeatureType;
+import org.deegree.filter.Filter;
+import org.deegree.filter.MatchAction;
+import org.deegree.filter.OperatorFilter;
+import org.deegree.filter.comparison.PropertyIsEqualTo;
+import org.deegree.filter.expression.Literal;
+import org.deegree.filter.expression.ValueReference;
+import org.deegree.protocol.wfs.getfeature.TypeName;
+
+/**
+ * {@link GMLReferenceResolver} that uses a {@link FeatureStore} for resolving local object references by using the
+ * gml:identifier (GML 3.2 only) property instead of the GML id. Currently supports urn:uuid: identifiers only.
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: mschneider $
+ * 
+ * @version $Revision: 31641 $, $Date: 2011-08-24 21:24:55 +0200 (Wed, 24 Aug 2011) $
+ */
+public class FeatureStoreGmlIdentifierResolver implements GMLReferenceResolver {
+
+    private final FeatureStore fs;
+
+    private final Pattern pattern = Pattern.compile( "urn\\:uuid\\:(.+)" );
+
+    private QName identifierName;
+
+    /**
+     * Creates a new {@link FeatureStoreGmlIdentifierResolver} instance.
+     * 
+     * @param fs
+     *            feature store to be used for retrieving local features, must not be <code>null</code>
+     */
+    public FeatureStoreGmlIdentifierResolver( FeatureStore fs ) {
+        this.fs = fs;
+        identifierName = new QName( "http://www.opengis.net/gml/3.2", "identifier" );
+    }
+
+    @Override
+    public GMLObject getObject( String uri, String baseURL ) {
+        System.out.println( "resolving!" + uri );
+        Matcher m = pattern.matcher( uri );
+        if ( m.find() ) {
+            List<FeatureType> fts = fs.getSchema().getFeatureTypes( null, false, false );
+            TypeName[] typeNames = new TypeName[fts.size()];
+            for ( int i = 0; i < fts.size(); ++i ) {
+                typeNames[i] = new TypeName( fts.get( i ).getName(), null );
+            }
+            String id = m.group( 1 );
+            ValueReference ref = new ValueReference( identifierName );
+            PropertyIsEqualTo eq = new PropertyIsEqualTo( ref, new Literal<PrimitiveValue>( id ), true, MatchAction.ALL );
+            Filter f = new OperatorFilter( eq );
+
+            List<Query> queries = new ArrayList<Query>();
+            for ( TypeName tn : typeNames ) {
+                queries.add( new Query( new TypeName[] { tn }, f, null, null, null ) );
+            }
+            System.out.println("queries: " + queries.size());
+            for ( Query q : queries ) {
+                System.out.println( "querying..." );
+                FeatureInputStream rs = null;
+                try {
+                    rs = fs.query( q );
+                    Feature obj = rs.iterator().next();
+                    if ( obj != null ) {
+                        System.out.println("have object!");
+                        return obj;
+                    }
+                } catch ( Throwable e ) {
+                    e.printStackTrace();
+                    // then it's not resolvable (?)
+                } finally {
+                    System.out.println("closongh");
+                    try{
+                    rs.close();
+                    }catch(Throwable e){
+                        // not closable
+                    }
+                    System.out.println("closed");
+                }
+                System.out.println("contn");
+            }
+        }// else?
+        return null;
+    }
+}
