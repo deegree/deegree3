a36fbc4e1b5488878ce9f6e0ee74015f87b74411
Lyn Goltz <goltz@lat-lon.de>
2012-09-24 14:07:57 +0000

    Added ISOMemoryMetadataStoreProvider and ISOMemoryMetadataStore

---
+++ deegree-datastores/deegree-mdstores/deegree-mdstore-iso-memory/src/main/java/org/deegree/metadata/iso/persistence/memory/ISOMemoryMetadataStore.java
@@ -0,0 +1,115 @@
+//$HeadURL: svn+ssh://mschneider@svn.wald.intevation.org/deegree/deegree3/trunk/deegree-core/deegree-core-metadata/src/main/java/org/deegree/metadata/iso/persistence/ISOMetadataStoreProvider.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2012 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.metadata.iso.persistence.memory;
+
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.deegree.commons.config.DeegreeWorkspace;
+import org.deegree.commons.config.ResourceInitException;
+import org.deegree.metadata.iso.ISORecord;
+import org.deegree.metadata.persistence.MetadataQuery;
+import org.deegree.metadata.persistence.MetadataResultSet;
+import org.deegree.metadata.persistence.MetadataStore;
+import org.deegree.metadata.persistence.MetadataStoreTransaction;
+import org.deegree.protocol.csw.MetadataStoreException;
+
+/**
+ * {@link MetadataStore} implementation for accessing ISO 19115 records kept in memory.
+ * 
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz</a>
+ * @author last edited by: $Author: lyn $
+ * 
+ * @version $Revision: 30992 $, $Date: 2011-05-31 16:09:20 +0200 (Di, 31. Mai 2011) $
+ */
+public class ISOMemoryMetadataStore implements MetadataStore<ISORecord> {
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init( DeegreeWorkspace arg0 )
+                            throws ResourceInitException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public MetadataResultSet<ISORecord> getRecords( MetadataQuery query )
+                            throws MetadataStoreException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getRecordCount( MetadataQuery query )
+                            throws MetadataStoreException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public MetadataResultSet<ISORecord> getRecordById( List<String> idList, QName[] recordTypeNames )
+                            throws MetadataStoreException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public MetadataStoreTransaction acquireTransaction()
+                            throws MetadataStoreException {
+        throw new UnsupportedOperationException(
+                                                 "Transactions are currently not supported for the ISOMemoryMetadataStore" );
+    }
+
+    /**
+     * @returns <code>null</code>, cause no JDBC connection is required
+     */
+    @Override
+    public String getConnId() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return "iso-memory";
+    }
+
+}
---
+++ deegree-datastores/deegree-mdstores/deegree-mdstore-iso-memory/src/main/java/org/deegree/metadata/iso/persistence/memory/ISOMemoryMetadataStoreProvider.java
@@ -0,0 +1,110 @@
+//$HeadURL: svn+ssh://mschneider@svn.wald.intevation.org/deegree/deegree3/trunk/deegree-core/deegree-core-metadata/src/main/java/org/deegree/metadata/iso/persistence/ISOMetadataStoreProvider.java $
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2012 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.metadata.iso.persistence.memory;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+
+import org.deegree.commons.config.DeegreeWorkspace;
+import org.deegree.commons.config.ResourceInitException;
+import org.deegree.commons.config.ResourceManager;
+import org.deegree.commons.jdbc.ConnectionManager.Type;
+import org.deegree.metadata.iso.ISORecord;
+import org.deegree.metadata.persistence.MetadataStore;
+import org.deegree.metadata.persistence.MetadataStoreProvider;
+import org.slf4j.Logger;
+
+/**
+ * {@link MetadataStoreProvider} for the {@link ISOMemoryMetadataStore}.
+ * 
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz</a>
+ * @author last edited by: $Author: lyn $
+ * 
+ * @version $Revision: 30800 $, $Date: 2011-05-12 16:49:44 +0200 (Do, 12. Mai 2011) $
+ */
+public class ISOMemoryMetadataStoreProvider implements MetadataStoreProvider {
+
+    private static final Logger LOG = getLogger( ISOMemoryMetadataStoreProvider.class );
+
+    private DeegreeWorkspace deegreeWorkspace;
+
+    private final static String CONFIG_NAMESPACE = "http://www.deegree.org/datasource/metadata/iso19139/memory";
+
+    private final static String CONFIG_SCHEMA = "/META-INF/schemas/datasource/metadata/iso19139/memory/3.2.0/memory.xsd";
+
+    @Override
+    public MetadataStore<ISORecord> create( URL arg0 )
+                            throws ResourceInitException {
+        return new ISOMemoryMetadataStore();
+    }
+
+    @Override
+    public Class<? extends ResourceManager>[] getDependencies() {
+        return null;
+    }
+
+    @Override
+    public void init( DeegreeWorkspace deegreeWorkspace ) {
+        this.deegreeWorkspace = deegreeWorkspace;
+
+    }
+
+    @Override
+    public String getConfigNamespace() {
+        return CONFIG_NAMESPACE;
+    }
+
+    @Override
+    public URL getConfigSchema() {
+        return ISOMemoryMetadataStore.class.getResource( CONFIG_SCHEMA );
+    }
+
+    @Override
+    public String[] getCreateStatements( Type dbType )
+                            throws UnsupportedEncodingException, IOException {
+        return new String[0];
+    }
+
+    @Override
+    public String[] getDropStatements( Type dbType )
+                            throws UnsupportedEncodingException, IOException {
+        return new String[0];
+    }
+
+}
