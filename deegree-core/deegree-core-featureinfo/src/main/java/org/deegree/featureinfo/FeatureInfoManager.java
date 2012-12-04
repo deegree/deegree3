e3fdf5a264352dd1587c88c28e65d022a9462f6f
Andreas Schmitz <schmitz@occamlabs.de>
2012-12-04 13:01:01 +0100

    extracted actual serializing magic from WMS into featureinfo module

--- deegree-core/deegree-core-featureinfo/src/main/java/org/deegree/featureinfo/FeatureInfoManager.java
+++ deegree-core/deegree-core-featureinfo/src/main/java/org/deegree/featureinfo/FeatureInfoManager.java
@@ -38,8 +38,33 @@
  Germany
 
  e-mail: info@deegree.org
-----------------------------------------------------------------------------*/
+ ----------------------------------------------------------------------------*/
 package org.deegree.featureinfo;
+
+import static org.deegree.featureinfo.templating.TemplatingUtils.runTemplate;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+import javax.xml.stream.XMLStreamWriter;
+
+import org.deegree.commons.config.DeegreeWorkspace;
+import org.deegree.commons.tom.gml.property.Property;
+import org.deegree.feature.Feature;
+import org.deegree.featureinfo.serializing.FeatureInfoGmlWriter;
+import org.deegree.featureinfo.serializing.FeatureInfoSerializer;
+import org.deegree.featureinfo.serializing.XsltFeatureInfoSerializer;
+import org.deegree.gml.GMLOutputFactory;
+import org.deegree.gml.GMLStreamWriter;
+import org.deegree.gml.GMLVersion;
+import org.slf4j.Logger;
 
 /**
  * Responsible for managing feature info output formats and their serializers.
@@ -50,5 +75,137 @@ package org.deegree.featureinfo;
  * @version $Revision: $, $Date: $
  */
 public class FeatureInfoManager {
+
+    private static final Logger LOG = getLogger( FeatureInfoManager.class );
+
+    private final HashSet<String> defaultGMLGFIFormats = new LinkedHashSet<String>();
+
+    private final HashMap<String, FeatureInfoSerializer> featureInfoSerializers = new HashMap<String, FeatureInfoSerializer>();
+
+    private final LinkedHashMap<String, String> supportedFeatureInfoFormats = new LinkedHashMap<String, String>();
+
+    public FeatureInfoManager( boolean addDefaultFormats ) {
+        if ( addDefaultFormats ) {
+            defaultGMLGFIFormats.add( "application/gml+xml; version=2.1" );
+            defaultGMLGFIFormats.add( "application/gml+xml; version=3.0" );
+            defaultGMLGFIFormats.add( "application/gml+xml; version=3.1" );
+            defaultGMLGFIFormats.add( "application/gml+xml; version=3.2" );
+            defaultGMLGFIFormats.add( "text/xml; subtype=gml/2.1.2" );
+            defaultGMLGFIFormats.add( "text/xml; subtype=gml/3.0.1" );
+            defaultGMLGFIFormats.add( "text/xml; subtype=gml/3.1.1" );
+            defaultGMLGFIFormats.add( "text/xml; subtype=gml/3.2.1" );
+            supportedFeatureInfoFormats.put( "application/vnd.ogc.gml", "" );
+            supportedFeatureInfoFormats.put( "text/xml", "" );
+            supportedFeatureInfoFormats.put( "text/plain", "" );
+            supportedFeatureInfoFormats.put( "text/html", "" );
+        }
+    }
+
+    public void addOrReplaceFormat( String format, String file ) {
+        defaultGMLGFIFormats.remove( format );
+        supportedFeatureInfoFormats.put( format, file );
+    }
+
+    public void addOrReplaceXsltFormat( String format, URL xsltUrl, GMLVersion version, DeegreeWorkspace workspace ) {
+        defaultGMLGFIFormats.remove( format );
+        XsltFeatureInfoSerializer xslt = new XsltFeatureInfoSerializer( version, xsltUrl, workspace );
+        featureInfoSerializers.put( format, xslt );
+    }
+
+    public void finalizeConfiguration() {
+        for ( String f : defaultGMLGFIFormats ) {
+            supportedFeatureInfoFormats.put( f, "" );
+        }
+    }
+
+    public void serializeFeatureInfo( FeatureInfoParams params )
+                            throws IOException {
+        String format = params.getFormat();
+        FeatureInfoSerializer serializer = featureInfoSerializers.get( format );
+        if ( serializer != null ) {
+            serializer.serialize( params.getNsBindings(), params.getFeatureCollection(), params.getOutputStream() );
+            return;
+        }
+
+        String fiFile = supportedFeatureInfoFormats.get( format );
+        if ( !fiFile.isEmpty() ) {
+            runTemplate( params.getOutputStream(), fiFile, params.getFeatureCollection(), params.isWithGeometries() );
+            return;
+        }
+
+        handleGmlOutput( format, params );
+        if ( format.equalsIgnoreCase( "text/plain" ) ) {
+            PrintWriter out = new PrintWriter( new OutputStreamWriter( params.getOutputStream(), "UTF-8" ) );
+            for ( Feature f : params.getFeatureCollection() ) {
+                out.println( f.getName().getLocalPart() + ":" );
+                for ( Property p : f.getProperties() ) {
+                    out.println( "  " + p.getName().getLocalPart() + ": " + p.getValue() );
+                }
+                out.println();
+            }
+            out.close();
+        }
+
+        if ( format.equalsIgnoreCase( "text/html" ) ) {
+            runTemplate( params.getOutputStream(), null, params.getFeatureCollection(), params.isWithGeometries() );
+        }
+    }
+
+    private void handleGmlOutput( String format, FeatureInfoParams params ) {
+        if ( format.equalsIgnoreCase( "application/vnd.ogc.gml" ) || format.equalsIgnoreCase( "text/xml" )
+             || defaultGMLGFIFormats.contains( format.toLowerCase() ) ) {
+            try {
+                XMLStreamWriter xmlWriter = params.getXmlWriter();
+
+                // for more than just quick 'hacky' schemaLocation attributes one should use a proper WFS
+                HashMap<String, String> bindings = new HashMap<String, String>();
+                String ns = determineNamespace( params );
+                if ( ns != null ) {
+                    bindings.put( ns, params.getSchemaLocation() );
+                    if ( !params.getNsBindings().containsValue( ns ) ) {
+                        params.getNsBindings().put( "app", ns );
+                    }
+                }
+                if ( !params.getNsBindings().containsKey( "app" ) ) {
+                    params.getNsBindings().put( "app", "http://www.deegree.org/app" );
+                }
+                bindings.put( "http://www.opengis.net/wfs", "http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd" );
+
+                GMLVersion gmlVersion = getGmlVersion( format );
+
+                GMLStreamWriter gmlWriter = GMLOutputFactory.createGMLStreamWriter( gmlVersion, xmlWriter );
+                gmlWriter.setOutputCrs( params.getCrs() );
+                gmlWriter.setNamespaceBindings( params.getNsBindings() );
+                gmlWriter.setExportGeometries( params.isWithGeometries() );
+                new FeatureInfoGmlWriter( gmlVersion ).export( params.getFeatureCollection(), gmlWriter,
+                                                               ns == null ? params.getSchemaLocation() : null, bindings );
+            } catch ( Throwable e ) {
+                LOG.warn( "Error when writing GetFeatureInfo GML response '{}'.", e.getLocalizedMessage() );
+                LOG.trace( "Stack trace:", e );
+            }
+        }
+    }
+
+    private String determineNamespace( FeatureInfoParams params ) {
+        String ns = params.getFeatureType() == null ? null : params.getFeatureType().getName().getNamespaceURI();
+        if ( ns != null && ns.isEmpty() ) {
+            ns = null;
+        }
+        return ns;
+    }
+
+    private GMLVersion getGmlVersion( String format ) {
+        GMLVersion gmlVersion = GMLVersion.GML_2;
+        if ( format.endsWith( "3.0" ) || format.endsWith( "3.0.1" ) ) {
+            gmlVersion = GMLVersion.GML_30;
+        }
+        if ( format.endsWith( "3.1" ) || format.endsWith( "3.1.1" ) ) {
+            gmlVersion = GMLVersion.GML_31;
+        }
+        if ( format.endsWith( "3.2" ) || format.endsWith( "3.2.1" ) ) {
+            gmlVersion = GMLVersion.GML_32;
+        }
+        return gmlVersion;
+    }
 
 }
---
+++ deegree-core/deegree-core-featureinfo/src/main/java/org/deegree/featureinfo/templating/TemplatingUtils.java
@@ -0,0 +1,104 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2012 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+ and
+ - Occam Labs UG (haftungsbeschränkt) -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ Occam Labs UG (haftungsbeschränkt)
+ Godesberger Allee 139, 53175 Bonn
+ Germany
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.featureinfo.templating;
+
+import static java.util.Collections.singletonList;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import org.deegree.feature.FeatureCollection;
+import org.deegree.featureinfo.FeatureInfoManager;
+import org.deegree.featureinfo.templating.java_cup.runtime.Symbol;
+import org.deegree.featureinfo.templating.lang.PropertyTemplateCall;
+import org.slf4j.Logger;
+
+/**
+ * Utility method to run a template against a feature collection.
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: stranger $
+ * 
+ * @version $Revision: $, $Date: $
+ */
+public class TemplatingUtils {
+
+    private static final Logger LOG = getLogger( TemplatingUtils.class );
+
+    public static void runTemplate( OutputStream response, String fiFile, FeatureCollection col, boolean geometries )
+                            throws IOException {
+        PrintWriter out = new PrintWriter( new OutputStreamWriter( response, "UTF-8" ) );
+
+        try {
+            InputStream in;
+            if ( fiFile == null ) {
+                in = FeatureInfoManager.class.getResourceAsStream( "html.gfi" );
+            } else {
+                in = new FileInputStream( fiFile );
+            }
+
+            Symbol s = new TemplatingParser( new TemplatingLexer( new InputStreamReader( in, "UTF-8" ) ) ).parse();
+            @SuppressWarnings(value = "unchecked")
+            HashMap<String, Object> tmpl = (HashMap<String, Object>) s.value;
+            StringBuilder sb = new StringBuilder();
+            new PropertyTemplateCall( "start", singletonList( "*" ), false ).eval( sb, tmpl, col, geometries );
+            out.println( sb.toString() );
+        } catch ( Throwable e ) {
+            if ( fiFile == null ) {
+                LOG.error( "Could not load internal template for GFI response." );
+            } else {
+                LOG.error( "Could not load template '{}' for GFI response.", fiFile );
+            }
+            LOG.trace( "Stack trace:", e );
+        } finally {
+            out.close();
+        }
+    }
+
+}
