9c92c4842f45b20feff7f0ab76c763e190a9f8aa
Torsten <torsten.friebe@googlemail.com>
2013-01-05 03:44:14 +0100

    jrcportal #974 - fix resource handling

--- deegree-datastores/deegree-mdstores/deegree-mdstore-iso/src/main/java/org/deegree/metadata/iso/persistence/ISOMetadataStore.java
+++ deegree-datastores/deegree-mdstores/deegree-mdstore-iso/src/main/java/org/deegree/metadata/iso/persistence/ISOMetadataStore.java
@@ -287,7 +287,7 @@ public class ISOMetadataStore implements MetadataStore<ISORecord> {
     public MetadataResultSet<ISORecord> getRecordById( final List<String> idList, final QName[] recordTypeNames )
                             throws MetadataStoreException {
         LOG.debug( Messages.getMessage( "INFO_EXEC", "getRecordsById" ) );
-        Connection connection;
+        Connection connection = null;
 		try {
 			connection = getConnection();
 			return new QueryHelper( dialect, getQueryables() ).executeGetRecordById( idList, connection );
