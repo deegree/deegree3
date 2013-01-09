aa784c902baae5cbec17ed0d7f3c20b143048a52
Andreas Schmitz <schmitz@occamlabs.de>
2013-01-09 11:02:17 +0100

    fixing remoteows tests wrt different jdk png encodings

--- deegree-tests/deegree-wms-remoteows-tests/src/test/java/org/deegree/services/wms/RemoteWMSIntegrationTest.java
+++ deegree-tests/deegree-wms-remoteows-tests/src/test/java/org/deegree/services/wms/RemoteWMSIntegrationTest.java
@@ -52,6 +52,7 @@ import java.io.InputStream;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.deegree.commons.utils.test.IntegrationTestUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -96,9 +97,12 @@ public class RemoteWMSIntegrationTest {
         base += "/deegree-wms-remoteows-tests/services" + request;
         LOG.info( "Requesting {}", base );
         InputStream in = retrieve( STREAM, base );
+        byte[] bsin = IOUtils.toByteArray( in );
+        in.close();
         double sim = 0;
 
         for ( byte[] response : this.response ) {
+            in = new ByteArrayInputStream( bsin );
             sim = Math.max( sim, determineSimilarity( in, new ByteArrayInputStream( response ) ) );
         }
         Assert.assertEquals( "Images are not similar enough (" + base + ").", 1.0, sim, 0.01 );
