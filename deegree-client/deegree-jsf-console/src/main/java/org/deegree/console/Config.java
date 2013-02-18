02bb069e98d25740ac1587e81175312d33bcd1d0
Andreas Schmitz <schmitz@occamlabs.de>
2013-02-18 14:50:59 +0100

    fixed editing of service metadata if contained in subdirectories

--- deegree-client/deegree-jsf-console/src/main/java/org/deegree/console/Config.java
+++ deegree-client/deegree-jsf-console/src/main/java/org/deegree/console/Config.java
@@ -220,7 +220,7 @@ public class Config implements Comparable<Config> {
 
     public String editMetadata()
                             throws IOException {
-        File metadataLocation = new File( location.getParent(), id + "_metadata.xml" );
+        File metadataLocation = new File( location.getParent(), new File( id ).getName() + "_metadata.xml" );
         Config metadataConfig = new Config( metadataLocation, METADATA_SCHEMA_URL, METADATA_EXAMPLE_URL,
                                             "/console/webservices/webservices" );
         return metadataConfig.edit();
