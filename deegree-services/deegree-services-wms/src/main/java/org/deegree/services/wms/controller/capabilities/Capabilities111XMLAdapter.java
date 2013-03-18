1a74da0e6a8b901e99e09e197a5b49ef3550b0c5
Andreas Schmitz <schmitz@occamlabs.de>
2013-03-18 11:10:41 +0100

    added synthetic root layer bounding box when multiple themes are configured

--- deegree-services/deegree-services-wms/src/main/java/org/deegree/services/wms/controller/capabilities/Capabilities111XMLAdapter.java
+++ deegree-services/deegree-services-wms/src/main/java/org/deegree/services/wms/controller/capabilities/Capabilities111XMLAdapter.java
@@ -39,6 +39,7 @@ package org.deegree.services.wms.controller.capabilities;
 import static org.deegree.commons.xml.CommonNamespaces.XLINK_PREFIX;
 import static org.deegree.commons.xml.CommonNamespaces.XLNNS;
 import static org.deegree.layer.dims.Dimension.formatDimensionValueList;
+import static org.deegree.services.wms.controller.capabilities.WmsCapabilities111SpatialMetadataWriter.writeSrsAndEnvelope;
 
 import java.util.List;
 import java.util.Map;
@@ -52,12 +53,15 @@ import org.deegree.commons.ows.metadata.ServiceIdentification;
 import org.deegree.commons.ows.metadata.ServiceProvider;
 import org.deegree.commons.utils.Pair;
 import org.deegree.commons.xml.XMLAdapter;
+import org.deegree.geometry.metadata.SpatialMetadata;
 import org.deegree.layer.dims.Dimension;
+import org.deegree.layer.metadata.LayerMetadata;
 import org.deegree.services.metadata.OWSMetadataProvider;
 import org.deegree.services.wms.MapService;
 import org.deegree.services.wms.controller.WMSController;
 import org.deegree.style.se.unevaluated.Style;
 import org.deegree.theme.Theme;
+import org.deegree.theme.Themes;
 
 /**
  * <code>Capabilities111XMLAdapter</code>
@@ -157,9 +161,24 @@ public class Capabilities111XMLAdapter extends XMLAdapter {
         } else {
             // synthetic root layer needed
             writer.writeStartElement( "Layer" );
-            // TODO
-            writer.writeAttribute( "queryable", "1" );
             writeElement( writer, "Title", "Root" );
+
+            // TODO think about a push approach instead of a pull approach
+            LayerMetadata lmd = null;
+            for ( Theme t : themes ) {
+                for ( org.deegree.layer.Layer l : Themes.getAllLayers( t ) ) {
+                    if ( lmd == null ) {
+                        lmd = l.getMetadata();
+                    } else {
+                        lmd.merge( l.getMetadata() );
+                    }
+                }
+            }
+            if ( lmd != null ) {
+                SpatialMetadata smd = lmd.getSpatialMetadata();
+                writeSrsAndEnvelope( writer, smd.getCoordinateSystems(), smd.getEnvelope() );
+            }
+
             for ( Theme t : themes ) {
                 themeWriter.writeTheme( writer, t );
             }
