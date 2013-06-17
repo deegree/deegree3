151163ec447bfc6ecf5489c5596298eb5b6396f2
Dirk Stenger <stenger@lat-lon.de>
2013-06-17 14:01:56 +0200

    Improved handling of empty date elements during parsing.

--- deegree-core/deegree-core-metadata/src/main/java/org/deegree/metadata/iso/parsing/ParseIdentificationInfo.java
+++ deegree-core/deegree-core-metadata/src/main/java/org/deegree/metadata/iso/parsing/ParseIdentificationInfo.java
@@ -593,7 +593,8 @@ public class ParseIdentificationInfo extends XMLAdapter {
                     String temporalExtentEnd = getNodeAsString( extentElem, new XPath( endXpath.toString(),
                                                                                        nsContextParseII ), null );
                     try {
-                        if ( temporalExtentBegin != null && temporalExtentEnd != null ) {
+                        if ( temporalExtentBegin != null && temporalExtentEnd != null
+                             && !"".equals( temporalExtentBegin ) && !"".equals( temporalExtentEnd ) ) {
                             tempBeg = parseDate( temporalExtentBegin );
                             tempEnd = parseDate( temporalExtentEnd );
                         }
@@ -780,7 +781,7 @@ public class ParseIdentificationInfo extends XMLAdapter {
                                                                 nsContextParseII ), null );
         Date date = null;
         try {
-            if ( revisionDateString != null ) {
+            if ( revisionDateString != null && !"".equals( revisionDateString ) ) {
                 date = parseDate( revisionDateString );
             } else {
                 date = null;
@@ -796,7 +797,7 @@ public class ParseIdentificationInfo extends XMLAdapter {
                                                                 "./gmd:date/gmd:CI_Date[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue='creation']/gmd:date/gco:DateTime | ./gmd:date/gmd:CI_Date[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue='creation']/gmd:date/gco:Date",
                                                                 nsContextParseII ), null );
         try {
-            if ( creationDateString != null ) {
+            if ( creationDateString != null && !"".equals( creationDateString ) ) {
                 date = parseDate( creationDateString );
             } else {
                 date = null;
@@ -812,7 +813,7 @@ public class ParseIdentificationInfo extends XMLAdapter {
                                                                    "./gmd:date/gmd:CI_Date[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue='publication']/gmd:date/gco:Date | ./gmd:date/gmd:CI_Date[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue='publication']/gmd:date/gco:DateTime",
                                                                    nsContextParseII ), null );
         try {
-            if ( publicationDateString != null ) {
+            if ( publicationDateString != null && !"".equals( publicationDateString ) ) {
                 date = parseDate( publicationDateString );
             } else {
                 date = null;
