882cad7ae2d16c5f2357d71303bfe740c6b7d722
copierrj <reijer.copier@idgis.nl>
2013-06-25 17:11:18 +0200

    additional debug logging

--- deegree-spring/src/main/java/org/deegree/spring/bootstrap/BootstrapApplicationContextHolderBuilder.java
+++ deegree-spring/src/main/java/org/deegree/spring/bootstrap/BootstrapApplicationContextHolderBuilder.java
@@ -4,11 +4,17 @@ import org.deegree.spring.ApplicationContextHolder;
 import org.deegree.spring.bootstrap.jaxb.BootstrapApplicationContextHolderConfig;
 import org.deegree.workspace.ResourceBuilder;
 import org.deegree.workspace.ResourceInitException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BootstrapApplicationContextHolderBuilder implements ResourceBuilder<ApplicationContextHolder> {
+    
+    private static final Logger LOG = LoggerFactory.getLogger( BootstrapApplicationContextHolderBuilder.class );
 
     private final BootstrapApplicationContextHolderMetadata metadata;
 
@@ -23,10 +29,13 @@ public class BootstrapApplicationContextHolderBuilder implements ResourceBuilder
 
     @Override
     public ApplicationContextHolder build() {
+        LOG.debug( "Building BootstrapApplicationContextHolder." );
+        
         try {
             final ConfigurableApplicationContext context;
             final String contextClass = config.getContextClass();
             if ( contextClass != null ) {
+                LOG.debug( "Using ContextClass {},", contextClass );                
                 context = new AnnotationConfigApplicationContext( Class.forName( contextClass ) );
             } else {
                 final String contextConfigLocation = config.getContextConfigLocation();
@@ -34,12 +43,14 @@ public class BootstrapApplicationContextHolderBuilder implements ResourceBuilder
                     throw new ResourceInitException(
                                                      "Both ContextClass and ContextConfigLocation are missing from BootstrapApplicationContextHolderConfig" );
                 }
-
+                
+                LOG.debug( "Using ContextConfigLocation {},", contextConfigLocation );
                 context = new GenericXmlApplicationContext( contextConfigLocation );
             }
 
             return new ApplicationContextHolder( metadata, context );
         } catch ( Exception e ) {
+            LOG.debug( "Couldn't build BootstrapApplicationContextHolder", e );
             throw new ResourceInitException( "Couldn't build BootstrapApplicationContextHolder", e );
         }
     }
