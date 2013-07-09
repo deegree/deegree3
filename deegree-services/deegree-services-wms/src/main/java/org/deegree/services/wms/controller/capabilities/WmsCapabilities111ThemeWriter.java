7d266ca5a8db60b30a87bfafc25c5f53bafcdcfe
copierrj <reijer.copier@idgis.nl>
2013-07-09 15:40:12 +0200

    Support for additional keywords lists and vocabulary attribute added.

--- deegree-services/deegree-services-wms/src/main/java/org/deegree/services/wms/controller/capabilities/WmsCapabilities111ThemeWriter.java
+++ deegree-services/deegree-services-wms/src/main/java/org/deegree/services/wms/controller/capabilities/WmsCapabilities111ThemeWriter.java
@@ -146,14 +146,16 @@ class WmsCapabilities111ThemeWriter {
 
     private void writeKeywords( XMLStreamWriter writer, LayerMetadata md, LayerMetadata lmd )
                             throws XMLStreamException {
-        List<Pair<List<LanguageString>, CodeType>> kws = md.getDescription().getKeywords();
-        if ( lmd != null && ( kws == null || kws.isEmpty() || kws.get( 0 ).first.isEmpty() ) ) {
-            kws = lmd.getDescription().getKeywords();
+        List<Pair<List<LanguageString>, CodeType>> kwsl = md.getDescription().getKeywords();
+        if ( lmd != null && ( kwsl == null || kwsl.isEmpty() || kwsl.get( 0 ).first.isEmpty() ) ) {
+            kwsl = lmd.getDescription().getKeywords();
         }
-        if ( kws != null && !kws.isEmpty() && !kws.get( 0 ).first.isEmpty() ) {
+        if ( kwsl != null && !kwsl.isEmpty() && !kwsl.get( 0 ).first.isEmpty() ) {
             writer.writeStartElement( "KeywordList" );
-            for ( LanguageString ls : kws.get( 0 ).first ) {
-                writeElement( writer, "Keyword", ls.getString() );
+            for ( Pair<List<LanguageString>, CodeType> kws : kwsl ) {
+                for ( LanguageString ls : kws.first ) {
+                    writeElement( writer, "Keyword", ls.getString() );
+                }
             }
             writer.writeEndElement();
         }
