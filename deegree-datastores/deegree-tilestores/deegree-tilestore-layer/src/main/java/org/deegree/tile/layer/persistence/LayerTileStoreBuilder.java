185e157886e7cc53b2ad20b90ae024efcf35213a
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2013-11-23 17:32:10 +0100

    added layer based tile store

---
+++ deegree-datastores/deegree-tilestores/deegree-tilestore-layer/src/main/java/org/deegree/tile/layer/persistence/LayerTileStoreBuilder.java
@@ -0,0 +1,105 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree
+ Copyright (C) 2001-2013 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+ and
+ - Occam Labs UG (haftungsbeschränkt) -
+ and others
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ e-mail: info@deegree.org
+ website: http://www.deegree.org/
+----------------------------------------------------------------------------*/
+package org.deegree.tile.layer.persistence;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.deegree.layer.Layer;
+import org.deegree.layer.persistence.LayerStore;
+import org.deegree.layer.persistence.LayerStoreProvider;
+import org.deegree.tile.DefaultTileDataSet;
+import org.deegree.tile.TileDataLevel;
+import org.deegree.tile.TileDataSet;
+import org.deegree.tile.TileMatrix;
+import org.deegree.tile.TileMatrixSet;
+import org.deegree.tile.layer.persistence.jaxb.LayerTileStoreJAXB;
+import org.deegree.tile.persistence.GenericTileStore;
+import org.deegree.tile.persistence.TileStore;
+import org.deegree.tile.tilematrixset.TileMatrixSetProvider;
+import org.deegree.workspace.ResourceBuilder;
+import org.deegree.workspace.ResourceInitException;
+import org.deegree.workspace.ResourceMetadata;
+import org.deegree.workspace.Workspace;
+
+/**
+ * This class is responsible for building geotiff tile stores.
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * 
+ * @since 3.4
+ */
+public class LayerTileStoreBuilder implements ResourceBuilder<TileStore> {
+
+    private Workspace workspace;
+
+    private ResourceMetadata<TileStore> metadata;
+
+    private LayerTileStoreJAXB cfg;
+
+    public LayerTileStoreBuilder( Workspace workspace, ResourceMetadata<TileStore> metadata, LayerTileStoreJAXB cfg ) {
+        this.workspace = workspace;
+        this.metadata = metadata;
+        this.cfg = cfg;
+    }
+
+    @Override
+    public TileStore build() {
+        try {
+            Map<String, TileDataSet> map = new LinkedHashMap<String, TileDataSet>();
+            for ( org.deegree.tile.layer.persistence.jaxb.LayerTileStoreJAXB.TileDataSet tds : cfg.getTileDataSet() ) {
+                String id = tds.getIdentifier();
+                String name = tds.getLayerName();
+                String storeid = tds.getLayerStoreId();
+                String tmsid = tds.getTileMatrixSetId();
+
+                TileMatrixSet tms = workspace.getResource( TileMatrixSetProvider.class, tmsid );
+                LayerStore lstore = workspace.getResource( LayerStoreProvider.class, storeid );
+                Layer lay = lstore.get( name );
+
+                List<TileDataLevel> levels = new ArrayList<TileDataLevel>();
+                for ( TileMatrix tm : tms.getTileMatrices() ) {
+                    LayerTileDataLevel lev = new LayerTileDataLevel( tm, lay );
+                    levels.add( lev );
+                }
+
+                Collections.reverse( levels );
+
+                DefaultTileDataSet set = new DefaultTileDataSet( levels, tms, "image/png" );
+                map.put( id, set );
+            }
+            return new GenericTileStore( map, metadata );
+        } catch ( Exception e ) {
+            throw new ResourceInitException( "Unable to build GeoTiff tile store: " + e.getLocalizedMessage(), e );
+        }
+    }
+
+}
