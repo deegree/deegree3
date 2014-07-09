24efaa5138ac2e3ce18d98e057cfdc32e439a5fe
Markus Schneider <schneider@occamlabs.de>
2014-07-09 20:42:01 +0200

    Added javadoc

--- deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/RequestWatchdog.java
+++ deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/RequestWatchdog.java
@@ -1,3 +1,37 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2014 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
 package org.deegree.services.controller.watchdog;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -6,8 +40,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.deegree.services.OWS;
 import org.slf4j.Logger;
 
+/**
+ * Periodically checks for timed-out {@link OWS} requests and interrupts the request threads.
+ * 
+ * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
+ * 
+ * @since 3.4
+ */
 public class RequestWatchdog implements Runnable {
 
     private static final Logger LOG = getLogger( RequestWatchdog.class );
@@ -18,22 +60,34 @@ public class RequestWatchdog implements Runnable {
 
     @Override
     public void run() {
-        LOG.debug( "RequestWatchDog starting" );
+        LOG.debug( "Starting main loop" );
         while ( true ) {
             interruptAndRemoveTimedOutRequests();
             try {
                 Thread.sleep( CHECK_INTERVAL_MILLIS );
             } catch ( InterruptedException e ) {
-                LOG.debug( "RequestWatchDog terminating" );
+                LOG.debug( "Interrupted. Exiting" );
                 return;
             }
         }
     }
 
+    /**
+     * Adds the given request to be watched for time-out.
+     * 
+     * @param request
+     *            request to watch, must not be <code>null</code>
+     */
     public void watch( final WatchedRequest request ) {
         watchedRequests.add( request );
     }
 
+    /**
+     * Removes the given request from the watched requests.
+     * 
+     * @param request
+     *            request to remove, must not be <code>null</code>
+     */
     public void unwatch( final WatchedRequest request ) {
         watchedRequests.remove( request );
     }
--- deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/WatchedRequest.java
+++ deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/WatchedRequest.java
@@ -1,14 +1,59 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2014 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
 package org.deegree.services.controller.watchdog;
 
+/**
+ * A request that is watched by the {@link RequestWatchdog}.
+ * 
+ * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
+ * 
+ * @since 3.4
+ */
 public class WatchedRequest {
-    
+
     private final Thread thread;
-    
+
     private final long stopTimeInMillis;
 
     /**
+     * Creates a new {@link WatchedRequest} instance.
+     * 
      * @param thread
+     *            thread of the request to watch, must not be <code>null</code>
      * @param stopTimeInMillis
+     *            time (in milliseconds) when the request will be interrupted
      */
     public WatchedRequest( final Thread thread, final long stopTimeInMillis ) {
         this.thread = thread;
@@ -16,17 +61,21 @@ public class WatchedRequest {
     }
 
     /**
-     * @return the thread
+     * Returns the thread that is executing the request.
+     * 
+     * @return the thread, never <code>null</code>
      */
     Thread getThread() {
         return thread;
     }
 
     /**
-     * @return the stopTimeInMillis
+     * Returns the time when the request needs to be stopped forcefully.
+     * 
+     * @return time (in milliseconds) when the request needs to be interrupted
      */
     long getStopTimeInMillis() {
         return stopTimeInMillis;
     }
-    
+
 }
