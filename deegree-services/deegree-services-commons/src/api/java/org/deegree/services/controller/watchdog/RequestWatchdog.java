b8185fdde18247fb0f48c78719242958756165eb
Markus Schneider <schneider@occamlabs.de>
2014-07-10 00:43:14 +0200

    Added per-instance, per request timeouts

--- deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/RequestWatchdog.java
+++ deegree-services/deegree-services-commons/src/api/java/org/deegree/services/controller/watchdog/RequestWatchdog.java
@@ -36,73 +36,77 @@ package org.deegree.services.controller.watchdog;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.deegree.services.OWS;
+import org.deegree.services.jaxb.controller.DeegreeServiceControllerType.RequestTimeoutMilliseconds;
 import org.slf4j.Logger;
 
 /**
- * Periodically checks for timed-out {@link OWS} requests and interrupts the request threads.
+ * Watchdog for enforcing time-outs on {@link OWS} requests.
  * 
  * @author <a href="mailto:schneider@occamlabs.de">Markus Schneider</a>
  * 
  * @since 3.4
  */
-public class RequestWatchdog implements Runnable {
+public class RequestWatchdog {
 
     private static final Logger LOG = getLogger( RequestWatchdog.class );
 
-    private static final long CHECK_INTERVAL_MILLIS = 500;
+    private final WatchdogDaemon daemon = new WatchdogDaemon();
 
-    private List<WatchedRequest> watchedRequests = Collections.synchronizedList( new ArrayList<WatchedRequest>() );
+    private final Thread daemonThread;
 
-    @Override
-    public void run() {
-        LOG.debug( "Starting main loop" );
-        while ( true ) {
-            interruptAndRemoveTimedOutRequests();
-            try {
-                Thread.sleep( CHECK_INTERVAL_MILLIS );
-            } catch ( InterruptedException e ) {
-                LOG.debug( "Interrupted. Exiting" );
-                return;
-            }
+    private final WatchedRequestFactory watchedRequestFactory;
+
+    /**
+     * Creates a new {@link RequestWatchdog} for the given time-out configuration.
+     * 
+     * @param timeoutConfigs
+     *            time-outs values per request type and service
+     */
+    public RequestWatchdog( final List<RequestTimeoutMilliseconds> timeoutConfigs ) {
+        daemonThread = new Thread( daemon, "RequestWatchdog" );
+        daemonThread.setDaemon( true );
+        watchedRequestFactory = new WatchedRequestFactory( timeoutConfigs );
+    }
+
+    /**
+     * Adds the current thread to the watchdog, enforcing the time-out configured for the service and request type.
+     * 
+     * @param serviceId
+     *            service identifier (e.g. "wms-ad"), must not be <code>null</code>
+     * @param requestType
+     *            request (e.g. "GetMap"), must not be <code>null</code>
+     */
+    public void watchCurrentThread( final String serviceId, final String requestType ) {
+        final WatchedRequest request = watchedRequestFactory.create( serviceId, requestType );
+        if ( request != null ) {
+            daemon.watch( request );
         }
     }
 
     /**
-     * Adds the given request to be watched for time-out.
-     * 
-     * @param request
-     *            request to watch, must not be <code>null</code>
+     * Removes the request associated with the current thread from the watchdog.
      */
-    public void watch( final WatchedRequest request ) {
-        watchedRequests.add( request );
+    public void unwatchCurrentThread() {
+        daemon.unwatch();
     }
 
     /**
-     * Removes the given request from the watched requests.
-     * 
-     * @param request
-     *            request to remove, must not be <code>null</code>
+     * Initializes a {@link WatchdogDaemon} instance.
      */
-    public void unwatch( final WatchedRequest request ) {
-        watchedRequests.remove( request );
+    public void init() {
+        LOG.info ("Starting");
+        daemonThread.start();
     }
 
-    private void interruptAndRemoveTimedOutRequests() {
-        final List<WatchedRequest> requestSnapshot = new ArrayList<WatchedRequest>( watchedRequests );
-        LOG.debug( "Checking for timed-out requests. Number of active requests: " + requestSnapshot.size() );
-        final long currentTimeMillis = System.currentTimeMillis();
-        for ( final WatchedRequest request : requestSnapshot ) {
-            if ( currentTimeMillis > request.getStopTimeInMillis() ) {
-                LOG.warn( "Time-out detected for request " + request + ". Interrupting request thread." );
-                request.getThread().interrupt();
-                watchedRequests.remove( request );
-            }
-        }
+    /**
+     * Destroys the {@link WatchdogDaemon}.
+     */    
+    public void destroy() {
+        LOG.info ("Stopping");
+        daemonThread.interrupt();
     }
 
 }
