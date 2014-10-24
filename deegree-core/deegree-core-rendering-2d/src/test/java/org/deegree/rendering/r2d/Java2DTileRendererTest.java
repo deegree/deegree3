1c92d4abeb1b62ec6274b09942f6ab7e6205427d
Dirk Stenger <stenger@lat-lon.de>
2014-10-24 17:07:51 +0200

    Added tests for Java2DTileRenderer

---
+++ deegree-core/deegree-core-rendering-2d/src/test/java/org/deegree/rendering/r2d/Java2DTileRendererTest.java
@@ -0,0 +1,44 @@
+package org.deegree.rendering.r2d;
+
+import org.deegree.geometry.primitive.Point;
+import org.deegree.geometry.standard.DefaultEnvelope;
+import org.deegree.tile.Tile;
+import org.junit.Test;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+import static java.awt.image.BufferedImage.TYPE_4BYTE_ABGR;
+import static org.mockito.Mockito.mock;
+
+public class Java2DTileRendererTest {
+
+    private final Java2DTileRenderer renderer = createRenderer();
+
+    @Test
+    public void testRenderWithEmptyIteratorShouldNotFail()
+                            throws Exception {
+        Iterator<Tile> emptyIterator = Collections.emptyIterator();
+        renderer.render( emptyIterator );
+    }
+
+    @Test
+    public void testRenderWithIteratorWithNullValueShouldNotFail()
+                            throws Exception {
+        ArrayList<Tile> list = new ArrayList<Tile>();
+        list.add( null );
+        Iterator<Tile> iteratorWithNullValue = list.iterator();
+        renderer.render( iteratorWithNullValue );
+    }
+
+    private Java2DTileRenderer createRenderer() {
+        BufferedImage image = new BufferedImage( 500, 500, TYPE_4BYTE_ABGR );
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        return new Java2DTileRenderer( graphics, 500, 500,
+                                       new DefaultEnvelope( mock( Point.class ), mock( Point.class ) ) );
+    }
+
+}
