2873e23f9ee380653cadec828521dfd5e91f6308
Markus Schneider <schneider@occamlabs.de>
2014-11-27 13:52:44 +0100

    Check for duplication of gml ids

--- deegree-datastores/deegree-featurestores/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/transaction/FeatureUpdater.java
+++ deegree-datastores/deegree-featurestores/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/transaction/FeatureUpdater.java
@@ -1,3 +1,37 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2014 by:
+ Department of Geography, University of Bonn
+ and
+ lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
 package org.deegree.feature.persistence.transaction;
 
 import static org.deegree.protocol.wfs.transaction.action.UpdateAction.REMOVE;
@@ -18,6 +52,7 @@ import org.deegree.feature.persistence.FeatureStoreException;
 import org.deegree.feature.property.GenericProperty;
 import org.deegree.geometry.Geometry;
 import org.deegree.geometry.primitive.Surface;
+import org.deegree.gml.utils.GMLObjectWalker;
 import org.deegree.protocol.wfs.transaction.action.ParsedPropertyReplacement;
 import org.deegree.protocol.wfs.transaction.action.UpdateAction;
 
@@ -33,11 +68,12 @@ public class FeatureUpdater {
      *            feature to be updated, must not be <code>null</code>
      * @param replacementProps
      *            properties to be replaced, must not be <code>null</code>
+     * @throws FeatureStoreException
+     *             if the update would result in an invalid feature instance
      */
     public void update( final Feature feature, final List<ParsedPropertyReplacement> replacementProps )
                             throws FeatureStoreException {
-
-        for ( ParsedPropertyReplacement replacement : replacementProps ) {
+        for ( final ParsedPropertyReplacement replacement : replacementProps ) {
             Property prop = replacement.getNewValue();
             UpdateAction updateAction = replacement.getUpdateAction();
             GenericProperty newProp = new GenericProperty( prop.getType(), null );
@@ -48,11 +84,9 @@ public class FeatureUpdater {
             } else if ( updateAction == null ) {
                 updateAction = REMOVE;
             }
-
             if ( updateAction == null ) {
                 updateAction = REPLACE;
             }
-
             int idx = replacement.getIndex();
             switch ( updateAction ) {
             case INSERT_AFTER:
@@ -105,13 +139,13 @@ public class FeatureUpdater {
                 break;
             }
             validateProperties( feature, feature.getProperties() );
+            checkForDuplicateIds( feature );
         }
     }
 
-    private void validateProperties( Feature feature, List<Property> props ) {
+    private void validateProperties( final Feature feature, final List<Property> props ) {
         Map<PropertyType, Integer> ptToCount = new HashMap<PropertyType, Integer>();
         for ( Property prop : props ) {
-
             if ( prop.getValue() instanceof Geometry ) {
                 Geometry geom = (Geometry) prop.getValue();
                 if ( geom != null ) {
@@ -154,6 +188,16 @@ public class FeatureUpdater {
                              + "' must be present no more than " + pt.getMaxOccurs() + " time(s).";
                 throw new InvalidParameterValueException( msg );
             }
+        }
+    }
+
+    private void checkForDuplicateIds( final Feature feature )
+                            throws FeatureStoreException {
+        final IdChecker idChecker = new IdChecker();
+        try {
+            new GMLObjectWalker( idChecker ).traverse( feature );
+        } catch ( final Exception e ) {
+            throw new FeatureStoreException( e.getMessage() );
         }
     }
 
---
+++ deegree-datastores/deegree-featurestores/deegree-featurestore-commons/src/main/java/org/deegree/feature/persistence/transaction/IdChecker.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2014 by:
+ Department of Geography, University of Bonn
+ and
+ lat/lon GmbH
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.feature.persistence.transaction;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.deegree.commons.tom.Reference;
+import org.deegree.feature.Feature;
+import org.deegree.geometry.Geometry;
+import org.deegree.gml.utils.GMLObjectVisitor;
+
+class IdChecker implements GMLObjectVisitor {
+
+    final Set<String> ids = new HashSet<String>();
+
+    @Override
+    public boolean visitGeometry( final Geometry geom ) {
+        checkForDuplication( geom.getId() );
+        return true;
+    }
+
+    @Override
+    public boolean visitFeature( final Feature feature ) {
+        checkForDuplication( feature.getId() );
+        return true;
+    }
+
+    @Override
+    public boolean visitReference( final Reference<?> ref ) {
+        return false;
+    }
+
+    private void checkForDuplication( final String id ) {
+        if ( id != null ) {
+            if ( ids.contains( id ) ) {
+                final String msg = "Duplication of object id '" + id + "'.";
+                throw new IllegalArgumentException( msg );
+            }
+            ids.add( id );
+        }
+    }
+
+}
