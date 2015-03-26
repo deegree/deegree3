137232c1e6f819ca203cebab4a55f387c25139b4
Markus Schneider <schneider@occamlabs.de>
2015-03-26 18:06:57 +0100

    Fixed xpath evaluation on properties of gml:TimeInstant/gml:TimePeriod

--- deegree-core/deegree-core-base/src/main/java/org/deegree/feature/xpath/node/TimePositionAdapter.java
+++ deegree-core/deegree-core-base/src/main/java/org/deegree/feature/xpath/node/TimePositionAdapter.java
@@ -1,5 +1,7 @@
 package org.deegree.feature.xpath.node;
 
+import static org.deegree.commons.tom.primitive.BaseType.STRING;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,6 +12,7 @@ import javax.xml.namespace.QName;
 import org.deegree.commons.tom.TypedObjectNode;
 import org.deegree.commons.tom.genericxml.GenericXMLElement;
 import org.deegree.commons.tom.gml.property.Property;
+import org.deegree.commons.tom.primitive.PrimitiveType;
 import org.deegree.commons.tom.primitive.PrimitiveValue;
 import org.deegree.feature.property.GenericProperty;
 import org.deegree.time.position.TimePosition;
@@ -18,6 +21,11 @@ public class TimePositionAdapter {
 
     public Property getAsXMLElement( final QName name, final TimePosition timeInstant ) {
         final Map<QName, PrimitiveValue> attrs = new HashMap<QName, PrimitiveValue>();
+        if ( timeInstant.getIndeterminatePosition() != null ) {
+            final String indeterminateValue = timeInstant.getIndeterminatePosition().name().toLowerCase();
+            final PrimitiveValue pv = new PrimitiveValue( indeterminateValue, new PrimitiveType( STRING ) );
+            attrs.put( new QName( "indeterminatePosition" ), pv );
+        }
         final PrimitiveValue value = new PrimitiveValue( timeInstant.getValue() );
         final List<TypedObjectNode> children = new ArrayList<TypedObjectNode>();
         children.add( value );
---
+++ deegree-core/deegree-core-base/src/test/resources/org/deegree/time/gml/reader/time_period_indeterminate.gml
@@ -0,0 +1,4 @@
+<TimePeriod gml:id="p1" xmlns="http://www.opengis.net/gml/3.2" xmlns:gml="http://www.opengis.net/gml/3.2">
+  <beginPosition>2001-05-23</beginPosition>
+  <endPosition indeterminatePosition="unknown" />
+</TimePeriod>
