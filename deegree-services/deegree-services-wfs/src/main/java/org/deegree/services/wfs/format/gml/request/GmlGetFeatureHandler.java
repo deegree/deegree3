1198be96478e3af244de832a10568e6ece6acc9e
Lyn Goltz <goltz@lat-lon.de>
2015-11-18 11:42:17 +0100

    write state to GetFeature response

--- deegree-services/deegree-services-wfs/src/main/java/org/deegree/services/wfs/format/gml/request/GmlGetFeatureHandler.java
+++ deegree-services/deegree-services-wfs/src/main/java/org/deegree/services/wfs/format/gml/request/GmlGetFeatureHandler.java
@@ -452,7 +452,7 @@ public class GmlGetFeatureHandler extends AbstractGmlRequestHandler {
                     if ( featuresSkipped < startIndex ) {
                         featuresSkipped++;
                     } else {
-                        writeMemberFeature( member, gmlStream, xmlStream, resolveState, featureMemberEl );
+                        writeMemberFeature( member, gmlStream, xmlStream, resolveState, featureMemberEl, wfsVersion );
                         featuresAdded++;
                     }
                 }
@@ -516,7 +516,7 @@ public class GmlGetFeatureHandler extends AbstractGmlRequestHandler {
         // retrieve and write result features
         GmlXlinkOptions resolveState = gmlStream.getReferenceResolveStrategy().getResolveOptions();
         for ( Feature member : allFeatures ) {
-            writeMemberFeature( member, gmlStream, xmlStream, resolveState, featureMemberEl );
+            writeMemberFeature( member, gmlStream, xmlStream, resolveState, featureMemberEl, wfsVersion );
         }
     }
 
