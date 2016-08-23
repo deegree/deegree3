1bb01b67bf94edfea5552311dd0719b962294eb4
Lyn Elisa Goltz <goltz@lat-lon.de>
2016-08-23 17:31:53 +0200

    write SQLFeatureStore configuration 3.4

--- deegree-datastores/deegree-featurestores/deegree-featurestore-sql/src/main/java/org/deegree/feature/persistence/sql/config/SQLFeatureStoreConfigWriter.java
+++ deegree-datastores/deegree-featurestores/deegree-featurestore-sql/src/main/java/org/deegree/feature/persistence/sql/config/SQLFeatureStoreConfigWriter.java
@@ -87,7 +87,7 @@ public class SQLFeatureStoreConfigWriter {
 
     private static final String CONFIG_NS = "http://www.deegree.org/datasource/feature/sql";
 
-    private static final String SCHEMA_LOCATION = "http://www.deegree.org/datasource/feature/sql http://schemas.deegree.org/datasource/feature/sql/3.1.0/sql.xsd";
+    private static final String SCHEMA_LOCATION = "http://www.deegree.org/datasource/feature/sql http://schemas.deegree.org/datasource/feature/sql/3.4.0/sql.xsd";
 
     private final MappedAppSchema schema;
 
@@ -113,7 +113,7 @@ public class SQLFeatureStoreConfigWriter {
                             throws XMLStreamException {
 
         writer.writeStartElement( "SQLFeatureStore" );
-        writer.writeAttribute( "configVersion", "3.1.0" );
+        writer.writeAttribute( "configVersion", "3.4.0" );
         writer.writeNamespace( DEFAULT_NS_PREFIX, CONFIG_NS );
         writer.writeNamespace( "xsi", XSINS );
         writer.writeAttribute( XSINS, "schemaLocation", SCHEMA_LOCATION );
