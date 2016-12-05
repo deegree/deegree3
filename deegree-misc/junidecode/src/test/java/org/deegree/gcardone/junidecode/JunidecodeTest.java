ae9ca5ce9cf1a999752c6da2b463bda6fdcf303f
Lyn Elisa Goltz <goltz@lat-lon.de>
2016-12-05 09:26:25 +0100

    #4171 - renamned package

--- deegree-misc/junidecode/src/test/java/org/deegree/gcardone/junidecode/JunidecodeTest.java
+++ deegree-misc/junidecode/src/test/java/org/deegree/gcardone/junidecode/JunidecodeTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package gcardone.junidecode;
+package org.deegree.gcardone.junidecode;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
