a08317514862b0b5bbcb4ec68b2b1d3b7057e514
Stephan Reichhelm <reichhelm@grit.de>
2020-04-03 14:44:25 +0200

    * optimize logging * correct textstyling2.png * correct license header

--- deegree-core/deegree-core-rendering-2d/src/test/java/org/deegree/rendering/r2d/AbstractSimilarityTest.java
+++ deegree-core/deegree-core-rendering-2d/src/test/java/org/deegree/rendering/r2d/AbstractSimilarityTest.java
@@ -1,3 +1,44 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2010 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+ and
+ - grit graphische Informationstechnik Beratungsgesellschaft mbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ grit graphische Informationstechnik Beratungsgesellschaft mbH
+ Landwehrstr. 143, 59368 Werne
+ Germany
+ http://www.grit.de/
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
 package org.deegree.rendering.r2d;
 
 import static java.util.Arrays.asList;
@@ -58,11 +99,11 @@ public abstract class AbstractSimilarityTest {
             LOG.error( "Images for test '{}' are not similar enough ({}>{})", name, sim, maximumDifference );
 
             try {
-                LOG.error( "Trying to store rendering_expected_{}/rendering_actual_{} in java.io.tmpdir", name );
+                LOG.error( "Trying to store rendering_{}_expected/rendering_{}_actual in java.io.tmpdir", name, name );
                 IOUtils.write( expectedData, new FileOutputStream( System.getProperty( "java.io.tmpdir" )
-                                                                   + "/rendering_expected" + name + ".png" ) );
-                IOUtils.write( actualData, new FileOutputStream( System.getProperty( "java.io.tmpdir" )
-                                                                 + "/rendering_actual" + name + ".png" ) );
+                                                                   + "/rendering_" + name + "_expected.png" ) );
+                IOUtils.write( actualData, new FileOutputStream( System.getProperty( "java.io.tmpdir" ) + "/rendering_"
+                                                                 + name + "_actual.png" ) );
 
                 System.out.println( "Result returned for " + name + " (base64 -d encoded.dat > failed-test.zip)" );
                 System.out.println( IntegrationTestUtils.toBase64Zip( actualData, name + ".png" ) );
