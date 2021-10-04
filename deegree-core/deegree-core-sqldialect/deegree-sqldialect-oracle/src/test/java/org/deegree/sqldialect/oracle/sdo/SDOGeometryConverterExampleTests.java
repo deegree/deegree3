3c32d671748a86e8d940e240aad971f26e2c3a32
Stephan Reichhelm <reichhelm@grit.de>
2021-10-04 11:29:12 +0200

    #7692 (PR#194) - Fix rendering of Oracle points with angle of rotation

--- deegree-core/deegree-core-sqldialect/deegree-sqldialect-oracle/src/test/java/org/deegree/sqldialect/oracle/sdo/SDOGeometryConverterExampleTests.java
+++ deegree-core/deegree-core-sqldialect/deegree-sqldialect-oracle/src/test/java/org/deegree/sqldialect/oracle/sdo/SDOGeometryConverterExampleTests.java
@@ -142,7 +142,7 @@ public class SDOGeometryConverterExampleTests {
         }
     }
 
-    private String replaceGmlIDs( String text ) {
+    static String replaceGmlIDs( String text ) {
         StringBuffer result = new StringBuffer();
         Pattern pattern = Pattern.compile( "gml:id=\"[^\"]+\"" );
         Matcher matcher = pattern.matcher( text );
@@ -154,7 +154,7 @@ public class SDOGeometryConverterExampleTests {
         return result.toString();
     }
 
-    private Geometry readGMLGeometry( File file )
+    static Geometry readGMLGeometry( File file )
                             throws Exception {
         XMLStreamReaderWrapper xmlReader = new XMLStreamReaderWrapper( file.toURI().toURL() );
         xmlReader.next();
@@ -163,7 +163,7 @@ public class SDOGeometryConverterExampleTests {
         return rdr.parse( xmlReader );
     }
 
-    private String writeGMLGeometry( Geometry geom )
+    static String writeGMLGeometry( Geometry geom )
                             throws Exception {
         XMLMemoryStreamWriter memoryWriter = new XMLMemoryStreamWriter();
         XMLStreamWriter xmlWriter = new IndentingXMLStreamWriter( memoryWriter.getXMLStreamWriter() );
@@ -173,7 +173,7 @@ public class SDOGeometryConverterExampleTests {
         return replaceGmlIDs( memoryWriter.toString() );
     }
 
-    private GeomHolder loadFromFile( File f )
+    static GeomHolder loadFromFile( File f )
                             throws IOException {
         @SuppressWarnings("resource")
         String output = new Scanner( f ).useDelimiter( "\\Z" ).next().replaceAll( "[\r\n]+", " " );
@@ -230,7 +230,7 @@ public class SDOGeometryConverterExampleTests {
         return null;
     }
 
-    private String toString( GeomHolder h ) {
+    static String toString( GeomHolder h ) {
         StringBuilder sb = new StringBuilder();
         sb.append( "MDSYS.SDO_GEOMETRY( " );
         sb.append( h.gtype ).append( ", " );
---
+++ deegree-core/deegree-core-sqldialect/deegree-sqldialect-oracle/src/test/resources/test/oracle/sdo/oriented/MultiPoint.gml
@@ -0,0 +1,12 @@
+<gml:MultiPoint xmlns:gml="http://www.opengis.net/gml/3.2" gml:id="ID_1">
+  <gml:pointMember>
+    <gml:Point gml:id="ID_2">
+      <gml:pos>12.000000 14.000000</gml:pos>
+    </gml:Point>
+  </gml:pointMember>
+  <gml:pointMember>
+    <gml:Point gml:id="ID_3">
+      <gml:pos>13.000000 15.000000</gml:pos>
+    </gml:Point>
+  </gml:pointMember>
+</gml:MultiPoint>
