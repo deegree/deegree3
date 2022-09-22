132395ea8469545539bf7250fc8c667c4e4e2c1e
Stephan Reichhelm <reichhelm@grit.de>
2022-09-22 16:42:15 +0200

    cleanup copyright headers as requested in https://github.com/deegree/deegree3/pull/1198#pullrequestreview-870823362

--- deegree-core/deegree-core-commons/src/main/java/org/deegree/commons/json/JSONParsingException.java
+++ deegree-core/deegree-core-commons/src/main/java/org/deegree/commons/json/JSONParsingException.java
@@ -1,3 +1,40 @@
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2011-2022 by:
+ - grit graphische Informationstechnik Beratungsgesellschaft mbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ grit graphische Informationstechnik Beratungsgesellschaft mbH
+ Landwehrstr. 143, 59368 Werne
+ Germany
+ http://www.grit.de/
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
 package org.deegree.commons.json;
 
 public class JSONParsingException extends RuntimeException {
@@ -6,14 +43,14 @@ public class JSONParsingException extends RuntimeException {
     private String stackTrace = "<< is empty >>";
 
     /**
-     * Creates a new instance of <code>XMLProcessingException</code> without detail message.
+     * Creates a new instance of <code>JSONParsingException</code> without detail message.
      */
     protected JSONParsingException() {
         // nothing to do
     }
 
     /**
-     * Constructs an instance of <code>XMLProcessingException</code> with the specified detail message.
+     * Constructs an instance of <code>JSONParsingException</code> with the specified detail message.
      * 
      * @param msg
      *            the detail message.
@@ -25,7 +62,7 @@ public class JSONParsingException extends RuntimeException {
     }
 
     /**
-     * Constructs an instance of <code>XMLProcessingException</code> with the specified cause.
+     * Constructs an instance of <code>JSONParsingException</code> with the specified cause.
      * 
      * @param cause
      *            the Throwable that caused this XMLParsingException
@@ -36,7 +73,7 @@ public class JSONParsingException extends RuntimeException {
     }
 
     /**
-     * Constructs an instance of <code>XMLProcessingException</code> with the specified detail message.
+     * Constructs an instance of <code>JSONParsingException</code> with the specified detail message.
      * 
      * @param msg
      *            the detail message.
