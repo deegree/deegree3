a1d07fdfa921e965eb9c3e9a3e6f94f656765bd4
Lyn Elisa Goltz <goltz@lat-lon.de>
2023-08-16 18:14:44 +0200

    #9142 - added NullWriter to fix dryRun

---
+++ deegree-tools/deegree-tools-gml/src/main/java/org/deegree/tools/featurestoresql/loader/NullWriter.java
@@ -0,0 +1,41 @@
+/*-
+ * #%L
+ * deegree-cli-utility
+ * %%
+ * Copyright (C) 2023 lat/lon GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package org.deegree.tools.featurestoresql.loader;
+
+import org.deegree.feature.Feature;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+/**
+ * Dummy ItemWriter not writing any features.
+ *
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class NullWriter implements ItemWriter<Feature> {
+
+	@Override
+	public void write(List<? extends Feature> list) throws Exception {
+
+	}
+
+}
