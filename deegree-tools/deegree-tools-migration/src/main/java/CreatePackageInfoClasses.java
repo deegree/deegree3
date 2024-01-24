4e8218af272118b3e4f9fcf57fd763edca858f23
Stephan Reichhelm <reichhelm@grit.de>
2024-01-24 13:13:22 +0100

    * create ModuleInfoProvider

---
+++ deegree-tools/deegree-tools-migration/src/main/java/CreatePackageInfoClasses.java
@@ -0,0 +1,48 @@
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+
+public class CreatePackageInfoClasses {
+
+	private static final Logger LOG = getLogger(CreatePackageInfoClasses.class);
+
+	public static void main(String[] args) throws Exception {
+		URL tplURL = CreatePackageInfoClasses.class.getResource("/PackageInfo.template");
+		URL tplList = CreatePackageInfoClasses.class.getResource("/PackageInfo.list");
+		String template = Files.readString(Paths.get(tplURL.toURI()));
+		for (String line : Files.readAllLines(Paths.get(tplList.toURI()))) {
+			if (line.trim().isEmpty() || line.trim().startsWith("#") || line.trim().endsWith("-")) {
+				continue;
+			}
+			String[] parts = line.trim().split("[ =\t]+", 2);
+			if (parts.length != 2 || parts[0] == null || parts[1] == null) {
+				LOG.warn("Ignored Line: {}", line);
+				continue;
+			}
+			String baseDir = parts[0].trim().substring(parts[0].indexOf("src\\main\\java") + 14);
+			String pkg = baseDir.replace("\\", ".");
+			String className = parts[1].trim();
+			String srcMainDir = parts[0].trim().substring(parts[0].indexOf("src\\main\\") + 9);
+
+			String newFile = template.replace("$PACKAGE$", pkg).replace("$CLASS$", className);
+
+			Path np = Paths.get(parts[0], className + ".java");
+			Path mi = Paths.get(srcMainDir, "resources/META-INF/services/org.deegree.moduleinfo.ModuleInfoProvider");
+
+			Files.createDirectories(np.getParent());
+			Files.writeString(np, newFile, CREATE, TRUNCATE_EXISTING);
+
+			Files.createDirectories(mi.getParent());
+			Files.writeString(mi, pkg + "." + className, CREATE, TRUNCATE_EXISTING);
+
+			LOG.info("-> Class {} Package: {} - {}", parts[1], pkg, parts[0]);
+		}
+	}
+
+}
---
+++ deegree-tools/deegree-tools-migration/src/main/java/IterateSourceTree.java
@@ -0,0 +1,65 @@
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+
+public class IterateSourceTree {
+
+	private static final Logger LOG = getLogger(IterateSourceTree.class);
+
+	public static boolean contains(Path path, Path needle) {
+		Path p = path;
+		while (p != null) {
+			if (p.endsWith(needle)) {
+				return true;
+			}
+			p = p.getParent();
+		}
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Path base = Paths.get("");
+		List<String> ignored = List.of("target", "uncoupled");
+		Path dirSrcMain = Path.of("src", "main", "java");
+		LOG.warn("DIR {}", base.toAbsolutePath());
+
+		Files.walkFileTree(base, new SimpleFileVisitor<>() {
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				String dirName = dir.getFileName().toString();
+				if (dirName.startsWith(".") || ignored.contains(dirName)) {
+					return FileVisitResult.SKIP_SUBTREE;
+				}
+				if (contains(dir, dirSrcMain)) {
+					long cnt = -1;
+					try (Stream<Path> entries = Files.walk( dir, 1  ) ) {
+						cnt = entries.filter( pf -> Files.isRegularFile( pf, LinkOption.NOFOLLOW_LINKS) ) //
+							   //.peek( pf -> LOG.info("- (file): {}", pf) ) //
+							   .count();
+					}
+
+					LOG.info("- {} = {}", dir, cnt);
+					// if ( cnt > 0) {
+					// return FileVisitResult.TERMINATE;
+					// }
+				}
+				return FileVisitResult.CONTINUE;
+
+			}
+		});
+		// .filter( Files::isDirectory )
+		// .forEach( p -> LOG.info("- {}", p) );
+	}
+
+}
