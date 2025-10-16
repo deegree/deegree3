730b1a90e3f2db162898c65225fbe4d144a74f5e
Julian Zilz <zilz@lat-lon.de>
2025-10-16 18:40:01 +0200

    #1868 - Remove references to old popup, use new popup, remove popup for log in and out

--- deegree-services/deegree-webservices/src/main/webapp/resources/js/console.js
+++ deegree-services/deegree-webservices/src/main/webapp/resources/js/console.js
@@ -1,18 +1,3 @@
-function show() {
-	document.getElementById("PLEASEWAIT").style.display = 'inline';
-	document.getElementById("PLEASEWAIT_BG").style.display = 'inline';
-}
-
-function blockInput(event) {
-	if (event.status == "begin") {
-		document.getElementById("PLEASEWAIT").style.display = 'inline';
-		document.getElementById("PLEASEWAIT_BG").style.display = 'inline';
-	} else if (event.status == "complete") {
-		document.getElementById("PLEASEWAIT").style.display = 'none';
-		document.getElementById("PLEASEWAIT_BG").style.display = 'none';
-	}
-}
-
 function confirmDelete() {
 	return confirm("Do you really want do delete?");
 }
